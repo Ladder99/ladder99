8cc068af16e7787af3363ec9312a202b4a9f5189
Brian Burns <bburns.km@gmail.com>
2021-03-05 23:39:26 -0600

    wip adapter out port

--- parts/adapter/code/src/index.js
+++ parts/adapter/code/src/index.js
@@ -15,6 +15,8 @@ const outputHost = process.env.OUTPUT_HOST || 'localhost'
 const outputPort = Number(process.env.OUTPUT_PORT || 7878)
 const outputUrl = `${outputHost}:${outputPort}`
 
+let tcpSocket
+
 setTimeout(() => {
   console.log(`MTConnect Adapter`)
   console.log(`Subscribes to MQTT topics, transforms to SHDR, sends to diode.`)
@@ -56,24 +58,69 @@ setTimeout(() => {
     }
   })
 
-  console.log(`Creating TCP output socket`)
-  const socket = new net.Socket()
+  //-------------------
 
-  console.log(`Connecting to output socket`, outputUrl, `...`)
-  socket.connect(outputUrl, () => {
-    console.log(`Connected to TCP output socket`)
+  console.log(`TCP creating socket...`)
+  const tcp = net.createServer()
+
+  tcp.on('connection', socket => {
+    tcpSocket = tcp
+    const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
+    console.log('TCP new client connection from', remoteAddress)
+    socket.on('data', chunk => {
+      const str = chunk.toString().trim()
+      // if (str === '* PING') {
+      //   const response = '* PONG 10000'
+      //   console.log(`TCP received PING - sending PONG:`, response)
+      //   socket.write(response + '\n')
+      // } else {
+      console.log('TCP connection data from %s: %j', remoteAddress, chunk)
+      console.log(`TCP data as string:`, str)
+      // udpSocket.write(chunk)
+      // }
+    })
+    socket.on('end', () => {
+      console.log('TCP connection closing...')
+    })
+    socket.once('close', () => {
+      console.log('TCP connection closed', remoteAddress)
+    })
+    socket.on('error', err => {
+      console.error('TCP connection error', remoteAddress, err)
+    })
   })
+
+  tcp.on('close', () => {
+    console.log(`TCP server - all connections closed.`)
+  })
+
+  tcp.on('listening', () => {
+    console.log('TCP server is listening...')
+  })
+
+  console.log(`TCP listening to socket at`, outputUrl, `...`)
+  tcp.listen(outputUrl, () => {
+    console.log('TCP listening to', tcp.address())
+  })
+
+  // console.log(`Creating TCP output socket`)
+  // const socket = new net.Socket()
+
+  // console.log(`Connecting to output socket`, outputUrl, `...`)
+  // socket.connect(outputUrl, () => {
+  //   console.log(`Connected to TCP output socket`)
+  // })
 
   // pass message on to diode
   function sendToDiode(str) {
     console.log(`Sending SHDR to output TCP at`, outputUrl, `...`)
-    socket.write(str)
+    tcpSocket.write(str)
   }
 
   function shutdown() {
     console.log(`Exiting...`)
     console.log(`Closing TCP output socket...`)
-    socket.end()
+    tcpSocket.end()
     console.log(`Closing MQTT connection...`)
     mqtt.end()
     process.exit()
