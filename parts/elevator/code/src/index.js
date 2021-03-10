d408446397e66bc9d15b73cc24836b37c92d7606
Brian Burns <bburns.km@gmail.com>
2021-03-10 17:52:29 -0600

    wip elevator

--- parts/elevator/code/src/index.js
+++ parts/elevator/code/src/index.js
@@ -5,22 +5,47 @@ const convert = require('xml-js') // https://github.com/nashwaan/xml-js
 try {
   const doc = yaml.load(fs.readFileSync('../config/devices.yaml', 'utf8'))
   console.log(doc)
+  const js = {
+    _declaration: {
+      _attributes: {
+        version: '1.0',
+        encoding: 'UTF-8',
+      },
+    },
+    // <MTConnectDevices
+    //   xmlns:m = "urn:mtconnect.org:MTConnectDevices:1.6"
+    //   xmlns:xsi = "http://www.w3.org/2001/XMLSchema-instance"
+    //   xmlns="urn:mtconnect.org:MTConnectDevices:1.6"
+    //   xsi:schemaLocation = "urn:mtconnect.org:MTConnectDevices:1.6 http://www.mtconnect.org/schemas/MTConnectDevices_1.6.xsd"
+    //  >
+    // element: [{ _attributes: { x: 2 } }],
+    MTConnectDevices: [
+      {
+        _attributes: {
+          'xmlns:m': 'urn:mtconnect.org:MTConnectDevices:1.6',
+          'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+          xmlns: 'urn:mtconnect.org:MTConnectDevices:1.6',
+          'xsi:schemaLocation':
+            'urn:mtconnect.org:MTConnectDevices:1.6 http://www.mtconnect.org/schemas/MTConnectDevices_1.6.xsd',
+        },
+        Header: {
+          _attributes: {
+            creationTime: '2021-02-23T18:44:40+00:00',
+            sender: 'localhost',
+            instanceId: '1267728234',
+            bufferSize: '131072',
+            version: '1.6.0.7',
+          },
+        },
+      },
+    ],
+  }
+
+  const xml = convert.js2xml(js, { compact: true, spaces: 2 })
+  console.log(xml)
 } catch (e) {
   console.log(e)
 }
-
-const input = {
-  _declaration: {
-    _attributes: {
-      version: '1.0',
-      encoding: 'UTF-8',
-    },
-  },
-  element: [{ _attributes: { x: 2 } }],
-}
-const xmlString = convert.js2xml(input, {
-  compact: true,
-})
 
 // const input = {
 //   declaration: {
@@ -34,6 +59,4 @@ const xmlString = convert.js2xml(input, {
 //   ],
 // }
 // const xmlString = convert.js2xml(input)
-
-// <?xml version="1.0" encoding="UTF-8"?><data><foo>bar</foo></data>
-console.log(xmlString)
+// console.log(xmlString)
