9d562e25d0197d620f9aca5bcfbdc6505b432c69
Brian Burns <bburns.km@gmail.com>
2021-03-14 04:50:21 -0500

    wip init client

--- parts/devices/input/ccs-pa/client.js
+++ parts/devices/input/ccs-pa/client.js
@@ -1,18 +1,21 @@
 // client sits between broker and adapter
 
 export function init(broker, adapter) {
-  const topic = 'l99/ccs/evt/query'
-  broker.onMessage = onQueryResult
-  broker.subscribe(topic)
+  const topicQuerySend = 'l99/ccs/cmd/query'
+  const topicQueryResult = 'l99/ccs/evt/query'
+
+  broker.subscribe(topicQueryResult, onQueryResult)
+  broker.send(topicQuerySend, '{}')
+
   function onQueryResult(msg) {
-    const unpack = getUnpack(topic)
+    broker.unsubscribe(topicQueryResult, onQueryResult)
+    const unpack = getUnpack()
     const data = unpack(msg)
-    //. shovel data off to adapter
-    adapter.shovel(data)
-    broker.onMessage = null
+    adapter.send(data)
   }
 }
 
+// (can ignore topic as all messages from this device are the same)
 export function getUnpack(topic) {
   return msg => JSON.parse(msg.payload.toString())
 }
