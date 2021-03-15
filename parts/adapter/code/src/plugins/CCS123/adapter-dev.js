de488572dea98ad889129657efe4397cf50aa665
Brian Burns <bburns.km@gmail.com>
2021-03-15 12:21:09 -0500

    wip

--- parts/adapter/code/src/plugins/CCS123/adapter-dev.js
+++ parts/adapter/code/src/plugins/CCS123/adapter-dev.js
@@ -1,41 +1,12 @@
+// adapter plugin code
+// derived from parts/devices/definitions/ccs-pa and instances/CCS123.yaml etc
+
 //. this is experimental for development
 
 export const handlers = {
   'l99/CCS123/evt/status': status,
   'l99/CCS123/evt/read': read,
 }
-
-function status() {}
-function read() {}
-
-export function getData(buffer) {
-  let data = JSON.parse(buffer.toString())
-  if (!Array.isArray(data)) {
-    data = [data]
-  }
-  return data
-}
-
-// // get output for given item and cache
-// function getOutput(cache, item) {}
-
-// export function getState(lookup) {
-//   const topic = 'l99/ccs/evt/status'
-//   const prop = 'state'
-//   const names = {
-//     50: 'WAIT',
-//     100: 'WAIT',
-//     200: 'PROGRAM_STOPPED',
-//     250: 'WAIT',
-//     300: 'WAIT',
-//     400: 'ACTIVE',
-//   }
-//   return lookup(topic, prop, names)
-// }
-
-// adapter plugin code
-// derived from parts/devices/definitions/ccs-pa and instances/CCS123.yaml etc
-
 const device = 'ccs-pa'
 const topics = {
   sendQuery: 'l99/ccs/cmd/query',
@@ -43,6 +14,29 @@ const topics = {
   receiveStatus: 'l99/ccs/evt/status',
   receiveRead: 'l99/ccs/evt/read',
 }
+
+function status() {}
+function read() {}
+
+// unpack a message payload and append some metadata.
+// eg converts JSON string payload to js.
+export function unpack(topic, payload) {
+  let data = JSON.parse(payload.toString())
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
+  const received = new Date()
+  const obj = { device, topic, data, received }
+  return obj
+}
+
+// export function getData(buffer) {
+//   let data = JSON.parse(buffer.toString())
+//   if (!Array.isArray(data)) {
+//     data = [data]
+//   }
+//   return data
+// }
 
 // initialize the client plugin.
 // queries the device for address space definitions, subscribes to topics.
@@ -71,18 +65,4 @@ export function init(mqtt, cache) {
     const obj = unpack(topic, payload)
     cache.save(obj)
   }
-}
-
-// helpers
-
-// unpack a message payload and append some metadata.
-// eg converts JSON string payload to js.
-export function unpack(topic, payload) {
-  let data = JSON.parse(payload.toString())
-  if (!Array.isArray(data)) {
-    data = [data]
-  }
-  const received = new Date()
-  const obj = { device, topic, data, received }
-  return obj
 }
---
+++ parts/adapter/code/src/plugins/CCS123/output.js
@@ -0,0 +1,15 @@
+//. this could have all the shdr calculations compiled from the device.yaml file?
+
+// export function getState(lookup) {
+//   const topic = 'l99/ccs/evt/status'
+//   const prop = 'state'
+//   const names = {
+//     50: 'WAIT',
+//     100: 'WAIT',
+//     200: 'PROGRAM_STOPPED',
+//     250: 'WAIT',
+//     300: 'WAIT',
+//     400: 'ACTIVE',
+//   }
+//   return lookup(topic, prop, names)
+// }
