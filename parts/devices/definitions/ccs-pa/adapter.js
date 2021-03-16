94f4935550da704e8e11004f7f25ae0da80f33c7
Brian Burns <bburns.km@gmail.com>
2021-03-16 03:28:22 -0500

    wip

--- parts/devices/definitions/ccs-pa/adapter.js
+++ parts/devices/definitions/ccs-pa/adapter.js
@@ -1,10 +1,3 @@
-// we/user write this code, then `just devices` will copy it to adapter/plugins -
+// we write this code, then `just devices` will copy it to adapter/plugins -
 // see that for the dev version.
 // get it working there, then move it here.
-
-const topics = {
-  sendQuery: 'l99/${serialNumber}/cmd/query',
-  receiveQuery: 'l99/${serialNumber}/evt/query',
-  receiveStatus: 'l99/${serialNumber}/evt/status',
-  receiveRead: 'l99/${serialNumber}/evt/read',
-}
