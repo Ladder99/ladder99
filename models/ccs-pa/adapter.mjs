70a031279afbd24fad3e94ff7f799c9cab06ddc3
Brian Burns <bburns.km@gmail.com>
2021-03-26 12:30:35 -0500

    cmts

--- models/ccs-pa/adapter.mjs
+++ models/ccs-pa/adapter.mjs
@@ -2,3 +2,6 @@
 // will develop adapter plugin skeleton here,
 // compiler will append calcs to end and copy it all to
 // setups/demo/volumes/adapter/<deviceId>.mjs
+
+//. or could you choose the plugin skeletons to use for each model?
+// eg ccs-pa could use mqtt AND kepware text plugins
