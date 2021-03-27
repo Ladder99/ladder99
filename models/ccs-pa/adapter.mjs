48f1ae9d11cfeae2c2e85e1f33734128d42b8e14
Brian Burns <bburns.km@gmail.com>
2021-03-27 09:35:14 -0500

    cmt

--- models/ccs-pa/adapter.mjs
+++ models/ccs-pa/adapter.mjs
@@ -2,4 +2,4 @@
 // setups/demo/volumes/adapter/<deviceId>.mjs
 
 //. choose the plugin skeletons to use for each model?
-// eg ccs-pa could use mqtt AND kepware text plugins
+// eg ccs-pa could use mqtt AND kepware opc AND asc cpc text plugins
