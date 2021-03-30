c0c78831533c10b4d2309552ac3be3aa1f0a50ea
Brian Burns <bburns.km@gmail.com>
2021-03-30 12:00:40 -0500

    ctd

--- services/adapter/src/plugins/opc.js
+++ services/adapter/src/plugins/opc.js
@@ -39,25 +39,102 @@ const endpointUrl =
 async function main() {
   try {
     // step 1 : connect to
-    // _"Connection"
+    await client.connect(endpointUrl)
+    console.log('connected !')
+
     // step 2 : createSession
-    // _"create session"
-    // step 3 : browse
-    // _"browsing the root folder"
+    const session = await client.createSession()
+    console.log('session created !')
+
+    // step 3 : browse the root folder
+    const browseResult = await session.browse('RootFolder')
+    console.log('references of RootFolder :')
+    for (const reference of browseResult.references) {
+      console.log('   -> ', reference.browseName.toString())
+    }
+
     // step 4 : read a variable with readVariableValue
-    // _"read a variable with readVariableValue"
+    const dataValue2 = await session.read({
+      nodeId: 'ns=1;s=free_memory',
+      attributeId: AttributeIds.Value,
+    })
+    console.log(' value = ', dataValue2.toString())
+
     // step 4' : read a variable with read
-    // _"read a variable with read"
+    const maxAge = 0
+    const nodeToRead = {
+      nodeId: 'ns=3;s=Scalar_Simulation_String',
+      attributeId: AttributeIds.Value,
+    }
+    const dataValue = await session.read(nodeToRead, maxAge)
+    console.log(' value ', dataValue.toString())
+
     // step 5: install a subscription and install a monitored item for 10 seconds
-    // _"install a subscription"
+    const subscription = ClientSubscription.create(session, {
+      requestedPublishingInterval: 1000,
+      requestedLifetimeCount: 100,
+      requestedMaxKeepAliveCount: 10,
+      maxNotificationsPerPublish: 100,
+      publishingEnabled: true,
+      priority: 10,
+    })
+    subscription
+      .on('started', function () {
+        console.log(
+          'subscription started for 2 seconds - subscriptionId=',
+          subscription.subscriptionId
+        )
+      })
+      .on('keepalive', function () {
+        console.log('keepalive')
+      })
+      .on('terminated', function () {
+        console.log('terminated')
+      })
+    // install monitored item
+    const itemToMonitor = {
+      nodeId: 'ns=1;s=free_memory',
+      attributeId: AttributeIds.Value,
+    }
+    const parameters = {
+      samplingInterval: 100,
+      discardOldest: true,
+      queueSize: 10,
+    }
+    const monitoredItem = ClientMonitoredItem.create(
+      subscription,
+      itemToMonitor,
+      parameters,
+      TimestampsToReturn.Both
+    )
+    monitoredItem.on('changed', dataValue => {
+      console.log(' value has changed : ', dataValue.value.toString())
+    })
+    await timeout(10000)
+    console.log('now terminating subscription')
+    await subscription.terminate()
+
     // step 6: finding the nodeId of a node by Browse name
-    // _"finding the nodeId of a node by Browse name"
+    const browsePath = makeBrowsePath(
+      'RootFolder',
+      '/Objects/Server.ServerStatus.BuildInfo.ProductName'
+    )
+    const result = await session.translateBrowsePath(browsePath)
+    const productNameNodeId = result.targets[0].targetId
+    console.log(' Product Name nodeId = ', productNameNodeId.toString())
+
     // close session
-    // _"closing session"
+    await session.close()
+
     // disconnecting
-    // _"disconnecting"
+    await client.disconnect()
+    console.log('done !')
   } catch (err) {
     console.log('An error has occured : ', err)
   }
 }
 main()
+
+async function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
