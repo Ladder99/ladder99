b3b7fee16afadc80e9b36a1ff9d0b3e871da1b79
Brian Burns <bburns.km@gmail.com>
2021-04-03 09:55:23 -0500

    example txt

--- services/application/src/application.js
+++ services/application/src/application.js
@@ -10,20 +10,20 @@ async function shovel() {
     },
   })
   const json = await response.json()
-  console.log(json)
-  const streams = json.MTConnectStreams.Streams
-  for (const stream of streams) {
-    const device = stream.DeviceStream
-    console.log(device)
-    const components = device.ComponentStreams
-    for (const component of components) {
-      console.log(component)
-      const events = component.ComponentStream.Events
-      for (const event of events) {
-        console.log(event)
-      }
-    }
-  }
+  console.dir(json, { depth: null })
+  // const streams = json.MTConnectStreams.Streams
+  // for (const stream of streams) {
+  //   const device = stream.DeviceStream
+  //   console.log(device)
+  //   const components = device.ComponentStreams
+  //   for (const component of components) {
+  //     console.log(component)
+  //     const events = component.ComponentStream.Events
+  //     for (const event of events) {
+  //       console.log(event)
+  //     }
+  //   }
+  // }
 }
 
 setInterval(shovel, 2000)
