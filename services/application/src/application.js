a4768cf3c1afdfed7d47e4a7b0ee728a6be60fc8
Brian Burns <bburns.km@gmail.com>
2021-04-07 23:28:32 -0500

    start using influxdb

--- services/application/src/application.js
+++ services/application/src/application.js
@@ -1,7 +1,20 @@
 import fetch from 'node-fetch'
+import Influx from 'influx'
 
 const url = process.env.URL || 'http://agent:5000/sample'
 const interval = Number(process.env.INTERVAL || 2000) // msec
+
+const influx = new Influx.InfluxDB({
+  host: 'influxdb',
+  database: 'ocean_tides',
+  schema: [
+    {
+      measurement: 'tide',
+      fields: { height: Influx.FieldType.FLOAT },
+      tags: ['unit', 'location'],
+    },
+  ],
+})
 
 async function shovel() {
   const response = await fetch(url, {
