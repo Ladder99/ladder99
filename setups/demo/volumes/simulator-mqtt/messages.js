31d4e30506aa13c53fc4564734acd77b26f9690c
Brian Burns <bburns.km@gmail.com>
2021-04-09 22:41:15 -0500

    wip

--- setups/demo/volumes/simulator-mqtt/messages.js
+++ setups/demo/volumes/simulator-mqtt/messages.js
@@ -532,7 +532,7 @@ module.exports = [
       connection: 'online',
       state: 400, // 200 stopped, 400 running
       program: 'pgm0',
-      step: 'step1',
+      step: 'Waiting',
       faults: {},
       cpu_time: 691322.50763624,
       utc_time: 1.6098097061826477e9,
@@ -565,12 +565,35 @@ module.exports = [
       connection: 'online',
       state: 200, // 200 stopped, 400 running
       program: 'pgm0',
-      step: 'step1',
+      step: 'Cycle_Start',
       faults: {
         1: { description: 'hard fault', hard: true, count: 1 },
         10: { description: 'another hard fault', hard: true, count: 1 }, // estop
         50: { description: 'soft fault', hard: false, count: 1 },
       },
+      cpu_time: 691322.50763624,
+      utc_time: 1.6098097061826477e9,
+      build_no: '1.3.0.3',
+      _ts: 1609809706196, // msec since 1970-01-01
+    },
+  },
+
+  {
+    topic: 'l99/${deviceId}/evt/read',
+    json: [
+      { address: '%Q0.0', value: 0 },
+      { address: '%Q0.1', value: 0 },
+    ],
+  },
+
+  {
+    topic: 'l99/${deviceId}/evt/status',
+    json: {
+      connection: 'online',
+      state: 400, // 200 stopped, 400 running
+      program: 'pgm0',
+      step: 'Cycle_Finish',
+      faults: {},
       cpu_time: 691322.50763624,
       utc_time: 1.6098097061826477e9,
       build_no: '1.3.0.3',
