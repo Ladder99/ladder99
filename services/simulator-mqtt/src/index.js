313249da89b3a712b9f0329acbcc91903f6a6dba
Brian Burns <bburns.km@gmail.com>
2021-04-11 04:43:49 -0500

    wip

--- services/simulator-mqtt/src/index.js
+++ services/simulator-mqtt/src/index.js
@@ -6,12 +6,13 @@ const mqttlib = require('mqtt')
 const host = process.env.MQTT_HOST || 'localhost'
 const port = Number(process.env.MQTT_PORT || 1883)
 const deviceId = process.env.DEVICE_ID // eg 'ccs-pa-001'
-const clientId = deviceId || 'simulator-' + Math.random()
-const config = { host, port, clientId }
 const messagesFile = process.env.MESSAGES_FILE
 const messageDelay = Number(process.env.MESSAGE_DELAY || 1000)
 const loop = Boolean(process.env.LOOP || false)
 const loopDelay = Number(process.env.LOOP_DELAY || 3000)
+
+const clientId = deviceId || 'simulator-' + Math.random()
+const config = { host, port, clientId }
 
 const messages = require(messagesFile)
 console.log({ messages })
