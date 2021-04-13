4c1ac0cb84ea2586a11e57c99536b2228d431b62
Brian Burns <bburns.km@gmail.com>
2021-04-13 10:52:40 -0500

    wip new input parser

--- services/adapter/src/adapter.js
+++ services/adapter/src/adapter.js
@@ -100,28 +100,46 @@ for (const device of devices) {
  */
 function getOutputs({ outputTemplates, types, deviceId }) {
   const outputs = outputTemplates.map(template => {
-    // build up dependsOn array during parse from what cache keys are seen
+    // // build up dependsOn array during parse from what cache keys are seen
+    // const dependsOn = []
+    // // m will be undefined if no match, or array with elements 1,2,3 with contents
+    // //. handle multiple <>'s in a string also - how do? .* needs to be greedy for one thing
+    // //. also check if str is multiline - then need to wrap in braces?
+    // const regexp = /(.*)<(.*)>(.*)/
+    // const m = (template.value || '').match(regexp)
+    // let value = cache => template.value // by default just return string value
+    // // got match
+    // if (m) {
+    //   const str = m[1] + `cache.get('${deviceId}-${m[2]}').value` + m[3]
+    //   value = cache => eval(str) // evaluate the cache access string
+    //   //. assume each starts with deviceId?
+    //   const dependency = `${deviceId}-${m[2]}`
+    //   dependsOn.push(dependency)
+    // }
+
+    // replace all occurrences of <key> with cache.get('').value
+    const regexp1 = /<(.*?)>/gm
+    template.value = template.value.replaceAll(
+      regexp1,
+      `cache.get('${deviceId}-$&').value` // $& is the matched substring
+    )
+    const value = cache => template.value // by default just return string value
+
+    // get dependsOn AFTER transforms, because user might have
+    // specified a cache get manually.
     const dependsOn = []
-    // m will be undefined if no match, or array with elements 1,2,3 with contents
-    //. handle multiple <>'s in a string also - how do? .* needs to be greedy for one thing
-    //. also check if str is multiline - then need to wrap in braces?
-    const regexp = /(.*)<(.*)>(.*)/
-    const m = (template.value || '').match(regexp)
-    let value = cache => template.value // by default just return string value
-    // got match
-    if (m) {
-      const str = m[1] + `cache.get('${deviceId}-${m[2]}').value` + m[3]
-      value = cache => eval(str) // evaluate the cache access string
-      //. assume each starts with deviceId?
-      const dependency = `${deviceId}-${m[2]}`
-      dependsOn.push(dependency)
+    const regexp2 = /cache\.get\('(.*?)'\).value/gm
+    let match
+    while ((match = regexp2.exec(template.value)) !== null) {
+      const key = match[1]
+      dependsOn.push(key)
     }
     const output = {
       dependsOn,
       //. assume each starts with deviceId?
       //. call this id, as it's such in the devices.xml?
       key: `${deviceId}-${template.key}`,
-      value, //. getValue?
+      value, //. getValue
     }
     return output
   })
