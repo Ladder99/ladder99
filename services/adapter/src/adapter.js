3bddb39a94c5e149b5339bd5b05bd50cb3d82ef7
Brian Burns <bburns.km@gmail.com>
2021-04-21 21:59:07 -0500

    cmt

--- services/adapter/src/adapter.js
+++ services/adapter/src/adapter.js
@@ -10,7 +10,7 @@ import { Cache } from './cache.js'
 // file system inputs
 const pluginsFolder = './plugins' // for protocol handlers, eg mqtt-json - must start with .
 const dataFolder = '/etc/ladder99' // incl devices.yaml
-const modelsFolder = '/etc/ladder99/models'
+const modelsFolder = '/etc/ladder99/models' // incl ccs-pa/model.yaml etc
 
 // load devices.yaml - see setups/demo/devices.yaml
 // const yamlfile = `${devicesFolder}/devices.yaml`
