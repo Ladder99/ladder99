67482ee349b76983eb708da1a6716f0112aff6eb
Brian Burns <bburns.km@gmail.com>
2021-04-28 12:39:33 -0500

    sh

--- pi_install.sh
+++ pi_install.sh
@@ -10,6 +10,18 @@
 # curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | \
 #   sudo bash -s -- --to /usr/local/bin
 
+# install and upgrade docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo apt upgrade docker
+
+# install libseccomp to fix docker bug
+# because when compile with docker get errors - 
+# "At least one invalid signature was encountered."
+# see https://askubuntu.com/questions/1263284/apt-update-throws-signature-error-in-ubuntu-20-04-container-on-arm#
+wget http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.1-1_armhf.deb
+sudo dpkg -i libseccomp2_2.5.1-1_armhf.deb
+
 # install docker-compose - not yet in docker command
 sudo pip3 install docker-compose
 
