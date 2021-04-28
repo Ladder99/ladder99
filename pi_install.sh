2cb97da0689baa47cdad554bc72244b02b339765
Brian Burns <bburns.km@gmail.com>
2021-04-28 12:10:05 -0500

    installsh

--- pi_install.sh
+++ pi_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# install apps on pi
+
 # install just - a command runner like make
 # see https://github.com/casey/just
-
 #. this grabs 64-bit version - no 32-bit avail in his releases yet. 
 # see https://github.com/casey/just/issues/739
 # uses go cargo to build?
-
 # curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | \
 #   sudo bash -s -- --to /usr/local/bin
+
+# install docker-compose - not yet in docker command
+sudo pip3 install docker-compose
