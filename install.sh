b58ef5bb66b00d97359b3db367fb7b3a135ec3f7
Brian Burns <bburns.km@gmail.com>
2021-04-28 10:46:19 -0500

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# install just - a command runner like make
+# see https://github.com/casey/just
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | sudo bash -s -- --to /usr/local/bin
