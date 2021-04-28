1b7dab5dff08bc69d88a95cfb5005be53fa52cef
Brian Burns <bburns.km@gmail.com>
2021-04-28 10:56:50 -0500

    no arm build for just yet

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@
 
 # install just - a command runner like make
 # see https://github.com/casey/just
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | sudo bash -s -- --to /usr/local/bin
+#. this grabs 64-bit version - no 32-bit avail in his releases. 
+# curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | \
+#   sudo bash -s -- --to /usr/local/bin
+
+# git clone https://github.com/casey/just.git
+
+
