65fa9670c9d8fc882d3224d9a07cbabea38005d9
Brian Burns <bburns.km@gmail.com>
2021-04-29 17:54:59 -0500

    filter down to Execution events

--- services/application/src/application.test.js
+++ services/application/src/application.test.js
@@ -1,5 +1,11 @@
 import * as domain from './domain.js'
 import tree from '../example-sample.js'
 
-domain.traverse(tree, console.log)
+// domain.traverse(tree, console.log)
 // domain.traverse(tree, dataItems => console.log(dataItems[0]))
+domain.traverse(tree, dataItems => {
+  if (dataItems[0].type === 'Execution') {
+    // ie id is 'ccs-pa-001-state'
+    console.log(dataItems[0])
+  }
+})
