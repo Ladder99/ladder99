dde2f2f68c1486b7cb667101736ae8d578a65535
Brian Burns <bburns.km@gmail.com>
2021-04-29 17:23:32 -0500

    app test

--- services/application/src/application.test.js
+++ services/application/src/application.test.js
@@ -2,5 +2,14 @@ import * as domain from './domain.js'
 import tree from '../example-current.js'
 
 domain.traverse(tree, node => {
-  console.log(node)
+  // console.log(node)
+  const types = Object.keys(node)
+  types.forEach(type => {
+    const dataItem = node[type]
+    const id = dataItem['@dataItemId']
+    const sequence = dataItem['@sequence']
+    const timestamp = dataItem['@timestamp']
+    const value = dataItem.Value
+    console.log(id, value)
+  })
 })
