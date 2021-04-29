a20f17d5a6601a34b63be91e0853dd2d64a93a84
Brian Burns <bburns.km@gmail.com>
2021-04-29 17:20:05 -0500

    wip traverse tree

--- services/application/src/domain.js
+++ services/application/src/domain.js
@@ -3,8 +3,21 @@ export function traverse(node, callback) {
   if (Array.isArray(node)) {
     node.forEach(subnode => traverse(subnode, callback))
   } else if (node !== null && typeof node === 'object') {
-    Object.values(node).forEach(value => traverse(value, callback))
+    // Object.values(node).forEach(value => traverse(value, callback))
+    Object.entries(node).forEach(([key, value]) => {
+      if (key === 'Samples') {
+        value.forEach(callback)
+      } else if (key === 'Events') {
+        value.forEach(callback)
+      } else if (key === 'Conditions') {
+        value.forEach(callback)
+      } else {
+        traverse(value, callback)
+      }
+    })
+  } else if (node === null || node === undefined) {
+    return
   } else {
-    callback(node)
+    // callback(node)
   }
 }
