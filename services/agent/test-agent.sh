6f47ae402163ca6112117d428f0001e4a285573d
Brian Burns <bburns.km@gmail.com>
2021-04-30 12:22:15 -0500

    wip dockerfile for agent with test

---
+++ services/agent/test-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run the adapter simulator
+/usr/bin/ruby /etc/mtconnect/adapter/run_scenario.rb -l \
+  /etc/mtconnect/adapter/VMC-3Axis-Log.txt
+
+# run the agent with sample agent config
+/usr/local/bin/agent run /etc/mtconnect/agent/agent.cfg
