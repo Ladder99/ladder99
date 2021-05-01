a33b7e4ba815bbc2ac9ebb277c758878f6223e8c
Brian Burns <bburns.km@gmail.com>
2021-05-01 03:02:00 -0500

    cleanup

--- services/agent/test-agent.sh
+++ services/agent/test-agent.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # run the adapter simulator
-#/usr/bin/ruby /etc/mtconnect/adapter/run_scenario.rb -l \
-#  /etc/mtconnect/adapter/VMC-3Axis-Log.txt &
 /usr/bin/ruby /etc/mtconnect/simulator/run_scenario.rb -l \
   /etc/mtconnect/simulator/VMC-3Axis-Log.txt &
 
 # run the agent with sample agent config
-# /usr/local/bin/agent debug /etc/mtconnect/adapter/agent.cfg
-/usr/local/bin/agent debug /etc/mtconnect/simulator/agent.cfg
+cd /etc/mtconnect/simulator && agent debug agent.cfg
