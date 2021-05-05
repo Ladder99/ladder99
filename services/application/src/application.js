439b20b14d4cc90585ed81e2e72dc6ac2cc11565
Brian Burns <bburns.km@gmail.com>
2021-05-05 10:37:50 -0500

    wip write values to db

--- services/application/src/application.js
+++ services/application/src/application.js
@@ -6,20 +6,11 @@ import * as domain from './domain.js'
 
 console.log(`MTConnect Application starting`)
 
-// const host = 'timescaledb'
-// const port = '5432'
-// const database = 'ladder99'
-// const username = 'postgres'
-// const password = 'gralgrut'
-// const connect = `postgres://${username}:${password}@${host}:${port}/${database}`
-
 const client = new Client()
-await client.connect()
-const res = await client.query('SELECT NOW(), $1::text as message', [
-  'Hello world!',
-])
-console.log(res.rows[0].message) // Hello world!
-await client.end()
+await client.connect() // uses envars PGHOST, PGPORT, etc - set with `source .env`
+// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+// console.log(res.rows[0].message) // Hello world!
+// await client.end()
 
 // const baseUrl = process.env.BASE_URL || 'http://localhost:5000'
 const baseUrl = process.env.BASE_URL || 'http://192.168.0.109:5000'
@@ -34,7 +25,7 @@ async function shovel() {
   // const sampleUrl = `${baseUrl}/sample?from=${from}&count=${count}`
   const url = currentUrl
   try {
-    console.log(`fetch from ${url}...`)
+    // console.log(`fetch from ${url}...`)
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -47,11 +38,15 @@ async function shovel() {
     // fs.writeFileSync('./example.json', JSON.stringify(tree))
 
     // traverse the tree and output state
-    domain.traverse(tree, dataItems => {
+    domain.traverse(tree, async dataItems => {
       if (dataItems[0].type === 'Execution') {
         // console.log(dataItems[0])
         const dataItem = dataItems[0] //.
         console.log(dataItem.value)
+        //. dump value to db
+        const sql = `INSERT INTO execution(time, value) VALUES($1, $2) RETURNING *`
+        const values = [dataItem.timestamp, dataItem.value]
+        await client.query(sql, values)
       }
     })
   } catch (error) {
@@ -62,3 +57,5 @@ async function shovel() {
     }
   }
 }
+
+await client.end()
