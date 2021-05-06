b1619e2755faa19870737b60a803b4c28e207705
Brian Burns <bburns.km@gmail.com>
2021-05-05 22:32:50 -0500

    add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,10 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
+html_logo = 'images/LadderLogo.png'
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
