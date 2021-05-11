ddefe64a600329bff294234fc7c8f05cc513ff45
Brian Burns <bburns.km@gmail.com>
2021-05-11 01:53:29 -0500

    moving agent to mtconnect-agent repo

--- services/agent/test-agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script is included in the image
-
-# run the adapter simulator
-/usr/bin/ruby /etc/mtconnect/simulator/run_scenario.rb -l \
-  /etc/mtconnect/simulator/VMC-3Axis-Log.txt &
-
-# run the agent with sample agent config
-cd /etc/mtconnect/simulator && agent debug agent.cfg
