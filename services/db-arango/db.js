511222ff847bec0236861e20de0496334f4ed5f1
Brian Burns <bburns.km@gmail.com>
2021-05-16 13:17:44 -0500

    wip

--- services/db-arango/db.js
+++ services/db-arango/db.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
+
 const { Database } = require('arangojs')
 
 const db = new Database({ url: process.env.DB_HOST })
-// console.log(db)
 
 db.database(process.env.DB_NAME)
-// console.log(db)
 
 // db.useBasicAuth(process.env.DB_USERNAME, process.env.DB_PASSWORD)
 
-// export default db
 module.exports = db
