e4edc4c2d7fe033512ee892b87e29958bcb9bbf8
Brian Burns <bburns.km@gmail.com>
2021-05-16 18:37:29 -0500

    cleanup

--- services/db-arango/db.js
+++ services/db-arango/db.js
@@ -1,10 +1,21 @@
+// see https://www.arangodb.com/tutorials/tutorial-node-js/
+
 require('dotenv').config()
 
 const { Database } = require('arangojs')
 
+// connect to running arrangodb server
+// const db = new Database() // defaults to localhost:8529
+// const db = new Database(process.env.DB_HOST) // can pass string
 const db = new Database({ url: process.env.DB_HOST })
-db.createDatabase(process.env.DB_NAME) // will just use it if already created?
-db.database(process.env.DB_NAME)
+
+// create a database
+await db.createDatabase(process.env.DB_NAME) // okay if already created
+
+// use it
+db.database(process.env.DB_NAME) // replaces useDatabase - works immediately
+
+// db.database(process.env.DB_NAME)
 // db.useBasicAuth(process.env.DB_USERNAME, process.env.DB_PASSWORD)
 
 module.exports = db
