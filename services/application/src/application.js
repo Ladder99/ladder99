c4358e5032788682b1113f7841a1add96c89e54c
Brian Burns <bburns.km@gmail.com>
2021-05-20 13:38:22 -0500

    cleanup

--- services/application/src/application.js
+++ services/application/src/application.js
@@ -2,43 +2,48 @@
 // capture agent data and write to database
 
 import fetch from 'node-fetch'
-import pg from 'pg' // postgres driver
-const { Client } = pg
+// import pg from 'pg' // postgres driver
+// const { Client } = pg
+import { Client } from 'pg' // postgres driver
 import * as logic from './logic.js'
 
 console.log(`MTConnect Application starting`)
 console.log(`---------------------------------------------------`)
 
-const client = new Client()
-// @ts-ignore
-await client.connect() // uses envars PGHOST, PGPORT, etc - set with `source .env`
-// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-// console.log(res.rows[0].message) // Hello world!
-// await client.end()
-
 const baseUrl = process.env.AGENT_BASE_URL || 'http://localhost:5000'
 const interval = Number(process.env.INTERVAL || 2000) // msec
 
-setInterval(shovel, interval)
+;(async function () {
+  // get postgres connection
+  const client = new Client()
+  await client.connect() // uses envars PGHOST, PGPORT, etc
 
-async function shovel() {
+  // test
+  // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  // console.log(res.rows[0].message) // Hello world!
+
+  // poll the agent
+  setInterval(() => shovel(client), interval)
+})()
+
+async function shovel(client) {
   const url = `${baseUrl}/current`
   // const from = 1
   // const count = 200
   // const url = `${baseUrl}/sample?from=${from}&count=${count}`
+
   try {
     // console.log(`fetch from ${url}...`)
     const response = await fetch(url, {
       method: 'GET',
       headers: { Accept: 'application/json' },
     })
-    const tree = await response.json()
+    const json = await response.json()
 
-    // traverse the tree and output state
-    logic.traverse(tree, async dataItems => {
+    // traverse the json tree and output state
+    logic.traverse(json, async dataItems => {
       if (dataItems[0].type === 'Execution') {
-        // console.log(dataItems[0])
-        const dataItem = dataItems[0] //. just one?
+        const dataItem = dataItems[0] //. just one for /current
         console.log(dataItem.value)
         // dump value to db
         //. add try block
