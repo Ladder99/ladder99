1f57a86dc4cb83a56be84f6c63c5f4c69fc17bd3
Brian Burns <bburns.km@gmail.com>
2021-06-02 12:30:30 -0500

    wip replay nodejs

--- services/replay/src/index.js
+++ services/replay/src/index.js
@@ -1,31 +1,33 @@
 // replay
-// replays recorded mqtt messages to mqtt broker
+// replays MQTT recordings made with rpdswtk/mqtt_recorder, a python app
 
 const mqttlib = require('mqtt')
 
 const host = process.env.MQTT_HOST || 'localhost'
 const port = Number(process.env.MQTT_PORT || 1883)
 const deviceId = process.env.DEVICE_ID // eg 'ccs-pa-001'
-const messagesFile = process.env.MESSAGES_FILE
-const messageDelay = Number(process.env.MESSAGE_DELAY || 1000)
+const model = process.env.MODEL // eg 'ccs-pa'
+const modelsFolder = process.env.MODELS_FOLDER || '/etc/models'
+// const messageDelay = Number(process.env.MESSAGE_DELAY || 1000)
 const loop = Boolean(process.env.LOOP || false)
 const loopDelay = Number(process.env.LOOP_DELAY || 3000)
 
-const clientId = deviceId || 'simulator-' + Math.random()
-const config = { host, port, clientId }
+// const messages = require(messagesFile)
+// console.log({ messages })
 
-const messages = require(messagesFile)
-console.log({ messages })
-
-console.log(`Simulator-mqtt`)
+console.log(`Replay`)
 console.log(`Simulates a device sending MQTT messages.`)
 console.log(`------------------------------------------------------------`)
 
+const clientId = deviceId // || 'simulator-' + Math.random()
+const config = { host, port, clientId }
 console.log(`Connecting to MQTT broker on`, config)
 const mqtt = mqttlib.connect(config)
 
 mqtt.on('connect', async function onConnect() {
+  const folder = `${modelsFolder}/${model}/simulations`
   console.log(`Publishing messages...`)
+
   do {
     for (const message of messages) {
       const topic = message.topic.replace('${deviceId}', deviceId)
