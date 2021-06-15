837368553fc2f142f0b56e132224345c5bf97b03
Brian Burns <bburns.km@gmail.com>
2021-06-15 02:39:30 -0500

    wip playback

--- services/tapedeck/src/tapedeck.js
+++ services/tapedeck/src/tapedeck.js
@@ -16,38 +16,42 @@ const port = Number(process.env.PORT || 1883)
 // const modelsFolder = process.env.MODELS_FOLDER || '/etc/models'
 // const file = process.env.FILE // eg '
 //. this needs to be a fixed folder? ie for docker volume?
-const folder = process.env.FOLDER || '/etc/tapedeck' // eg '/Users/bburns/Desktop/tapedeck'
+// const folder = process.env.FOLDER || '/etc/tapedeck' // eg '/Users/bburns/Desktop/tapedeck'
+const folder = '/etc/tapedeck'
 const loop = Boolean(process.env.LOOP || false)
 
 console.log(`Tapedeck`)
 console.log(`Play/record MQTT messages`)
 console.log(`------------------------------------------------------------`)
 
-const clientId = deviceId
+const columns = 'topic,payload,qos,retain,time_now,time_delta'.split(',')
+
+const clientId = `tapedeck-${Math.random()}`
 const config = { host, port, clientId }
+
 console.log(`Connecting to MQTT broker on`, config)
 const mqtt = mqttlib.connect(config)
 
 mqtt.on('connect', async function onConnect() {
-  const simulationsFolder = `${modelsFolder}/${model}/simulations`
+  console.log(`Connected...`)
+
+  // const simulationsFolder = `${modelsFolder}/${model}/simulations`
   const csvfiles = fs
-    .readdirSync(simulationsFolder)
+    .readdirSync(folder)
     .filter(csvfile => csvfile.endsWith('.csv'))
     .sort()
-  const columns = 'topic,payload,qos,retain,time_now,time_delta'.split(',')
-
-  console.log(`Connected - publishing messages...`)
 
   // do while loop
   do {
     for (const csvfile of csvfiles) {
-      const csvpath = `${simulationsFolder}/${csvfile}`
+      const csvpath = `${folder}/${csvfile}`
       console.log(`Reading ${csvpath}...`)
       const csv = fs.readFileSync(csvpath)
       const rows = parse(csv, { columns })
       for (const row of rows) {
         const { payload, qos, time_delta } = row
-        const topic = row.topic.replace('${deviceId}', deviceId)
+        // const topic = row.topic.replace('${deviceId}', deviceId)
+        const topic = row.topic
         console.log(`Publishing topic ${topic}: ${payload.slice(0, 40)}...`)
         mqtt.publish(topic, payload)
         wait(time_delta * 1000)
