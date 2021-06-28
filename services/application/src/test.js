20fef58d4d2ed2446b0093ddc243739fd9887239
Brian Burns <bburns.km@gmail.com>
2021-06-27 22:09:39 -0500

    wip testing

--- services/application/src/test.js
+++ services/application/src/test.js
@@ -6,9 +6,14 @@ const path = 'services/application/examples/mazak/5717sm.xml'
 const xml = fs.readFileSync(path).toString()
 const json = JSON.parse(convert.xml2json(xml, { compact: true }))
 // console.log(json)
-traverse(json)
+libapp.print(json)
+exit()
+traverse(json, callback)
+function callback(obj) {
+  console.log(obj)
+}
 
-function traverse(node, callback) {
+function traverse(node, callback, parentTag = null) {
   if (libapp.isObject(node)) {
     const keys = Object.keys(node)
     for (const key of keys) {
@@ -16,10 +21,11 @@ function traverse(node, callback) {
       if (key === '_declaration') {
         continue
       } else if (key === '_attributes') {
-        const obj = { tag: key, ...node }
-        console.log(obj)
+        const obj = { tag: parentTag, ...value }
+        callback(obj)
+        // console.log(obj)
       } else {
-        traverse(value, callback)
+        traverse(value, callback, key)
       }
     }
   } else if (Array.isArray(node)) {
