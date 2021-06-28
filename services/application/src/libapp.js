c747f1b3c87b50d2cc4348a6b007ec12c701ecd5
Brian Burns <bburns.km@gmail.com>
2021-06-28 14:49:47 -0500

    node path works

--- services/application/src/libapp.js
+++ services/application/src/libapp.js
@@ -28,36 +28,40 @@ export function traverse(
   node,
   nodes,
   edges,
-  parentTag = 'root',
+  parentTag = '',
   parentKey = '',
-  path = ''
+  parents = []
 ) {
   if (isObject(node)) {
     const keys = Object.keys(node)
     // assign random key to each node, use to define edges?
     const _key = crypto.randomBytes(8).toString('hex')
-    let obj = { tag: parentTag, _key }
+    let obj = { tag: parentTag, _key, parents }
     for (const key of keys) {
       const value = node[key]
       if (key === '_declaration') {
       } else if (key === '_attributes') {
-        obj = { ...obj, ...value, path }
+        obj = { ...obj, ...value, parents }
       } else if (key === '_text') {
-        obj = { ...obj, value, path }
+        obj = { ...obj, value, parents }
       } else {
-        // const gparentId =
-        traverse(value, nodes, edges, key, _key, path + '/' + key)
+        const newparents = [...parents, obj]
+        traverse(value, nodes, edges, key, _key, newparents)
       }
     }
+    obj.path = obj.parents.map(node => node.id || node.tag).join('/')
+    delete obj.parents
     nodes.push(obj)
     if (parentKey) {
       edges.push({ _from: parentKey, _to: _key })
     }
   } else if (Array.isArray(node)) {
-    // const gparentId = node.parent ? node.parent._attributes.id : ''
+    // const obj = { id: 'lkm' }
+    const gparent = parents[parents.length - 2]
+    const obj = { tag: gparent.tag }
+    const newparents = [...parents, obj]
     for (const el of node) {
-      // traverse(el, nodes, edges, parentTag, parentKey, path + '/' + 'lkmlkm')
-      traverse(el, nodes, edges, parentTag, parentKey, path + `(id=pok)`)
+      traverse(el, nodes, edges, parentTag, parentKey, newparents)
     }
   } else {
     console.log('>>what is this?', { node })
