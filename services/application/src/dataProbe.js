d37025a627ba27998a7f9700811c6db621dcef4b
Brian Burns <bburns.km@gmail.com>
2021-06-29 18:44:38 -0500

    cmts

--- services/application/src/dataProbe.js
+++ services/application/src/dataProbe.js
@@ -7,21 +7,17 @@ import * as libapp from './libapp.js'
 export class Probe extends Data {
   type = 'probe'
 
-  // async read(endpoint) {
-  //   this.json = await endpoint.fetchJson('probe')
-  //   this.parseHeader()
-  // }
-
   async write(db) {
     console.log(this.json)
     const nodes = []
     const edges = []
     libapp.traverse(this.json, nodes, edges)
+
     console.log(nodes)
-    // console.log(nodes.slice(0, 20))
-    // const nodesPath = '/Users/bburns/Desktop/nodes.json'
+
     const nodesPath = 'nodes.json'
     fs.writeFileSync(nodesPath, JSON.stringify(nodes, null, 2))
+    process.exit(0)
 
     // const probeGraph = data.getProbeGraph() // get probe data into graph structure
     // libapp.print(probeGraph)
