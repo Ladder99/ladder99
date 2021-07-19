0052b2379d6177ce8bedbb21cee38e558ff5628a
Brian Burns <bburns.km@gmail.com>
2021-07-19 03:32:42 -0500

    wip

--- pipeline/adapter/src/plugins/test.js
+++ pipeline/adapter/src/plugins/test.js
@@ -9,6 +9,12 @@ export class AdapterPlugin {
     // or - idea was that diff devices could share a common cache for calcs?
     const receivedTime = new Date()
     // const payload = JSON.parse(message)
-    const payload = { foo: 'bar' }
+    const payload = { connection: 'online' }
+    // write to cache
+    const key = 'connection'
+    const item = lookup($, part)
+    const cacheId = deviceId + '-' + key // eg 'pa1-fault_count'
+    // item.receivedTime = receivedTime
+    cache.set(cacheId, item) // save to the cache - may send shdr to tcp
   }
 }
