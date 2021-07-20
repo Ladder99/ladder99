5dd89b4ac325915446dd495c6fd0a945e15fefd5
Brian Burns <bburns.km@gmail.com>
2021-07-20 00:09:55 -0500

    wip

--- pipeline/adapter/src/plugins/test.js
+++ pipeline/adapter/src/plugins/test.js
@@ -1,20 +1,21 @@
 // test plugin
 
-import fetch from 'node-fetch'
+// import fetch from 'node-fetch'
 
 export class AdapterPlugin {
   init({ deviceId, host, port, cache, inputs }) {
     console.log('init test plugin', { deviceId })
-    //. poll the device, send data directly to agent as shdr?
-    // or - idea was that diff devices could share a common cache for calcs?
+    //. poll device, send data directly to agent as shdr
     const receivedTime = new Date()
     // const payload = JSON.parse(message)
     const payload = { connection: 'online' }
+
     // write to cache
-    const key = 'connection'
-    const item = lookup($, part)
-    const cacheId = deviceId + '-' + key // eg 'pa1-fault_count'
-    // item.receivedTime = receivedTime
-    cache.set(cacheId, item) // save to the cache - may send shdr to tcp
+    // //. but cache must have calcs defined for the diff keys eh?
+    // const key = 'connection'
+    // const item = lookup($, part)
+    // const cacheId = deviceId + '-' + key // eg 'pa1-fault_count'
+    // // item.receivedTime = receivedTime
+    // cache.set(cacheId, item) // save to the cache - may send shdr to tcp
   }
 }
