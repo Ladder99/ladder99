3e025251093e671862a56066d999cd523f66d107
Brian Burns <bburns.km@gmail.com>
2021-07-20 09:59:57 -0500

    camera plugin sim works
    sh/pipeline start demo adapter agent play

--- pipeline/adapter/src/plugins/test.js
+++ pipeline/adapter/src/plugins/test.js
@@ -6,30 +6,31 @@ export class AdapterPlugin {
   init({ deviceId, protocol, host, port, cache, inputs, socket }) {
     const url = `${protocol}://${host}:${port}` // eg http://play:8080
     console.log('init test plugin', { deviceId, url })
+
     const timer = setInterval(poll, 1000)
 
-    //. hmm, if connection broke, this would start piling up awaiting fn calls...
+    //. poll device, send data directly to agent as shdr
+    //. hmm, if connection broke, this would start piling up awaiting fn calls -
+    //  how avoid that?
     async function poll() {
       try {
         const response = await fetch(url)
         const text = await response.text()
         const json = JSON.parse(text)
         console.log(json)
+        const value = json.status === 'online' ? 'AVAILABLE' : 'UNAVAILABLE'
+        const timestamp = new Date().toISOString() //. get from json
+        const shdr = `${timestamp}|connection|${value}`
+        console.log(shdr)
+        socket.write(shdr + '\n')
       } catch (error) {
         if (error.code === 'ECONNREFUSED') {
-          //. ignore - will try again
+          // ignore - will try again
         } else {
           throw error
         }
       }
     }
-
-    // //. poll device, send data directly to agent as shdr
-    // const timestamp = new Date()
-    // // const payload = JSON.parse(message)
-    // // const payload = { connection: 'online' }
-    // const shdr = `${timestamp}|connection|AVAILABLE`
-    // socket.write(shdr + '\n')
 
     // write to cache
     // //. but cache must have calcs defined for the diff keys eh?
