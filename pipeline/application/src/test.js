0838b679d071bc486a36d6bcc8894cc4a5fa8df8
Brian Burns <bburns.km@gmail.com>
2021-07-22 10:42:07 -0500

    test works - grabs operator prop

--- pipeline/application/src/test.js
+++ pipeline/application/src/test.js
@@ -6,13 +6,25 @@ import fs from 'fs' // node lib - filesystem
 import convert from 'xml-js' // https://github.com/nashwaan/xml-js
 import * as tree from './tree.js'
 
-// const path = 'examples/mazak/probe5717sm.xml'
-const path = 'examples/mazak/current5717.xml'
-const xml = fs.readFileSync(path).toString()
-const json = JSON.parse(convert.xml2json(xml, { compact: true }))
-// const objs = tree.getProbeObjects(json)
-const objs = tree.getObservationObjects(json)
-console.log(objs)
+const json = getJson('examples/mazak/probe5717sm.xml')
+const objs = tree.getProbeObjects(json)
+// console.log(objs)
+
+const propsById = {}
+for (const obj of objs) {
+  propsById[obj.id] = obj
+}
+console.log(propsById.operator)
+
+// const json2 = getJson('examples/mazak/current5717.xml')
+// const objs2 = tree.getObservationObjects(json2)
+// console.log(objs2)
 
 // const nodesFile = 'foo.json'
 // fs.writeFileSync(nodesFile, JSON.stringify(json, null, 2))
+
+function getJson(path) {
+  const xml = fs.readFileSync(path).toString()
+  const json = JSON.parse(convert.xml2json(xml, { compact: true }))
+  return json
+}
