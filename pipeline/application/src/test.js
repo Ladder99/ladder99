3a4ac5c062a35df9ba4fc5e401b651420a38fe4e
Brian Burns <bburns.km@gmail.com>
2021-07-23 23:50:54 -0500

    yaml

--- pipeline/application/src/test.js
+++ pipeline/application/src/test.js
@@ -13,10 +13,10 @@ console.log('------------------------------------------------')
 console.log()
 
 // load and parse probe xml
-const json = getJson('examples/mazak/probe5717.xml')
-// const json = getJson('examples/ccs-pa/probe.xml')
 // const json = getJson('examples/demo/devices.xml')
 // const json = getJson('examples/vmc/probe.xml')
+const json = getJson('examples/ccs-pa/probe.xml')
+// const json = getJson('examples/mazak/probe5717.xml')
 const objs = tree.getProbeObjects(json)
 // console.log(objs)
 // console.log(objs.map(obj => `${obj.signature}: ${obj.id}`).join('\n'))
