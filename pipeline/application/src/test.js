a359ef6d7bb1dbbd7cf7c73434d52d46b6fc7dd8
Brian Burns <bburns.km@gmail.com>
2021-07-24 21:33:04 -0500

    wip

--- pipeline/application/src/test.js
+++ pipeline/application/src/test.js
@@ -19,33 +19,32 @@ const json = getJson('examples/ccs-pa/probe.xml')
 // const json = getJson('examples/mazak/probe5717.xml')
 
 const objs = tree.getObjects(json)
-console.log(objs)
-console.log(objs.map(obj => `${obj.id}`).join('\n'))
-// process.exit(0)
-
-// const yaml = common.importYaml('./src/canonical.yaml')
-// console.log(yaml)
-
-const nodes = tree.getNodes(objs)
-
-// const dict = tree.getProbeDict(json)
-// console.log(dict)
-const propdefs = tree.getPropertyDefs(nodes)
-console.log(propdefs)
-
-// const nodes = Object.values(dict)
-// console.log(nodes)
-// console.log(nodes.map(node => node.path))
-
-// show a propdef
-// console.log(dict.message)
-
-//. add devices and propdefs to db
-
-// for (let device of Object.values(devices)) {
-//   device.node_id = db.add(device)
+console.log(objs[1]) // {
+//   tag: 'DataItem',
+//   id: 'pa1-connection',
+//   category: 'EVENT',
+//   type: 'AVAILABILITY',
+//   steps: [ '', 'availability' ]
 // }
 
+//. these will get added to dict of id -> path, yes? or to node?
+const nodes = tree.getNodes(objs)
+console.log(nodes[1]) // { id: 'pa1-connection', type: 'DataItem', path: 'availability' }
+const d = {}
+nodes.forEach(node => (d[node.id] = node.path))
+// console.log(d)
+
+//. these will get added to db nodes table, and node_id added to them
+//. also keep dict of path -> node_id
+const propdefs = tree.getPropertyDefs(nodes)
+propdefs.forEach((propdef, i) => (propdef.node_id = i + 1))
+console.log(propdefs[0]) // { type: 'PropertyDef', path: 'availability' }
+const d2 = {}
+propdefs.forEach(propdef => (d2[propdef.path] = propdef.node_id))
+// console.log(d2)
+
+nodes.forEach(node => (d[node.id] = d2[node.path]))
+console.log(d)
 process.exit(0)
 
 // // load and parse current xml
@@ -67,3 +66,6 @@ function getJson(path) {
   const json = JSON.parse(convert.xml2json(xml, { compact: true }))
   return json
 }
+
+// const yaml = common.importYaml('./src/canonical.yaml')
+// console.log(yaml)
