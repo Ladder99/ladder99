32fc351baac4f731e78b80248042bad52ed3cf34
Brian Burns <bburns.km@gmail.com>
2021-07-24 21:51:49 -0500

    cmt

--- pipeline/application/src/test.js
+++ pipeline/application/src/test.js
@@ -18,36 +18,31 @@ console.log()
 const json = getJson('examples/ccs-pa/probe.xml')
 // const json = getJson('examples/mazak/probe5717.xml')
 
-// const objs = tree.getObjects(json)
-// console.log(objs[1]) // {
-//   tag: 'DataItem',
-//   id: 'pa1-connection',
-//   category: 'EVENT',
-//   type: 'AVAILABILITY',
-//   steps: [ '', 'availability' ]
-// }
+//------------------------------------------------------------------------
 
+// get objects (devices, dataitems)
 const foos = tree.getFoos(json)
 console.log(foos[1]) // { id: 'pa1-connection', type: 'DataItem', path: 'availability' }
-const dataItemIdToNodeId = {}
 
-// const devices = tree.getDevices(nodes)
-
-//. these will get added to db nodes table, and node_id added to them
-//. also keep dict of path -> node_id
-// const propdefs = tree.getPropertyDefs(nodes)
-// const { devices, propdefs } = tree.getNodes(foos)
+// get nodes (devices, unique propdefs)
+// these will get added to db nodes table, and node_id added to them
+// also keep dict of path -> node_id
 const nodes = tree.getNodes(foos)
 
-nodes.forEach((node, i) => (node.node_id = i + 1)) // emulate db add/get
-console.log(nodes[0]) // { type: 'PropertyDef', path: 'availability' }
+// simulate db add/get
+nodes.forEach((node, i) => (node.node_id = i + 1))
+console.log(nodes[1]) // { type: 'PropertyDef', path: 'availability', node_id: 1 }
+
+// make map from propdef path to node_id
 const propdefPathToNodeId = {}
 nodes.forEach(node => (propdefPathToNodeId[node.path] = node.node_id))
 
+// make map from current/sample dataitem id to propdef node_id
+const dataItemIdToNodeId = {}
 nodes.forEach(
   node => (dataItemIdToNodeId[node.id] = propdefPathToNodeId[node.path])
 )
-console.log(dataItemIdToNodeId) // maps current/sample id to propdef node_id
+console.log(dataItemIdToNodeId)
 
 process.exit(0)
 
