ae22d557d665c61eceea051dcbe40dccb82a261c
Brian Burns <bburns.km@gmail.com>
2021-07-30 11:36:56 -0500

    cleanup

--- pipeline/adapter/src/drivers/cpc.js
+++ pipeline/adapter/src/drivers/cpc.js
@@ -9,18 +9,7 @@ export class AdapterDriver {
       console.log('CPC driver connected...')
     })
     client.on('error', function (error) {
-      console.log('handled error')
       console.log(error)
     })
-
-    // // handle tcp connection from cpc
-    // tcp.on('connection', async socket => {
-    //   const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
-    //   console.log('CPC driver new client connection from', remoteAddress)
-    // })
-
-    // // start tcp connection for this device
-    // console.log(`CPC driver try listening to socket at`, { host, port }, `...`)
-    // tcp.listen(port, host) // eg 10.20.30.101:9999
   }
 }
