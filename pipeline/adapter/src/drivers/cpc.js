3e8a77cdc8442db116c66a64745abcc99b6620f8
Brian Burns <bburns.km@gmail.com>
2021-07-30 11:01:24 -0500

    oh use net.connect

--- pipeline/adapter/src/drivers/cpc.js
+++ pipeline/adapter/src/drivers/cpc.js
@@ -4,16 +4,21 @@ import net from 'net' // node lib for tcp
 
 export class AdapterDriver {
   init({ deviceId, protocol, host, port, cache, inputs, socket }) {
-    const tcp = net.createServer()
-
-    // handle tcp connection from cpc
-    tcp.on('connection', async socket => {
-      const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
-      console.log('CPC driver new client connection from', remoteAddress)
+    // const tcp = net.createServer()
+    const options = { host, port }
+    console.log(`CPC driver trying to connect to TCP server at`, options, '...')
+    const client = net.connect(options, () => {
+      console.log('connected to tcp server')
     })
 
-    // start tcp connection for this device
-    console.log(`CPC driver try listening to socket at`, { host, port }, `...`)
-    tcp.listen(port, host) // eg 10.20.30.101:9999
+    // // handle tcp connection from cpc
+    // tcp.on('connection', async socket => {
+    //   const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
+    //   console.log('CPC driver new client connection from', remoteAddress)
+    // })
+
+    // // start tcp connection for this device
+    // console.log(`CPC driver try listening to socket at`, { host, port }, `...`)
+    // tcp.listen(port, host) // eg 10.20.30.101:9999
   }
 }
