52d05826ef8ec5eacee8d2132c925d2e7ef1ec2d
Brian Burns <bburns.km@gmail.com>
2021-07-30 04:14:03 -0500

    wip

--- pipeline/adapter/src/drivers/cpc.js
+++ pipeline/adapter/src/drivers/cpc.js
@@ -6,14 +6,15 @@ export class AdapterDriver {
   init({ deviceId, protocol, host, port, cache, inputs, socket }) {
     const tcp = net.createServer()
 
-    // handle tcp connection from cpc
-    tcp.on('connection', async socket => {
-      const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
-      console.log('CPC driver new client connection from', remoteAddress)
-    })
+    // // handle tcp connection from cpc
+    // tcp.on('connection', async socket => {
+    //   const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
+    //   console.log('CPC driver new client connection from', remoteAddress)
+    // })
 
-    // start tcp connection for this device
+    // // start tcp connection for this device
     console.log(`CPC driver try listening to socket at`, { host, port }, `...`)
-    tcp.listen(port, host) // eg 10.20.30.101:9999
+    // tcp.listen(port, host) // eg 10.20.30.101:9999
+    tcp.listen(port, host, () => console.log('listening')) // eg 10.20.30.101:9999
   }
 }
