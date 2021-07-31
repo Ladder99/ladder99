7afca6a9e9f545fd2c0cf177f5ea636caa0456b9
Brian Burns <bburns.km@gmail.com>
2021-07-30 21:26:43 -0500

    wip

--- pipeline/adapter/src/drivers/cpc.js
+++ pipeline/adapter/src/drivers/cpc.js
@@ -5,9 +5,7 @@ import net from 'net' // node lib for tcp
 export class AdapterDriver {
   init({ deviceId, protocol, host, port, cache, inputs, socket }) {
     console.log(`CPC driver connecting to TCP server at`, { host, port }, '...')
-    const client = net.connect(port, host, () => {
-      console.log('CPC driver connected...')
-    })
+    const client = net.connect(port, host)
     client.on('error', error => {
       console.log(error)
     })
@@ -15,12 +13,10 @@ export class AdapterDriver {
       console.log(data.toString())
     })
     client.on('connect', () => {
-      console.log(`CPC connected 2`)
-      client.write(
-        Buffer.from(
-          `PathListGet:ReadValues:.Autoclave.Inputs.AIRTC\\Value,.Autoclave.RecipeProcessor\\RunStatus`
-        )
-      )
+      console.log(`CPC driver connected...`)
+      const cmd = `PathListGet:ReadValues:.Autoclave.Inputs.AIRTC\\Value,.Autoclave.RecipeProcessor\\RunStatus\n`
+      console.log(`CPC driver writing ${cmd}...`)
+      client.write(Buffer.from(cmd))
     })
   }
 }
