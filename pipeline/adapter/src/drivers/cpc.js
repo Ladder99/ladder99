c66e13b5ef2b26bf101ffbeee13b22be8d1624b4
Brian Burns <bburns.km@gmail.com>
2021-07-30 22:01:04 -0500

    cmt

--- pipeline/adapter/src/drivers/cpc.js
+++ pipeline/adapter/src/drivers/cpc.js
@@ -6,16 +6,20 @@ export class AdapterDriver {
   init({ deviceId, protocol, host, port, cache, inputs, socket }) {
     console.log(`CPC driver connecting to TCP server at`, { host, port }, '...')
     const client = net.connect(port, host)
+
     client.on('error', error => {
       console.log(error)
     })
+
     client.on('data', data => {
       const str = data.toString()
       console.log(`CPC driver received ${str}...`)
     })
+
     client.on('end', () => {
       console.log('CPC driver disconnected from server...')
     })
+
     client.on('connect', () => {
       console.log(`CPC driver connected...`)
       // const cmd = `PathListGet:ReadValues:.Autoclave.Inputs.AIRTC\\Value,.Autoclave.RecipeProcessor\\RunStatus`
