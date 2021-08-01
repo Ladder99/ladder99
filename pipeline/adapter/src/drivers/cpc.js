4617ec1563574154b3b82374794776df9a78caaf
Brian Burns <bburns.km@gmail.com>
2021-08-01 05:18:27 -0500

    cleanup

--- pipeline/adapter/src/drivers/cpc.js
+++ pipeline/adapter/src/drivers/cpc.js
@@ -7,12 +7,11 @@ export class AdapterDriver {
     console.log(`Initialize cpc plugin...`)
 
     // get ids and query string
-    // eg 'PathListGet:ReadValues:.Autoclave.Alarms.ControlPower\Condition,.Autoclave.Alarms.ControlPower\AlarmActive,.Autoclave.Variables.OperatorName\Value,.Autoclave.Scripts.MotorHours.CoolPumpAOn\Value,.Autoclave.RecipeProcessor.Recipe.RecipeData\Description,.Autoclave.Inputs.AIRTC\Value'
     const ids = inputs.inputs.map(input => `${deviceId}-${input.key}`)
     const paths = inputs.inputs.map(input => input.path).join(',')
     const query = `PathListGet:ReadValues:${paths}`
-    console.log('ids', ids)
-    console.log('query', query)
+    console.log('ids', ids) // eg ['ac1-operator_name', 'ac1-recipe_description', ...]
+    console.log('query', query) // eg 'PathListGet:ReadValues:.Autoclave.Alarms.ControlPower\Condition,...'
 
     console.log(`CPC driver connecting to TCP server at`, { host, port }, '...')
     const client = net.connect(port, host)
@@ -21,17 +20,15 @@ export class AdapterDriver {
     client.on('connect', () => {
       console.log(`CPC driver connected...`)
       poll()
-      // setInterval(poll, 2000)
+      // setInterval(poll, 2000) //. do this
     })
 
     // receive data from device, write to cache, output shdr to agent
     client.on('data', data => {
-      // eg 'PathListGet:ReadValues:=,True,Joshau Schneider,254.280816,,0'
-      const str = data.toString()
+      const str = data.toString() // eg 'PathListGet:ReadValues:=,True,Joshau Schneider,254.280816,,0'
       console.log(`CPC driver received ${str}...`)
-      // get values eg ['', 'True', 'Joshau Schneider', ...]
       const [_, valuesStr] = str.split(':=')
-      const values = valuesStr.split(',')
+      const values = valuesStr.split(',') // eg ['', 'True', 'Joshau Schneider', ...]
       // write values to cache, which will output shdr to agent
       ids.forEach((id, i) => {
         const value = values[i]
