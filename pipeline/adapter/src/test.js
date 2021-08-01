17aecd3438f0ed4fe556c6868c1160579a88cf8e
Brian Burns <bburns.km@gmail.com>
2021-08-01 02:44:46 -0500

    wip testing

--- pipeline/adapter/src/test.js
+++ pipeline/adapter/src/test.js
@@ -5,6 +5,29 @@
 
 import { Cache } from './cache.js'
 import { AdapterDriver } from './drivers/cpc.js'
+import * as common from './common.js'
 
 console.log(Cache)
 console.log(AdapterDriver)
+
+const source = {
+  model: 'asc/econoclave',
+  driver: 'cpc', // type of adapter plugin - manages protocol and payload
+  protocol: 'tcp',
+  host: '10.20.30.101',
+  port: '9999',
+}
+
+console.log(source)
+
+async function main() {
+  const plugin = new AdapterDriver()
+  console.log(plugin)
+
+  const pathInputs = `../../models/asc/econoclave/inputs.yaml`
+  console.log(`Reading ${pathInputs}...`)
+  const inputs = common.importYaml(pathInputs) || {}
+  console.log(inputs)
+}
+
+main()
