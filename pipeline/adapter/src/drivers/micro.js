1b71fda970a6fa39fcda096559c50892021973a8
Brian Burns <bburns.km@gmail.com>
2021-08-11 05:07:46 -0500

    micro wip

--- pipeline/adapter/src/drivers/micro.js
+++ pipeline/adapter/src/drivers/micro.js
@@ -14,40 +14,31 @@ export class AdapterDriver {
 
     async function readData() {
       try {
-        // const data = await si.cpu()
-        // console.log('CPU Information:')
-        // console.log('- manufucturer: ' + data.manufacturer)
-        // console.log('- brand: ' + data.brand)
-        // console.log('- speed: ' + data.speed)
-        // console.log('- cores: ' + data.cores)
-        // console.log('- physical cores: ' + data.physicalCores)
-
-        // console.log(data)
-
         const data = await si.get({
-          cpu: 'manufacturer, brand, speed, cores',
+          // cpu: 'manufacturer, brand, speed, cores',
           cpuTemperature: 'main, cores',
           mem: 'total, free, used',
-          battery:
-            'hasBattery, currentCapacity, maxCapacity, capacityUnit, percent', // mWh
-          osInfo: 'platform, distro, release, codename, arch, hostname',
-          currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
-          disksIO: 'rIO, wIO',
-          fsSize: 'fs, type, size, available',
-          wifiInterfaces: 'id, model, vendor',
-          dockerContainers: 'name, createdAt, state',
+          // battery:
+          //   'hasBattery, currentCapacity, maxCapacity, capacityUnit, percent', // mWh
+          // osInfo: 'platform, distro, release, codename, arch, hostname',
+          // currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
+          // disksIO: 'rIO, wIO',
+          // fsSize: 'fs, type, size, available',
+          // wifiInterfaces: 'id, model, vendor',
+          // dockerContainers: 'name, createdAt, state',
         })
         console.log(data)
 
-        const str = JSON.stringify(data)
-        // cache.set(`${deviceId}-statistics`, { value: str })
-        cache.set(`${deviceId}-temperature`, {
-          value: data.cpuTemperature.main,
-        })
+        // get memory in DATA_SET format for shdr,
+        // eg "free=48237472 used=12387743 total=38828348"
         const memstr = Object.keys(data.mem)
           .map(key => `${key}=${data.mem[key]}`)
           .join(' ')
         cache.set(`${deviceId}-memory`, { value: memstr })
+
+        cache.set(`${deviceId}-temperature`, {
+          value: data.cpuTemperature.main,
+        })
 
         setAvailable()
       } catch (e) {
@@ -58,78 +49,14 @@ export class AdapterDriver {
 
     setInterval(readData, pollInterval)
 
-    // let reading = false
-    // let timer = null
-
-    // usb.on('attach', function (device) {
-    //   console.log('Dymo M10 attached')
-    //   setAvailable()
-    //   timer = setInterval(startReading, pollInterval)
-    // })
-
-    // usb.on('detach', function (device) {
-    //   if (
-    //     device.deviceDescriptor.idVendor === vendorId &&
-    //     device.deviceDescriptor.idProduct === productId
-    //   ) {
-    //     console.log('Dymo M10 detached')
-    //     reading = false
-    //     clearInterval(timer)
-    //     timer = null
-    //   }
-    // })
-
-    // function startReading() {
-    //   if (reading) return
-    //   try {
-    //     const device = new HID.HID(vendorId, productId)
-
-    //     console.log('Starting to read data from scale')
-    //     reading = true
-
-    //     device.on('data', function (data) {
-    //       const buffer = Buffer.from(data)
-    //       const mass = buffer[4] + 256 * buffer[5]
-
-    //       let grams = 0
-    //       if (buffer[2] === dataModeOunces) {
-    //         const scalingFactor = Math.pow(10, data[3] - 256)
-    //         const ounces = mass * scalingFactor
-    //         grams = ounces * 0.035274
-    //       } else if (buffer[2] === dataModeGrams) {
-    //         // grams = mass
-    //         grams = mass * 0.1 // chris says 10g was reporting as 0.1kg, so scale it
-    //       }
-
-    //       const kg = grams / 1000
-    //       cache.set(`${deviceId}-mass`, { value: kg })
-    //       setAvailable()
-    //     })
-
-    //     device.on('error', function (error) {
-    //       setUnavailable()
-    //       console.log(error)
-    //       reading = false
-    //       device.close()
-    //     })
-    //   } catch (err) {
-    //     setUnavailable()
-    //     if (/cannot open device/.test(err.message)) {
-    //       console.log('Dymo M10 cannot be found')
-    //     } else {
-    //       console.log(err)
-    //     }
-    //   }
-    // }
-
     function setAvailable() {
       cache.set(`${deviceId}-availability`, { value: 'AVAILABLE' })
     }
 
     function setUnavailable() {
       cache.set(`${deviceId}-availability`, { value: 'UNAVAILABLE' })
+      cache.set(`${deviceId}-memory`, { value: 'UNAVAILABLE' })
       cache.set(`${deviceId}-temperature`, { value: 'UNAVAILABLE' })
-      cache.set(`${deviceId}-statistics`, { value: 'UNAVAILABLE' })
     }
   }
 }
