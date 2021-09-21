d140792f932344ee73531076c391a344858b0bf7
Brian Burns <bburns.km@gmail.com>
2021-09-21 12:13:19 -0500

    wip zpl driver

--- pipeline/adapter/src/drivers/printer/zpl.js
+++ pipeline/adapter/src/drivers/printer/zpl.js
@@ -20,45 +20,48 @@ export class AdapterDriver {
     // console.log('query', query)
 
     console.log(`ZPL driver connecting to TCP server at`, { host, port }, '...')
-    //. do try catch
-    // const client = net.connect(port, host)
 
-    //. send ~HQ__ command to get printer status
+    try {
+      const client = net.connect(port, host)
 
-    // // connected to device - poll it for data by writing a command
-    // client.on('connect', () => {
-    //   console.log(`ZPL driver connected...`)
-    //   poll()
-    //   // setInterval(poll, 2000) //. do this
-    // })
+      // connected to device - poll it for data by writing a command
+      client.on('connect', () => {
+        console.log(`ZPL driver connected...`)
+        poll()
+        // setInterval(poll, 2000) //. do this
+      })
 
-    // // receive data from device, write to cache, output shdr to agent
-    // client.on('data', data => {
-    //   const str = data.toString() // eg 'PathListGet:ReadValues:=,True,Joshau Schneider,254.280816,,0'
-    //   console.log(`ZPL driver received ${str}...`)
-    //   const [_, valuesStr] = str.split(':=')
-    //   const values = valuesStr.split(',') // eg ['', 'True', 'Joshau Schneider', ...]
-    //   // write values to cache, which will output shdr to agent
-    //   ids.forEach((id, i) => {
-    //     const type = types[i] // eg 'boolean'
-    //     const typeFn = typeFns[type] // eg value => value==='True'
-    //     const value = typeFn(values[i]) // eg true
-    //     cache.set(id, { value }) // set cache value, which triggers shdr output
-    //   })
-    // })
+      // send ! U1 getvar "zpl.system_status" to get printer status
+      const cmd = `! U1 getvar "zpl.system_status"`
 
-    // client.on('error', error => {
-    //   console.log(error)
-    // })
-
-    // client.on('end', () => {
-    //   console.log('ZPL driver disconnected from server...')
-    // })
-
-    // // 'poll' device using tcp client.write
-    // function poll() {
-    //   console.log(`ZPL driver writing ${query}...`)
-    //   client.write(query + '\r\n')
-    // }
+      // receive data from device, write to cache, output shdr to agent
+      client.on('data', data => {
+        const str = data.toString() // eg '0,0,00000000,00000000,0,00000000,00000000'
+        console.log(`ZPL driver received ${str}...`)
+        // const [_, valuesStr] = str.split(':=')
+        // const values = valuesStr.split(',') // eg ['', 'True', 'Joshau Schneider', ...]
+        // // write values to cache, which will output shdr to agent
+        // ids.forEach((id, i) => {
+        //   const type = types[i] // eg 'boolean'
+        //   const typeFn = typeFns[type] // eg value => value==='True'
+        //   const value = typeFn(values[i]) // eg true
+        //   cache.set(id, { value }) // set cache value, which triggers shdr output
+        // })
+      })
+      client.on('error', error => {
+        console.log(error)
+      })
+      client.on('end', () => {
+        console.log('ZPL driver disconnected from server...')
+      })
+      // 'poll' device using tcp client.write
+      function poll() {
+        console.log(`ZPL driver writing ${cmd}...`)
+        client.write(cmd + '\r\n')
+      }
+    } catch (err) {
+      console.log(err)
+      //. keep trying eh?
+    }
   }
 }
