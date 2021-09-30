edf533a9facd27550bd617e3946e557dffcb7085
Brian Burns <bburns.km@gmail.com>
2021-09-29 21:37:06 -0500

    wip test

--- pipeline/adapter/src/drivers/helpers-test.js
+++ pipeline/adapter/src/drivers/helpers-test.js
@@ -1,8 +1,26 @@
 import { compile } from './helpers.js'
 
-const code = `msg('foo') + <bar>`
-const { js, refs } = compile(code, 'pr1-')
+const prefix = 'pr1-'
 
-console.log(code)
-console.log(js)
-console.log(refs)
+if (0) {
+  const code = `msg('foo') + <bar>`
+  const { js, refs } = compile(code, prefix)
+  console.log(code)
+  console.log(js)
+  console.log(refs)
+}
+
+{
+  const inputs = {
+    has_current_job: "=!!msg('%Z61.0')",
+    carton_quantity: '=(<job_meta> || {}).carton_quantity',
+  }
+  const k = {}
+  for (let [key, code] of Object.entries(inputs)) {
+    const { js, refs } = compile(code, prefix)
+    console.log(key)
+    console.log(code)
+    console.log(js)
+    console.log(refs)
+  }
+}
