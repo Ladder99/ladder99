635f6498792ee9ee311aaa0a0b4cb71c5d1ec6d0
Brian Burns <bburns.km@gmail.com>
2021-10-18 01:28:40 -0500

    wip

--- pipeline/relay/src/metrics3.js
+++ pipeline/relay/src/metrics3.js
@@ -1,8 +1,15 @@
 // track changes to dimensions and values as observations come in,
 // dump changes to database once a minute.
 
+// constants
+const secondsPerDay = 24 * 60 * 60
+const secondsPerHour = 60 * 60
+const millisecondsPerSecond = 1000
+const secondsPerMillisecond = 0.001
+const daysPerMillisecond = 1 / (secondsPerDay * 1000)
+
 // dump bins to db once a minute
-const timeout = 60 * 1000
+const writeToDbTimeout = 60 * millisecondsPerSecond
 
 //
 
@@ -17,13 +24,13 @@ export class Tracker {
   }
 
   // start the timer that dumps bins to the db
-  startTimer(timeout = 60 * 1000) {
-    console.log('startTimer', timeout)
-    this.timer = setInterval(this.handleTimer.bind(this), timeout)
+  startTimer(writeToDbTimeout = 60 * 1000) {
+    console.log('startTimer', writeToDbTimeout)
+    this.timer = setInterval(this.writeToDb.bind(this), writeToDbTimeout)
   }
 
   // handle a list of observations
-  // should be sorted by time first
+  // should be sorted by time
   handleObservations(observations) {
     console.log('handleObservations')
     this.observations = observations
@@ -76,15 +83,75 @@ export class Tracker {
     const { dimensionKey } = observation
   }
 
-  handleTimer() {
-    console.log('handleTimer - dump any bin adjustments to db')
+  writeToDb() {
+    console.log('writeToDb - dump any bin adjustments to db')
     //. do time_calendar also
     //. dump bins to db
     console.log('bins', this.bins)
-    // const sql = this.getSql()
+    const sql = this.getSql()
+    console.log(sql)
     // this.db.write(sql)
     // clear bins
     this.bins = {}
+  }
+
+  // get sql statements to write given accumulator bin data to db.
+  // accumulatorBins is like { device_id: bins }
+  //   with bins like { dimensions: accumulators }
+  //   dimensions are like "{operator:'Alice'}"
+  //   with accumulators like { time_active: 1, time_available: 2 }}
+  getSql(accumulatorBins) {
+    let sql = ''
+    //
+    // iterate over device+bins
+    // device_id is a db node_id, eg 3
+    // bins is a dict like { dimensions: accumulators }
+    for (let [device_id, bins] of Object.entries(accumulatorBins)) {
+      //
+      // iterate over dimensions+accumulators
+      // dimensions is eg '{"operator":"Alice", ...}' - ie gloms dimensions+values together
+      // accumulators is eg { time_active: 1, time_available: 2, ... },
+      //   ie all the accumulator slots and their time values in seconds.
+      for (let [dimensions, accumulators] of Object.entries(bins)) {
+        //
+        const accumulatorSlots = Object.keys(accumulators) // eg ['time_active', 'time_available']
+        if (accumulatorSlots.length === 0) continue // skip if no data
+
+        // split dimensions into dimensions+values and get associated time.
+        const dims = splitDimensionKey(dimensions) // eg to {operator: 'Alice'}
+        const seconds1970 = getHourInSeconds(dims) // rounded to hour, in seconds
+        if (!seconds1970) continue // skip if got NaN or something
+        const timestring = new Date(
+          seconds1970 * millisecondsPerSecond
+        ).toISOString() // eg '2021-10-15T11:00:00Z"
+
+        // iterate over accumulator slots, eg 'time_active', 'time_available'.
+        for (let accumulatorSlot of accumulatorSlots) {
+          // get total time accumulated for the slot
+          const timeDelta = accumulators[accumulatorSlot]
+          if (timeDelta > 0) {
+            // add values one at a time to existing db records.
+            // would be better to do all with one stmt somehow,
+            // but it's already complex enough.
+            // this is an upsert command pattern in postgres -
+            // try to add a new record - if key is already there,
+            // update existing record by adding timeDelta to the value.
+            sql += `
+INSERT INTO bins (device_id, time, dimensions, values)
+  VALUES (${device_id}, '${timestring}', 
+    '${dimensions}'::jsonb,
+    '{"${accumulatorSlot}":${timeDelta}}'::jsonb)
+ON CONFLICT (device_id, time, dimensions) DO
+  UPDATE SET
+    values = bins.values ||
+      jsonb_build_object('${accumulatorSlot}', 
+        (coalesce((bins.values->>'${accumulatorSlot}')::real, 0.0::real) + ${timeDelta}));
+  `
+          }
+        }
+      }
+    }
+    return sql
   }
 
   // add info to observations, incl time as hours1970.
@@ -123,12 +190,6 @@ export function getDimensionKey(observation, dimensionDefs) {
 export function splitDimensionKey(dimensionKey) {
   return JSON.parse(dimensionKey)
 }
-
-const secondsPerDay = 24 * 60 * 60
-const secondsPerHour = 60 * 60
-const secondsPerMillisecond = 0.001
-// const secondsPerMinute = 60
-const daysPerMillisecond = 1 / (secondsPerDay * 1000)
 
 // get day of year, 1-366
 // from stackoverflow
