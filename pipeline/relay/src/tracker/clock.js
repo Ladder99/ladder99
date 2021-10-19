8873f17554dc3c72101a2636c946d0070f915da5
Brian Burns <bburns.km@gmail.com>
2021-10-19 03:29:50 -0500

    wip fix key to name

--- pipeline/relay/src/tracker/clock.js
+++ pipeline/relay/src/tracker/clock.js
@@ -1,29 +1,29 @@
 export class Clock {
   constructor() {
-    this.startTimes = {} //. per device_id
+    this.startTimes = {} // per device_id and name
   }
 
   // start clock for the given observation
   start(observation) {
-    const { device_id, key } = observation
+    const { device_id, name } = observation
     if (this.startTimes[device_id] === undefined) {
       this.startTimes[device_id] = {}
     }
     // (add guard in case agent sends same value again)
-    if (this.startTimes[device_id][key] === undefined) {
-      this.startTimes[device_id][key] = observation.seconds1970
+    if (this.startTimes[device_id][name] === undefined) {
+      this.startTimes[device_id][name] = observation.seconds1970
     }
   }
 
   // stop clock for given observation, return time delta in seconds
   stop(observation) {
-    const { device_id, key } = observation
+    const { device_id, name } = observation
     let seconds
     if (this.startTimes[device_id] !== undefined) {
-      if (this.startTimes[device_id][key] !== undefined) {
-        seconds = observation.seconds1970 - this.startTimes[device_id][key]
+      if (this.startTimes[device_id][name] !== undefined) {
+        seconds = observation.seconds1970 - this.startTimes[device_id][name]
         // clear the clock
-        delete this.startTimes[device_id][key]
+        delete this.startTimes[device_id][name]
       }
     }
     return seconds
@@ -31,9 +31,9 @@ export class Clock {
 
   // clear clock for given observation
   clear(observation) {
-    const { device_id, key } = observation
+    const { device_id, name } = observation
     if (this.startTimes[device_id] !== undefined) {
-      delete this.startTimes[device_id][key]
+      delete this.startTimes[device_id][name]
     }
   }
 
