0ba42825e885266c2be14c4cbc257be643cee971
Brian Burns <bburns.km@gmail.com>
2021-10-20 09:22:17 -0500

    cmt

--- pipeline/relay/src/agentReader.js
+++ pipeline/relay/src/agentReader.js
@@ -69,8 +69,8 @@ export class AgentReader {
     // for metric calcs
     this.tracker = new tracker.Tracker(this.db, dimensionDefs, valueDefs)
     // this.tracker.startTimer(60) // start timer which dumps bins to db every interval secs
-    //. for testing
-    this.tracker.startTimer(5) // start timer which dumps bins to db every interval secs
+    //... for testing - write to db every 3 secs
+    this.tracker.startTimer(3) // start timer which dumps bins to db every interval secs
 
     // probe - get agent data structures and write to db
     probe: do {
