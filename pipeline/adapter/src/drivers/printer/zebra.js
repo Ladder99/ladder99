b00388ae2fd00e549a12996da05bb6cc46729cfc
Brian Burns <bburns.km@gmail.com>
2021-10-27 12:50:20 -0500

    wip zebra

--- pipeline/adapter/src/drivers/printer/zebra.js
+++ pipeline/adapter/src/drivers/printer/zebra.js
@@ -4,7 +4,11 @@ import net from 'net' // node lib for tcp - https://nodejs.org/api/net.html
 import * as parsers from './zebra-parsers.js'
 import * as lib from '../../lib.js'
 
-const pollInterval = 5000 // ms
+// want this low as possible - see below comment
+const pollInterval = 3000 // ms
+// each command needs some time to wait for printer to respond
+// and currently we have 4 commands, so this times that is total time to
+// loop through all commands. keep it under the pollInterval.
 const messagePauseTime = 500 // ms
 
 export class AdapterDriver {
@@ -46,16 +50,19 @@ export class AdapterDriver {
       }
     }
 
+    // each command handler should handle a null/undefined response
+    // by setting each dataitem it handles to undefined.
     const commandHandlers = {
       // host status - p212
-      // note: When a ~HS command is sent, the printer will not send a response
-      // to the host if the printer is in one of these conditions:
+      // manual says: When a ~HS command is sent, the printer will not send a
+      // response to the host if the printer is in one of these conditions:
       // MEDIA OUT, RIBBON OUT, HEAD OPEN, REWINDER FULL, HEAD OVER-TEMPERATURE
-      '~HS': str => {
-        if (str) {
+      // (but how accurate is the manual? eg some printers don't respond to HQES)
+      '~HS': response => {
+        if (response) {
           // get paper out flag, pause flag, buffer full flag, under/over temp flags,
           // head up flag, ribbon out flag, label waiting flag, labels remaining
-          const ret = parsers.parseHS(str)
+          const ret = parsers.parseHS(response)
           // @ts-ignore
           setCache('labels_remaining', ret.labelsRemaining)
           // execution state:
@@ -64,20 +71,21 @@ export class AdapterDriver {
           //   interrupted - if any error condition
           //   active - if labelsRemaining > 0
           //   ready - otherwise
-          // see also poll() fn
+          // note: poll() fn will overwrite this with interrupted if any errors found
           setCache('state', ret.labelsRemaining > 0 ? 'ACTIVE' : 'READY')
           // handle errors and warnings
           if (ret.errors) errors.push(...ret.errors)
           if (ret.warnings) warnings.push(...ret.warnings)
         } else {
+          setCache('labels_remaining')
           setCache('state')
         }
       },
 
       // host query status - errors and warnings - see guide p205
-      '~HQES': str => {
-        if (str) {
-          const ret = parsers.parseHQES(str)
+      '~HQES': response => {
+        if (response) {
+          const ret = parsers.parseHQES(response)
           // A valid value for a data item in the category CONDITION can be one
           // of Normal, Warning, or Fault.
           //. set multiple conditions? eg a warning AND an error? mtc allows that.
@@ -85,38 +93,23 @@ export class AdapterDriver {
           //. pass array of values here? let cache handle it?
           //. how handle multiple messages - eg some warnings, some faults?
           //. for now, just handle one condition value at a time
-          // if (ret.errors.length > 0) {
-          //   setCache('cond', 'FAULT')
-          //   // setCache('msg', ret.msgs)
-          //   setCache('state', 'INTERRUPTED') // execution state - see also HS handler below
-          // } else if (ret.warnings.length > 0) {
-          //   setCache('cond', 'WARNING')
-          //   // setCache('msg', ret.msgs)
-          // } else {
-          //   setCache('cond', 'NORMAL')
-          //   // setCache('msg')
-          // }
           // handle errors and warnings
           if (ret.errors) errors.push(...ret.errors)
           if (ret.warnings) warnings.push(...ret.warnings)
-        } else {
-          // set to unavailable
-          setCache('cond')
-          // setCache('msg')
         }
       },
 
       // head diagnostic - get head temp, darkness adjust (?) - p199
-      '~HD': str => {
-        const ret = str ? parsers.parseHD(str) : {}
+      '~HD': response => {
+        const ret = response ? parsers.parseHD(response) : {}
         setCache('ht', ret['Head Temp']) // Celsius
         setCache('dark', ret['Darkness Adjust']) // -30 to +30 or sthing
         setCache('fr', ret['Print Speed']) // feed rate - inches/sec
       },
 
       // host query odometer - nonresettable and user resettable 1 and 2
-      '~HQOD': str => {
-        const length = str ? parsers.parseHQOD(str) : undefined
+      '~HQOD': response => {
+        const length = response ? parsers.parseHQOD(response) : undefined
         setCache('tl', length) // total length, inches
       },
 
@@ -124,7 +117,7 @@ export class AdapterDriver {
       // MAINTENANCE ALERT MESSAGES
       // CLEAN: PLEASE CLEAN PRINT HEAD
       // REPLACE: PLEASE REPLACE PRINT HEAD
-      // '~HQMI': str => {},
+      // '~HQMI': response => {},
 
       // host query maintenance alert
       // ~HQMA
@@ -136,7 +129,7 @@ export class AdapterDriver {
       // PRINT REPLACEMENT ALERT: NO
       // PRINT CLEANING ALERT: NO
       // UNITS: C
-      // '~HQMA': str => {},
+      // '~HQMA': response => {},
     }
 
     // connected to device - poll it for data by writing a command
@@ -159,6 +152,7 @@ export class AdapterDriver {
       if (handler) {
         handler(str)
       }
+      //. make sure all are in consistent state here? or in poll fn?
     })
 
     client.on('error', error => {
@@ -180,37 +174,40 @@ export class AdapterDriver {
       // iterate over cmds, set handler temporarily
       const commands = Object.keys(commandHandlers)
       for (let command of commands) {
-        //. this is assuming that printer will respond to cmds in order - ehhhh
+        //. this is assuming that printer will respond to cmds in order... uhh
         handler = commandHandlers[command] // will be called by client.on('data'), above
         console.log(`Zebra driver writing ${command}...`)
         //. do try/catch - handle disconnection, reconnection
         client.write(command + '\r\n')
-        //. give printer some time to respond
+        // give printer a little time to respond
         await new Promise(resolve => setTimeout(resolve, messagePauseTime))
-      }
-      // get errors and warnings as one string
-      const errorMsgs = errors.map(error => `ERROR: ${error}`)
-      const warningMsgs = warnings.map(warning => `WARNING: ${warning}`)
-      const msgs = [...errorMsgs, ...warningMsgs].join(', ') || 'UNAVAILABLE'
-      setCache('msg', msgs)
-      // set condition and state
-      if (errors.length > 0) {
-        setCache('cond', 'FAULT')
-        setCache('state', 'INTERRUPTED') // execution state - see also HS handler
-      } else if (warnings.length > 0) {
-        setCache('cond', 'WARNING')
-      } else {
-        setCache('cond', 'NORMAL')
+
+        //. make sure all is in consistent state here
+        // get errors and warnings as one string
+        const errorMsgs = errors.map(error => `ERROR: ${error}`)
+        const warningMsgs = warnings.map(warning => `WARNING: ${warning}`)
+        const msgs = [...errorMsgs, ...warningMsgs].join(', ') || 'UNAVAILABLE'
+        setCache('msg', msgs)
+        // set condition and state
+        if (errors.length > 0) {
+          setCache('cond', 'FAULT')
+          setCache('state', 'INTERRUPTED') // execution state - see also HS handler
+        } else if (warnings.length > 0) {
+          setCache('cond', 'WARNING')
+        } else {
+          setCache('cond', 'NORMAL')
+        }
       }
     }
 
     // set all cache keys to UNAVAILABLE
     function setAllUnavailable() {
-      // call all the cmd handlers with no param
+      setCache('avail')
+      setCache('emp') // power
+      // call all the cmd handlers with no param -
+      // each should clear the dataitems it handles
       const handlers = Object.values(commandHandlers)
       handlers.forEach(handler => handler())
-      setCache('avail')
-      setCache('emp')
     }
 
     // set a cache key to the given value, which will trigger shdr output
