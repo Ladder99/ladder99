e939338ae6fee4c182757595142725a92c3cd13c
Brian Burns <bburns.km@gmail.com>
2021-10-28 10:45:55 -0500

    wip adapter refac

--- pipeline/adapter/src/helpers-test.js
+++ pipeline/adapter/src/helpers-test.js
@@ -1,5 +1,5 @@
 // test helper fns
-// run:
+// usage:
 //   cd pipeline/adapter
 //   node src/helpers-test.js
 
@@ -14,6 +14,7 @@ const inputs = {
         has_current_job: "!!$['%Z61.0']",
         job_meta: "msg('%Z61.0')",
         carton_quantity: '(<job_meta> || {}).carton_quantity',
+        job_complete: "msg('%Z61.22') === true",
       },
     },
   },
@@ -26,45 +27,52 @@ const deviceId = 'pr1'
 
 const prefix = deviceId + '-'
 
-// ~mqtt message payload
-const payload = [{ address: '%Z61.0', value: { carton_quantity: 5 } }]
+// ~mqtt message payloads
+const payloads = [
+  [{ address: '%Z61.0', value: { carton_quantity: 5 } }], // job meta
+  [{ address: '%Z61.22', value: true }], // job complete
+]
 
-// initialize $ dictionary
-const $ = {}
-payload.forEach(item => ($[item.address] = item))
+for (let payload of payloads) {
+  // //. amend payload?
+  // payload.push({ address: '%Z61.0', value: undefined })
 
-// compile inputs yaml
-const handler = inputs.handlers['l99/ccs/foo']
-const macros = getMacros(prefix, handler.accessor)
-// const maps = compileExpressions(handler.expressions, macros)
-const { augmentedExpressions, maps } = compileExpressions(
-  handler.expressions,
-  macros
-)
-console.log('augexprs', augmentedExpressions)
-console.log('maps', maps)
-handler.augmentedExpressions = augmentedExpressions
-handler.maps = maps
+  // initialize $ dictionary
+  // ie from initialize: 'payload.forEach(item => $[item.address] = item)'
+  const $ = {}
+  payload.forEach(item => ($[item.address] = item))
 
-// get set of keys for eqns we need to execute
-const equationKeys = getEquationKeys(payload, handler.maps)
-console.log('equationKeys', equationKeys)
+  // compile inputs yaml
+  const handler = inputs.handlers['l99/ccs/foo']
+  const macros = getMacros(prefix, handler.accessor)
+  const { augmentedExpressions, maps } = compileExpressions(
+    handler.expressions,
+    macros
+  )
+  // console.log('augexprs', augmentedExpressions)
+  // console.log('maps', maps)
+  handler.augmentedExpressions = augmentedExpressions
+  handler.maps = maps
 
-let keyvalues = {}
+  // get set of keys for eqns we need to execute
+  const equationKeys = getEquationKeys(payload, handler.maps)
+  // console.log('equationKeys', equationKeys)
 
-// iterate over set of eqnkeys and evaluate each
-for (let equationKey of equationKeys) {
-  // const input = handler.inputs[equationKey]
-  // const value = input.fn(cache, $)
-  // cache[equationKey] = value
-  const expression = handler.augmentedExpressions[equationKey]
-  const value = expression.fn(cache, $, keyvalues) // run the expression fn
-  if (value !== undefined) {
-    const cacheId = deviceId + '-' + equationKey // eg 'pa1-fault_count'
-    cache[equationKey] = value
-    // cache.set(cacheId, value) // save to the cache - may send shdr to tcp
-    // equationKeys2.add(cacheId)
+  let keyvalues = {}
+
+  // iterate over set of eqnkeys and evaluate each
+  for (let equationKey of equationKeys) {
+    const expression = handler.augmentedExpressions[equationKey]
+    const value = expression.fn(cache, $, keyvalues) // run the expression fn
+    if (value !== undefined) {
+      const cacheId = deviceId + '-' + equationKey // eg 'pa1-fault_count'
+      // cache.set(cacheId, value) // save to the cache - may send shdr to tcp
+      if (value !== undefined) {
+        cache[equationKey] = value
+      }
+      // equationKeys2.add(cacheId)
+    }
   }
-}
 
-console.log('cache', cache)
+  console.log('cache', cache)
+}
