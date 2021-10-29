eadde94fbefdc0138310d62d11bea862af417933
Brian Burns <bburns.km@gmail.com>
2021-10-29 05:12:44 -0500

    wip tester

--- pipeline/adapter/src/helpers-test.js
+++ pipeline/adapter/src/helpers-test.js
@@ -3,7 +3,6 @@
 //   cd pipeline/adapter
 //   node src/helpers-test.js
 
-// import { getMacros, compileExpressions, getEquationKeys } from './helpers.js'
 import * as helpers from './helpers.js'
 import * as lib from './lib.js'
 
@@ -16,17 +15,16 @@ const inputs = {
         has_current_job: "=!!$['%Z61.0']",
         job_meta: "msg('%Z61.0')",
         carton_quantity: '(<job_meta> || {}).carton_quantity',
-        job_complete: "msg('%Z61.22') === true",
+        job_complete: "=msg('%Z61.22') === true",
       },
     },
   },
 }
 
-// ~cache
+// ~cache class object
 const cache = {}
 
 const deviceId = 'pr1'
-
 const prefix = deviceId + '-'
 
 // ~mqtt message payloads
@@ -38,7 +36,7 @@ const payloads = [
   [{ address: '%Z61.22', value: true }], // job complete
 ]
 
-let last$ = {}
+// let last$ = {}
 
 for (let payload of payloads) {
   console.log('payload', payload)
@@ -55,14 +53,22 @@ for (let payload of payloads) {
     handler.expressions,
     macros
   )
-  // console.log('augexprs', augmentedExpressions)
-  // console.log('maps', maps)
   handler.augmentedExpressions = augmentedExpressions
   handler.maps = maps
+
+  // get set of exprs to always run
+  handler.alwaysRun = new Set()
+  for (let key of Object.keys(augmentedExpressions)) {
+    const expr = augmentedExpressions[key]
+    if (expr.always) {
+      handler.alwaysRun.add(key)
+    }
+  }
 
   // get set of keys for eqns we need to execute
   const equationKeys = helpers.getEquationKeys(payload, handler.maps)
   // const equationKeys = helpers.getEquationKeys1b(payload, last$, handler.maps)
+
   // make sure all '=' expressions will be evaluated
   lib.mergeIntoSet(equationKeys, handler.alwaysRun)
 
@@ -83,7 +89,7 @@ for (let payload of payloads) {
       // equationKeys2.add(cacheId)
     }
   }
-  last$ = { ...$ }
+  // last$ = { ...$ }
 
   console.log('cache', cache)
   console.log()
