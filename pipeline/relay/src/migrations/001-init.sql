65b76e43383bbfcd455ff0a4b61438c39cf77c6a
Brian Burns <bburns.km@gmail.com>
2021-11-21 03:50:05 -0600

    sql - add tablefunc extension

--- pipeline/relay/src/migrations/001-init.sql
+++ pipeline/relay/src/migrations/001-init.sql
@@ -8,8 +8,14 @@
 ---------------------------------------------------------------------
 -- add functions to postgres
 
--- timescale - makes hypertables for storing time-series data
+-- timescaledb - lets you make hypertables for storing time-series data
+-- see https://www.timescale.com/
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+
+-- tablefunc - makes crosstab function available
+-- see https://www.postgresql.org/docs/13/tablefunc.html
+-- and https://stackoverflow.com/questions/3002499/postgresql-crosstab-query
+CREATE EXTENSION IF NOT EXISTS tablefunc;
 
 
 ---------------------------------------------------------------------
