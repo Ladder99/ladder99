a93bb7f17a10fcc8b25c99638534efcfda7c7d4d
Brian Burns <bburns.km@gmail.com>
2021-12-19 06:06:22 -0600

    jobboss - polling loop

--- pipeline/adapter/src/drivers/erp/jobboss.js
+++ pipeline/adapter/src/drivers/erp/jobboss.js
@@ -1,9 +1,10 @@
 // jobboss driver
 
 import tedious from 'tedious' // mssql driver https://github.com/tediousjs/tedious
+import * as lib from '../../lib.js'
 
 export class AdapterDriver {
-  init({ deviceId, protocol, cache, inputs, socket, connection }) {
+  async init({ deviceId, protocol, cache, inputs, socket, connection }) {
     console.log(`JobBoss - initialize driver...`)
 
     setUnavailable()
@@ -35,12 +36,19 @@ export class AdapterDriver {
     })
     db.on('error', error => {
       console.log(error)
+      setUnavailable()
     })
     db.on('end', () => {
       console.log(`JobBoss - disconnected from database`)
       setUnavailable()
     })
     db.connect()
+
+    //. make loop to poll for current job info
+    while (true) {
+      console.log(`JobBoss - polling for job info...`)
+      await lib.sleep(5000)
+    }
 
     function setAvailable() {
       cache.set(`${deviceId}-availability`, 'AVAILABLE')
