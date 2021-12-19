788890f3769d9b25ece9ce2288a8467b9afb0f2e
Brian Burns <bburns.km@gmail.com>
2021-12-19 10:57:09 -0600

    try select qry

--- pipeline/adapter/src/drivers/erp/jobboss.js
+++ pipeline/adapter/src/drivers/erp/jobboss.js
@@ -1,7 +1,6 @@
 // jobboss driver
 
-// import tedious from 'tedious' // mssql driver https://github.com/tediousjs/tedious
-import mssql from 'mssql'
+import mssql from 'mssql' // ms sql server driver
 // import * as lib from '../../lib.js'
 
 export class AdapterDriver {
@@ -10,53 +9,14 @@ export class AdapterDriver {
 
     setUnavailable()
 
-    // const config = {
-    //   server: connection.server,
-    //   authentication: {
-    //     type: 'default',
-    //     options: {
-    //       userName: connection.username,
-    //       password: connection.password,
-    //     },
-    //   },
-    //   options: {
-    //     encrypt: false,
-    //     database: connection.database,
-    //     // cryptoCredentialsDetails: {
-    //     //   minVersion: 'TLSv1',
-    //     // },
-    //   },
-    // }
-
     const config = {
       server: connection.server,
+      database: connection.database,
       user: connection.user,
       password: connection.password,
-      database: connection.database,
-      // options: {
-      //   encrypt: false,
-      //   database: connection.database,
-      // },
     }
 
-    // const Connection = tedious.Connection
     console.log(`JobBoss - connecting to database...`, connection.server)
-    // const db = new Connection(config)
-    // db.on('connect', error => {
-    //   console.log('JobBoss - connected')
-    //   setAvailable()
-    //   //. poll for current job info
-    //   setInterval(poll, 5000)
-    // })
-    // db.on('error', error => {
-    //   console.log(error)
-    //   setUnavailable()
-    // })
-    // db.on('end', () => {
-    //   console.log(`JobBoss - disconnected from database`)
-    //   setUnavailable()
-    // })
-    // db.connect()
 
     mssql.connect(config, err => {
       console.log('JobBoss - connected')
@@ -65,14 +25,14 @@ export class AdapterDriver {
       setInterval(poll, 5000)
     })
 
-    // const sql = `select 42, 'hello world'`
-    const sql = `
-select top 10 opt.*
-from job_operation op
-join job_operation_time opt on op.job_operation = opt.job_operation
-where work_center = 'marumatsu'
-and opt.work_date between '2021-11-18' and '2021-11-19'
-`
+    const sql = `select 42, 'hello'`
+    //     const sql = `
+    // select top 10 opt.*
+    // from job_operation op
+    // join job_operation_time opt on op.job_operation = opt.job_operation
+    // where work_center = 'marumatsu'
+    // and opt.work_date between '2021-11-18' and '2021-11-19'
+    // `
 
     async function poll() {
       console.log(`JobBoss - polling for job info...`)
