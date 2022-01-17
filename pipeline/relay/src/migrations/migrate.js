8a795bf4591762e567875e4c86373d5851074fa0
Brian Burns <bburns.km@gmail.com>
2022-01-17 02:11:13 -0600

    cmt

--- pipeline/relay/src/migrations/migrate.js
+++ pipeline/relay/src/migrations/migrate.js
@@ -1,19 +1,18 @@
 import fs from 'fs' // node lib
 
 //. handle versions - use meta table
-//. move src/migrations elsewhere eventually
-//. move this code into migrations/migrate.js, call from relay
+//. move src/migrations elsewhere eventually?
 export async function migrate(db) {
   console.log(`Migrating database structures...`)
-  await readFile(db, `./001-extensions.sql`)
-  await readFile(db, `./002-base-tables.sql`)
-  await readFile(db, `./003-base-views.sql`)
-  await readFile(db, `./004-base-functions.sql`)
-  await readFile(db, `./005-get_timeline.sql`)
-  await readFile(db, `./006-get_jobs.sql`)
-  await readFile(db, `./007-get_rate.sql`)
-  await readFile(db, `./008-get_availability.sql`)
-  await readFile(db, `./009-metrics.sql`)
+  await readFile(db, `src/migrations/001-extensions.sql`)
+  await readFile(db, `src/migrations/002-base-tables.sql`)
+  await readFile(db, `src/migrations/003-base-views.sql`)
+  await readFile(db, `src/migrations/004-base-functions.sql`)
+  await readFile(db, `src/migrations/005-get_timeline.sql`)
+  await readFile(db, `src/migrations/006-get_jobs.sql`)
+  await readFile(db, `src/migrations/007-get_rate.sql`)
+  await readFile(db, `src/migrations/008-get_availability.sql`)
+  await readFile(db, `src/migrations/009-metrics.sql`)
 }
 
 async function readFile(db, filename) {
