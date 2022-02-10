c3248c65b7b98235f300e11989999e3729060529
Brian Burns <bburns.km@gmail.com>
2022-02-10 02:57:57 -0600

    refac cpc

--- services/adapter/src/drivers/autoclave/cpc.js
+++ services/adapter/src/drivers/autoclave/cpc.js
@@ -11,6 +11,10 @@
 
 import net from 'net' // node lib for tcp - https://nodejs.org/api/net.html
 
+const pollInterval = 2000 // msec
+
+// these functions transform values received from cpc depending on
+// the type specified in eg modules/autoclave/asc/inputs.yaml.
 const typeFns = {
   undefined: value => value,
   boolean: value => value === 'True',
@@ -18,17 +22,17 @@ const typeFns = {
 }
 
 export class AdapterDriver {
-  init({ deviceId, protocol, host, port, cache, inputs, socket }) {
-    console.log(`Initialize CPC driver...`)
+  init({ deviceId, host, port, cache, inputs }) {
+    console.log(`CPC Initialize driver...`)
     cache.set(`${deviceId}-avail`, 'UNAVAILABLE')
 
     // get ids and query string
     const ids = inputs.inputs.map(input => `${deviceId}-${input.key}`) // eg ['ac1-operator_name', 'ac1-recipe_description', ...]
-    const paths = inputs.inputs.map(input => input.path).join(',')
+    const paths = inputs.inputs.map(input => input.path).join(',') // the cpc property path string, eg '.Autoclave.Alarms.ControlPower...,...'
     const types = inputs.inputs.map(input => input.type) // eg [undefined, undefined, boolean, ...]
     const query = `PathListGet:ReadValues:${paths}` // eg 'PathListGet:ReadValues:.Autoclave.Alarms.ControlPower\Condition,...'
-    console.log('ids', ids)
-    console.log('query', query)
+    console.log('CPC ids', ids)
+    console.log('CPC query', query)
 
     console.log(`CPC driver connecting to TCP server at`, { host, port }, '...')
     const client = net.connect(port, host)
@@ -37,8 +41,8 @@ export class AdapterDriver {
     client.on('connect', () => {
       console.log(`CPC driver connected...`)
       cache.set(`${deviceId}-avail`, 'AVAILABLE')
-      poll()
-      setInterval(poll, 2000)
+      poll() // first poll
+      setInterval(poll, pollInterval) // subsequent polls
     })
 
     // receive data from device, write to cache, output shdr to agent
@@ -46,8 +50,8 @@ export class AdapterDriver {
       const str = data.toString() // eg 'PathListGet:ReadValues:=,True,Joshau Schneider,254.280816,,0'
       console.log(`CPC driver received ${str.slice(0, 255)}...`)
       const valuesStr = str.split(':=')[1]
-      // note: for the list of messages, it sends one after the other - after the
-      // first one it doesn't include the :=, so this will return null.
+      // note: for the list of messages, it sends one after the other -
+      // after the first one it doesn't include the :=, so this will return null.
       // so can ignore those, since we just want the first line.
       if (valuesStr) {
         const values = valuesStr.split(',') // eg ['', 'True', 'Joshau Schneider', ...]
@@ -69,7 +73,7 @@ export class AdapterDriver {
       console.log('CPC driver disconnected from server...')
     })
 
-    // 'poll' device using tcp client.write
+    // 'poll' device using tcp client.write - will receive data in 'data' events
     function poll() {
       console.log(`CPC driver writing ${query}...`)
       client.write(query + '\r\n')
