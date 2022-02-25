b3216b4dea60cafc72b73117292f55c56816be0d
Brian Burns <bburns.km@gmail.com>
2022-02-25 14:22:09 -0600

    get_count udf

---
+++ services/relay/src/migrations/010-get_count.sql
@@ -0,0 +1,42 @@
+create or replace function get_count(
+  in p_device text, -- the device name, eg 'Cutter'
+  in p_path text, -- the counter path, eg 'controller/partOccurrence/part_count-lifetime'
+  in p_start bigint, -- start time in milliseconds since 1970-01-01
+  in p_stop bigint -- stop time in milliseconds since 1970-01-01
+)
+-- returns table ("time" timestamptz, "count" float)
+returns float
+language plpgsql
+as
+$body$
+declare
+  v_start timestamptz := ms2timestamptz(p_start);
+  v_stop timestamptz := ms2timestamptz(p_stop);
+  v_count0 float;
+  v_count1 float;
+begin
+  v_count0 := (
+    select value from history_float 
+    where device=p_device and path=p_path and time < v_start
+    order by time desc
+    limit 1
+  );
+  v_count1 := (
+    select value from history_float 
+    where device=p_device and path=p_path and time >= v_start
+    order by time asc
+    limit 1
+  );
+  v_count := v_count1 - v_count0;
+  return v_count;
+end;
+$body$;
+
+-- test
+--  select count
+--  from get_count(
+--    'Cutter',
+--    'controller/partOccurrence/part_count-lifetime',
+--    timestamptz2ms('2022-02-01 18:00:00'),
+--    timestamptz2ms('2022-02-02 18:00:00'),
+--  );
