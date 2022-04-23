31a74ee06122049e22d6b85abd9464b950cd1d7f
Brian Burns <bburns.km@gmail.com>
2022-04-22 23:29:46 -0500

    wip get_timeline add search_limit param

--- services/relay/src/migrations/005-get_timeline.sql
+++ services/relay/src/migrations/005-get_timeline.sql
@@ -12,15 +12,17 @@
 --   SELECT time, value AS "Availability" 
 --   FROM get_timeline('Line$line', 'availability', $__from, $__to);
 
--- do this if change parameters OR return signature
--- DROP FUNCTION IF EXISTS get_timeline(text, text, bigint, bigint);
+-- need this if change parameters OR return signature
+DROP FUNCTION IF EXISTS get_timeline(text, text, bigint, bigint, boolean);
+DROP FUNCTION IF EXISTS get_timeline(text, text, bigint, bigint, boolean, text);
 
 CREATE OR REPLACE FUNCTION get_timeline (
   IN devicename text, -- the device name, eg 'Line1'
   IN pathname text, -- the history view path, eg 'availability'
   IN from_ms bigint, -- start time in milliseconds since 1970-01-01
   IN to_ms bigint, -- end time in milliseconds since 1970-01-01
-  IN clamp boolean = TRUE -- return left edge as first time 
+  IN clamp boolean = TRUE, -- return left edge as first time 
+  IN search_limit text = '1h' -- search limit - don't search further into past than this
 )
 RETURNS TABLE ("time" timestamp, "value" text) -- ANSI standard SQL
 LANGUAGE sql
@@ -55,9 +57,10 @@ FROM
     value
   FROM history_all
   WHERE
-    device = devicename and
-    path = pathname and
-    time <= (TABLE from_time)
+    device = devicename
+    and path = pathname
+    and time <= (TABLE from_time)
+    and time >= (TABLE from_time) - search_limit::interval
   ORDER BY history_all.time DESC
   LIMIT 1 -- just want the last value
   ) AS subquery1 -- subquery name is required but not used
@@ -69,13 +72,13 @@ $BODY$;
 --.... turn off before committing
 
 -- WITH
---   p_day AS (VALUES ('2021-11-05'::date)),
+--   p_day AS (VALUES ('2022-04-21'::date)),
 --   p_from AS (VALUES ((EXTRACT(epoch FROM (TABLE p_day)) * 1000)::bigint)),
 --   p_to AS (VALUES ((EXTRACT(epoch FROM (TABLE p_day) + INTERVAL '1 week') * 1000)::bigint))
 -- SELECT time, value AS "Sales Order" 
 -- FROM get_timeline(
---   'Line1',
---   'controller/processOccurrence/process_aggregate_id-order_number[sales_order]', 
+--   'Marumatsu',
+--   'availability', 
 --   (table p_from), 
 --   (table p_to)
 -- );
