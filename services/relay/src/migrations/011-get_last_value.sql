1db560315de69f1147f41767cbc036c985c1b058
Brian Burns <bburns.km@gmail.com>
2022-07-16 18:08:35 -0500

    cmts

--- services/relay/src/migrations/011-get_last_value.sql
+++ services/relay/src/migrations/011-get_last_value.sql
@@ -5,6 +5,12 @@
 -- need this if change parameters OR return signature
 DROP FUNCTION IF EXISTS get_last_value(text, text, text);
 
+-- note: added the search_limit parameter because if devicename or pathname
+-- are not there, or no value in db, would scan through entire history table for a value.
+-- the search limit keeps it reasonable.
+
+-- BUT better to just set statement_timeout to 2 secs or sthing...
+
 CREATE OR REPLACE FUNCTION get_last_value (
   IN devicename text, -- the device name, eg 'Marumatsu'
   IN pathname text, -- the history view path, eg 'availability'
@@ -14,6 +20,7 @@ RETURNS TABLE ("time" timestamp, "value" text) -- ANSI standard SQL
 LANGUAGE sql
 AS
 $BODY$
+
 SELECT time, value->>0 as value -- note: ->>0 extracts the top-level jsonb value
 FROM history_all
 WHERE
@@ -22,6 +29,15 @@ WHERE
   and time > now() - search_limit::interval
 ORDER BY time desc
 LIMIT 1
+
+-- -- note: ->>0 extracts the top-level jsonb value
+-- select time, value->>0 as value from history
+-- where 
+--   node_id=(select node_id from nodes where props->>'name'=devicename)
+--   and dataitem_id=(select node_id from nodes where props->>'path'=pathname)
+-- order by time desc
+-- limit 1
+
 $BODY$;
 
 -- test fn
