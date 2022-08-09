965f921074089f1ba1297ae1e90bb023af7bf59a
Brian Burns <bburns.km@gmail.com>
2022-08-09 17:13:30 -0500

    turn off 020sql

--- services/relay/src/migrations/020-update.sql
+++ services/relay/src/migrations/020-update.sql
@@ -1,70 +1,4 @@
 ---------------------------------------------------------------------
--- nodes table
----------------------------------------------------------------------
--- ditch unique path index
-DROP INDEX IF EXISTS nodes_path;
-
--- add unique gid index (gid = global id = device uuid + dataitemid OR a unique alias)
-CREATE UNIQUE INDEX IF NOT EXISTS nodes_gid ON nodes ((props->>'gid'));
-
-
----------------------------------------------------------------------
--- views
----------------------------------------------------------------------
-
-DROP VIEW IF EXISTS dataitems;
-DROP VIEW IF EXISTS devices;
-DROP VIEW IF EXISTS history_text;
-DROP VIEW IF EXISTS history_float;
-DROP VIEW IF EXISTS history_all;
-
----------------------------------------------------------------------
--- history_all
----------------------------------------------------------------------
--- the history table, but with name and path dereferenced,
--- and value as the original jsonb object.
-
-CREATE OR REPLACE VIEW history_all AS
-SELECT 
-  devices.props->>'name' AS device,
-  dataitems.props->>'path' AS path,
-  history.time,
-  history.value -- a jsonb object - need to cast it as in below views
-FROM history
-JOIN nodes AS devices ON history.node_id=devices.node_id
-JOIN nodes AS dataitems ON history.dataitem_id=dataitems.node_id;
-
-
----------------------------------------------------------------------
--- history_float
----------------------------------------------------------------------
--- the history_all view, but with only numeric values.
-
--- note: float is an alias for 'double precision' or float8
-
---. how handle UNDEFINED? should translate to null in relay?
-
-CREATE OR REPLACE VIEW history_float AS
-SELECT device, path, time, value::float
-FROM history_all
-WHERE jsonb_typeof(value) = 'number';
-
-
----------------------------------------------------------------------
--- history_text
----------------------------------------------------------------------
--- the history_all view, but with only text values.
-
--- note: #>>'{}' extracts the top-level json string without enclosing double quotes
--- see https://dba.stackexchange.com/questions/207984/unquoting-json-strings-print-json-strings-without-quotes
-
-CREATE OR REPLACE VIEW history_text AS
-SELECT device, path, time, value#>>'{}' as value
-FROM history_all
-WHERE jsonb_typeof(value) = 'string';
-
-
----------------------------------------------------------------------
 -- devices
 ---------------------------------------------------------------------
 
@@ -77,10 +11,10 @@ SELECT
   nodes.props->>'name' as name,
   nodes.props->>'id' as id,
   nodes.props->>'uuid' as uuid, 
-  nodes.props->>'gid' as gid, -- uuid:id
+  nodes.props->>'gid' as gid, -- uuid:id or unique alias
   nodes.props->>'fullpath' as fullpath,
   nodes.props->>'path' as path,
-  nodes.props->>'path' as path
+  nodes.props->>'order' as "order"
 FROM 
   nodes
 WHERE
@@ -88,36 +22,27 @@ WHERE
 
 
 ---------------------------------------------------------------------
--- dataitems
+-- nodes table
 ---------------------------------------------------------------------
+--. why were we dropping this?
+-- -- ditch unique path index
+-- DROP INDEX IF EXISTS nodes_path;
 
-CREATE OR REPLACE VIEW dataitems AS
-SELECT
-  nodes.node_id,
-  nodes.props->>'path' as path,
-  nodes.props->>'category' as category, 
-  nodes.props->>'type' as type,
-  nodes.props->>'subType' as subtype,
-  nodes.props->>'units' as units,
-  nodes.props->>'nativeUnits' as nativeunits,
-  nodes.props->>'statistic' as statistic
-FROM 
-  nodes
-WHERE
-  nodes.props->>'node_type'='DataItem';
+-- add unique gid index (gid = global id = device uuid + dataitemid OR a unique alias)
+CREATE UNIQUE INDEX IF NOT EXISTS nodes_gid ON nodes ((props->>'gid'));
+
 
 ---------------------------------------------------------------------
 -- nodes_all
 ---------------------------------------------------------------------
 
-CREATE OR REPLACE VIEW nodes_all AS
-SELECT
-  nodes.node_id,
-  nodes.props->>'path' as path
-FROM 
-  nodes
--- WHERE
---   nodes.props->>'node_type'='DataItem';
-
-
+--. what was this for?
+-- CREATE OR REPLACE VIEW nodes_all AS
+-- SELECT
+--   nodes.node_id,
+--   nodes.props->>'path' as path
+-- FROM 
+--   nodes
+-- -- WHERE
+-- --   nodes.props->>'node_type'='DataItem';
 
