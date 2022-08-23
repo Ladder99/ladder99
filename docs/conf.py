036361e64381ee60b2ee3a23cb0003305ff110d7
Brian Burns <bburns.km@gmail.com>
2022-08-23 12:59:16 -0500

    styles

--- docs/conf.py
+++ docs/conf.py
@@ -82,6 +82,9 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['pages/_static']
 
+# custom.css is inside one of the html_static_path folders (e.g. _static)
+html_css_files = ["custom.css"]
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
