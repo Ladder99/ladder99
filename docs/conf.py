dd213a7a0e10c555a597ee50532b9a9b0534c354
Brian Burns <bburns.km@gmail.com>
2022-08-23 13:12:09 -0500

    confpy

--- docs/conf.py
+++ docs/conf.py
@@ -91,8 +91,8 @@ html_css_files = ["custom.css"]
 # html_extra_path = []
 
 
-def setup(app):
-    app.add_css_file('custom.css')
+# def setup(app):
+#     app.add_css_file('custom.css')
 
 
 sphinx_tabs_disable_tab_closing = True
