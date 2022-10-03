97afd597c951705d4905ee1fedf9743fdee195cc
Brian Burns <bburns.km@gmail.com>
2022-10-03 19:12:50 -0400

    fix feedback?

--- services/adapter/src/drivers/feedback.js
+++ services/adapter/src/drivers/feedback.js
@@ -3,6 +3,27 @@
 // currently used for watching changes to jobboss job number,
 // sending a part count reset to a marumatsu cutter via mqtt.
 
+// in setup.yaml, do sthing like this -
+//
+// adapter:
+//   drivers:
+//     feedback:
+//       dataitem: job # this will be prefixed with deviceId, eg to 'm1-job' - reset partcount when changes
+//       interval: 2000 # ms to wait between polls
+//       driver: sharedMqtt
+//       command: l99/B01000/cmd/modbus # mqtt topic to send commands to
+//       payload:
+//         address: null # varies by device - eg 142
+//         value: null # send values[0], wait, then values[1]
+//         unitid: 199
+//         quantity: 1
+//         fc: 6
+//       values: [5392, 0] # values for payload
+//       wait:
+//         topic: l99/B01000/evt/io # mqtt topic to watch after posting command
+//         attribute: a15 # watch this payload attribute
+//         value: 5392 # wait for it to get this value before sending second command
+
 //. will this be replaced by MTConnect Interfaces eventually?
 
 // optional flag defined in .env
@@ -25,7 +46,7 @@ export class AdapterDriver {
     this.provider = provider // eg the mqttProvider.js object
 
     // get base data, if there
-    const feedback = setup?.adapter?.drivers?.feedback || {}
+    const feedback = setup.adapter?.drivers?.feedback || {}
     this.command = feedback.command || 'missing-command-topic' // topic for commands
     this.payload = feedback.payload || {} // defaults for command payload, if any
     this.values = feedback.values // array of the two values to send with commands
@@ -45,6 +66,7 @@ export class AdapterDriver {
     const newValue = this.cache.get(this.dataitem) // eg 'm1-job'
     this.oldValue = this.oldValue || newValue // this will avoid firing all this off if just starting up, when oldValue=null
     if (newValue !== this.oldValue) {
+      //
       // send msg, wait for response, send second
       console.log(this.me, `value changed from ${this.oldValue} to ${newValue}`)
 
@@ -52,7 +74,7 @@ export class AdapterDriver {
       // will subscribe to mqttProvider with dispatch based on payload.id
       const topic = this.wait.topic
       const callback = waitForSignal.bind(this)
-      const selector = payload => payload.id == this.source.id // eg id=535172
+      const selector = payload => payload.id == this.source.id // eg id=535172 - use == in case string/number
       console.log(this.me, `subscribing to`, topic, selector)
       this.provider.subscribe(topic, callback, selector)
 
@@ -65,7 +87,11 @@ export class AdapterDriver {
       this.provider.publish(this.command, JSON.toString(payload))
       this.oldValue = newValue
 
+      //. what if the response never comes? need a timeout after a minute?
+      // then cancel the subscription, and send the second command anyway?
+
       function waitForSignal(topic, payload) {
+        payload = payload.toString()
         payload = JSON.parse(payload)
         console.log(this.me, `got response`, payload)
         // eg this.wait.attribute is 'a15', values[0] is 5392
