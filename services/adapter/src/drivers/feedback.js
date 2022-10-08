2135c05ca3ff2687fc8209d676a1c7120338138e
Brian Burns <bburns.km@gmail.com>
2022-10-08 13:40:32 -0400

    try getting feedback to work

--- services/adapter/src/drivers/feedback.js
+++ services/adapter/src/drivers/feedback.js
@@ -8,7 +8,8 @@
 //. will this be replaced by MTConnect Interfaces eventually?
 
 // import * as lib from '../common/lib.js'
-import * as foo from '../common/foo.js'
+// import * as foo from '../common/foo.js'
+import { getSelector } from '../helpers.js'
 
 // optional flag defined in .env.
 // lets you turn on the feedback mechanism for a live/production device,
@@ -40,46 +41,14 @@ export class AdapterDriver {
     this.values = this.publish.values || [] // eg [5392, 0] the two values to send with commands
     this.wait = feedback.wait || {} // { topic, payload } topic and payload filter to wait on
 
-    // const waitAttribute = this.wait.attribute // eg 'a15'
+    // topic to wait on
+    this.waitTopic = this.wait.topic
 
-    // this.waitSelector = {
-    //   filter: payload =>
-    //     payload.id == source.id && payload.a15 == this.values[0],
-    //   equal: payload => this.filter(payload) && payload.length === 2,
-    // }
+    // get wait selector boolean or fn for the payload object we're going to wait for
+    this.waitSelector = getSelector(this.wait.payload)
 
-    // // filter by fn - faster than generic object comparison
-    // //. wrap selector in the closure?
-    // const waitFilter = payload =>
-    //   payload.id == selector.id &&
-    //   payload[waitAttribute] == selector[waitAttribute]
-    // // mqttProvider will use this to prevent duplicate subscriptions, and for unsubscribing
-    // // const waitEqual = payload => waitFilter(payload) && payload.length === 2
-    // const waitEqual = (subscriber1, subscriber2) =>
-    //   subscriber1.callback.name === subscriber2.callback.name &&
-    //   subscriber1.selector.id === subscriber2.selector.id &&
-    //   subscriber1.selector[waitAttribute] ===
-    //     subscriber2.selector[waitAttribute]
-
-    // const waitFilter = getFilterFn(this.wait.payload)
-    // const waitEqual = getEqualFn(this.wait.payload)
-    //.
-
-    // // filter by fn - faster than generic object comparison
-    // //. could wrap selector in the closure
-    // const waitFilter = payload =>
-    //   payload.id == selector.id &&
-    //   payload[waitAttribute] == selector[waitAttribute]
-
-    // // mqttProvider will use this to prevent duplicate subscriptions, and for unsubscribing the right fn instance.
-    // const waitEqual = (subscriber1, subscriber2) =>
-    //   subscriber1.callback.name === subscriber2.callback.name &&
-    //   subscriber1.selector.id === subscriber2.selector.id &&
-    //   subscriber1.selector[waitAttribute] ===
-    //     subscriber2.selector[waitAttribute]
-
-    // get a wait selector { filter, equal } for the payload object we're going to wait for
-    this.waitSelector = foo.getSelector(this.wait.payload)
+    // bind callback to this instance so will always have same address for subscribing and unsubscribing
+    this.waitCallback = this.feedbackCallback.bind(this)
 
     // publish:
     // topic: l99/B01000/cmd/modbus # mqtt topic to send commands to
@@ -90,8 +59,6 @@ export class AdapterDriver {
     //   quantity: 1
     //   fc: 6
     // values: [5392, 0] # values for payload.value - matches wait.payload.a15 below
-
-    this.publish = {}
 
     // check dataitem value - when changes, send reset cmd, wait for response, send 2nd cmd
     this.oldValue = null
@@ -108,12 +75,10 @@ export class AdapterDriver {
 
       // send command, wait for response, send second command
 
-      const values = this.values // eg [5392, 0]
+      // const values = this.values // eg [5392, 0]
 
       // subscribe to response topic
       // will subscribe to mqttProvider with dispatch based on payload.id and waitAttribute value
-      const waitTopic = this.wait.topic
-      const waitCallback = feedbackCallback.bind(this)
 
       // this worked, but was too slow
       // const waitSelector = { id: this.source.id, [waitAttribute]: values[0] } // filter by example
@@ -121,50 +86,49 @@ export class AdapterDriver {
       // subscribe to wait topic
       // this.provider.subscribe(waitTopic, waitCallback, waitSelector, false)
       // sendLastMessage false so doesn't call the callback if topic already registered
-      this.provider.subscribe(waitTopic, waitCallback, this.waitSelector, false)
+      this.provider.subscribe(
+        this.waitTopic,
+        this.waitCallback,
+        this.waitSelector,
+        false
+      )
 
       // publish to command topic
       const { address } = this.source // { driver, connection, address, id }
       const commandTopic = this.command.topic
-      const commandPayload = { ...this.payload, address, value: values[0] } // { address, value, unitid, quantity, fc }
+      const commandPayload = { ...this.payload, address, value: this.values[0] } // { address, value, unitid, quantity, fc }
       console.log(this.me, `publish`, commandTopic, commandPayload)
       this.provider.publish(commandTopic, JSON.toString(commandPayload))
       this.oldValue = newValue
 
       // q. what if a response never comes? timeout after a minute?
       // well, it's okay - we check for duplicate subscriptions, so they won't clog the system.
+    }
+  }
 
-      // callback for wait topic
-      function feedbackCallback(topic, payload) {
-        payload = payload.toString()
-        payload = JSON.parse(payload)
+  // callback for wait topic
+  feedbackCallback(topic, payload) {
+    payload = payload.toString()
+    payload = JSON.parse(payload)
 
-        const sentValue = payload[waitAttribute] // eg payload.a15
-        console.log(this.me, `feedbackCallback response`, payload, sentValue)
+    const sentValue = payload[waitAttribute] // eg payload.a15
+    console.log(this.me, `feedbackCallback response`, payload, sentValue)
 
-        // note: we use == because either might be a string, not number.
-        // selector should have checked this already, but just in case.
-        if (sentValue == values[0]) {
-          // publish the second command
-          const commandPayload = { ...this.payload, address, value: values[1] }
-          this.provider.publish(commandTopic, JSON.toString(commandPayload))
+    // note: we use == because either might be a string, not number.
+    // selector should have checked this already, but just in case.
+    if (sentValue == values[0]) {
+      // publish the second command
+      const commandPayload = { ...this.payload, address, value: this.values[1] }
+      this.provider.publish(commandTopic, JSON.toString(commandPayload))
 
-          // unsubscribe from the wait topic
-          this.provider.unsubscribe(waitTopic, waitCallback, waitSelector)
-          // this.provider.unsubscribe(
-          //   waitTopic,
-          //   waitCallback,
-          //   waitFilter,
-          //   waitEqual
-          // )
-        } else {
-          console.log(
-            this.me,
-            `error feedbackCallback got wrong value`,
-            sentValue
-          )
-        }
-      }
+      // unsubscribe from the wait topic
+      this.provider.unsubscribe(
+        this.waitTopic,
+        this.waitCallback,
+        this.waitSelector
+      )
+    } else {
+      console.log(this.me, `error feedbackCallback got wrong value`, sentValue)
     }
   }
 }
