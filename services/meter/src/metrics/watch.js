1b024330315a6e1ac5bc9de03a38d503e6669dc6
Brian Burns <bburns.km@gmail.com>
2022-10-12 18:37:39 -0400

    wip broken

--- services/meter/src/metrics/watch.js
+++ services/meter/src/metrics/watch.js
@@ -13,8 +13,9 @@ export class Metric {
     this.device = device
     this.metric = metric
 
-    this.lastStopTime = undefined
-    this.lastWatchValue = undefined
+    // this.lastStopTime = undefined
+    // this.lastWatchValue = undefined
+    this.lastRecord = { time: undefined, value: undefined }
 
     this.me = `Watch ${device.name}:`
     console.log(this.me, 'start metric', metric.watchPath, metric.updatePath)
@@ -41,133 +42,85 @@ export class Metric {
   // poll db and update lifetime count - called by timer
   async poll() {
     //
-    const deviceName = this.device.name
-
-    // due to nature of js event loop, poll won't be called exactly every this.interval ms.
-    // that means we could miss job count records in the gaps, causing 'misses'.
-    // // so keep track of lastStopTime.
-    // so keep track of lastRecord = { time, value }.
-    // well even that didn't help...
-    // need an offset to give adapter time to write data also.
+    // note: due to nature of js event loop, poll won't be called exactly every this.interval ms,
+    // which means we could miss job count records in the gaps, causing 'misses'.
+    // so keep track of lastRecord = { time, value }
+    // well even that didn't help - still had gaps.
+    // so need an offset to give adapter time to write data also.
     const now = new Date()
     const stopMs = now.getTime() - this.offset // ms
     const stopTime = new Date(stopMs).toISOString()
     const startTime =
       this.lastStopTime ?? new Date(stopMs - this.interval).toISOString()
 
-    // //. merge these operations
-    // if (this.metric.operation === 'accumulate') {
-    //   // get values and accumulate deltas since last poll
-    //   await this.accumulate()
-    // } else if (this.metric.operation === 'count') {
-    //   // get count of non-unavailable transitions since last poll
-    //   await this.count()
-    // }
-    await this.accumulate()
-
-    // save time for next poll
-    // this.lastStopTime = stopTime
-  }
-
-  async accumulate() {
-    //
-    // // get last lifetime count, before start time
-    // const record = await this.db.getLastRecord2(
-    //   'history_float',
-    //   this.device.name,
-    //   this.metric.updatePath,
-    //   startTime
-    // )
-    // let lifetimeCount = record ? record.value : 0
-    // console.log(this.me, `last lifetime count`, lifetimeCount)
-
-    // get counts
-    // note that the function is a union that includes the record before start time.
-    //. could save the trouble of the union and just keep a record of the last value - less work for db.
-    // const rows = await this.db.getHistory2(
-    //   'history_all',
-    //   this.device.name,
-    //   this.metric.watchPath,
-    //   startTime,
-    //   stopTime
-    // )
-
-    // // getHistory3 gets records where start>=time<stop, no unions
-    // const rows = await this.db.getHistory3(
-    //   'history_all',
-    //   this.device.name,
-    //   this.metric.watchPath,
-    //   startTime,
-    //   stopTime
-    // )
-
     // get records from history_all where start<=time<stop and value not 'UNAVAILABLE'
-    const rows = await this.db.getHistoryNonUnavailable(
-      this.device.name,
-      this.metric.watchPath,
-      startTime,
-      stopTime
-    )
+    const rows =
+      (await this.db.getHistoryNonUnavailable(
+        this.device.name,
+        this.metric.watchPath,
+        startTime,
+        stopTime
+      )) || []
 
-    if (rows && rows.length > 0) {
-      // const record = { time: this.lastStopTime, value: this.lastLifetimeCount }
-      // rows.splice(0, 0, record) // add record before start time to front of array
-      rows.splice(0, 0, this.lastRecord) // add previous record to front of array
-      console.log(this.me, `watch rows`, rows)
+    // if (rows && rows.length > 0) {
+    // rows.splice(0, 0, this.lastRecord) // add previous record to front of array
+    console.log(this.me, `watch rows`, rows)
 
-      // rows will be like (for start=10:00:00am, stop=10:00:06am)
-      // time, value
-      // 9:59:59am, 99
-      // 10:00:00am, 100
-      // 10:00:01am, 101
-      // 10:00:02am, 102
-      // 10:00:03am, 'UNAVAILABLE'
-      // 10:00:04am, 0
-      // 10:00:05am, 1
-      // 10:00:06am, 2
+    // rows will be like (for start=10:00:00am, stop=10:00:06am)
+    // time, value
+    // 9:59:59am, 99
+    // 10:00:00am, 100
+    // 10:00:01am, 101
+    // 10:00:02am, 102
+    // 10:00:03am, 'UNAVAILABLE'
+    // 10:00:04am, 0
+    // 10:00:05am, 1
+    // 10:00:06am, 2
 
-      // build up an array of rows to write to history table
-      // with { device, dataitem, time, value }
-      let previousRow = rows[0] // { time, value }
-      const lifetimeRows = []
-      if (this.metric.operation === 'accumulate') {
-        for (let row of rows.slice(1)) {
-          // get delta from previous value
-          const deltaCount = row.value - previousRow.value
-          if (deltaCount > 0) {
-            lifetimeCount += deltaCount
-            const lifetimeRow = {
-              node_id: this.device_id,
-              dataitem_id: this.update_id,
-              time: row.time.toISOString(),
-              value: lifetimeCount,
-            }
-            lifetimeRows.push(lifetimeRow)
+    // build up an array of rows to write to history table
+    // with { device, dataitem, time, value }
+    // let previousRow = rows[0] // { time, value }
+    let previousRow = this.lastRecord // { time, value }
+    const lifetimeRows = []
+    if (this.metric.operation === 'accumulate') {
+      for (let row of rows.slice(1)) {
+        // get delta from previous value
+        const deltaCount = row.value - previousRow.value
+        if (deltaCount > 0) {
+          lifetimeCount += deltaCount
+          const lifetimeRow = {
+            node_id: this.device_id,
+            dataitem_id: this.update_id,
+            time: row.time.toISOString(),
+            value: lifetimeCount,
           }
-          previousRow = row
+          lifetimeRows.push(lifetimeRow)
         }
-      } else if (this.metric.operation === 'count') {
-        for (let row of rows.slice(1)) {
-          // check if value changed from previous row
-          // const deltaCount = row.value - previousRow.value
-          if (row.value !== previousRow.value) {
-            lifetimeCount += 1
-            const lifetimeRow = {
-              node_id: this.device_id,
-              dataitem_id: this.update_id,
-              time: row.time.toISOString(),
-              value: lifetimeCount,
-            }
-            lifetimeRows.push(lifetimeRow)
-          }
-          previousRow = row
-        }
+        previousRow = row
       }
-      console.log(this.me, `writing lifetime rows`, lifetimeRows)
-      await this.db.addHistory(lifetimeRows)
-
-      this.lastRecord = rows[rows.length - 1]
+    } else if (this.metric.operation === 'count') {
+      // for (let row of rows.slice(1)) {
+      for (let row of rows) {
+        // check if value changed from previous row
+        // const deltaCount = row.value - previousRow.value
+        if (row.value !== previousRow.value) {
+          lifetimeCount += 1
+          const lifetimeRow = {
+            node_id: this.device_id,
+            dataitem_id: this.update_id,
+            time: row.time.toISOString(),
+            value: lifetimeCount,
+          }
+          lifetimeRows.push(lifetimeRow)
+        }
+        previousRow = row
+      }
     }
+    console.log(this.me, `writing lifetime rows`, lifetimeRows)
+    await this.db.addHistory(lifetimeRows)
+
+    // save record for next poll
+    this.lastRecord = rows[rows.length - 1]
   }
 
   //   // backfill missing partcount records
