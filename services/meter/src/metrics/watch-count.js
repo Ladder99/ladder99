0ef9543f5dfc078e4ecebbeb86576244fdfe3529
Brian Burns <bburns.km@gmail.com>
2022-10-12 17:54:49 -0400

    wip broken switch watchjs and countjs

---
+++ services/meter/src/metrics/watch-count.js
@@ -0,0 +1,79 @@
+// watch a dataitem value and do some action on change
+
+// in setup.yaml, add a section like this:
+
+// meter:
+//   watch:
+//     watchPath: processes/job/process_aggregate_id-order_number
+//     updatePath: process/life/cycle_count-all
+//     operation: increment
+
+const metricIntervalDefault = 5 // seconds
+
+export class Metric {
+  //
+  async start({ client, db, device, metric }) {
+    this.client = client
+    this.db = db
+    this.device = device
+    this.metric = metric
+
+    this.lastWatchValue = undefined
+
+    this.me = `Watch ${this.device.name}:`
+    console.log(this.me, `path is ${this.metric.watchPath}`)
+
+    // get node_ids since we'll be writing directly to history table
+    console.log(this.me, `get device and dataitem node_ids...`)
+    //. made getNodeId method in historian branch
+    this.device_id = await this.db.getDeviceId(device.name) // repeats until device is there
+    this.update_id = await this.db.getDataItemId(metric.updatePath) // repeat until dataitem is there
+
+    // get polling interval - either from metric in setup yaml or default value
+    this.interval = (metric.interval || metricIntervalDefault) * 1000 // ms
+
+    this.offset = 3000 // ms - look this far back in time for raw count values so adapter has time to write data
+
+    await this.poll() // do first poll
+    this.timer = setInterval(this.poll.bind(this), this.interval) // poll db
+  }
+
+  async poll() {
+    //
+    //. could be watching a number, or string - this returns a json object? check
+    const currentWatchValue = await this.db.getLatestValue(
+      'history_all',
+      this.device, // has { name }
+      this.metric.watchPath
+    )
+    console.log(this.me, `currentWatchValue`, currentWatchValue)
+
+    // initialize saved watch value if needed - prevents immediate action on startup
+    this.lastWatchValue = this.lastWatchValue ?? currentWatchValue
+
+    // check if watch value changed - if so, do action
+    if (currentWatchValue !== this.lastWatchValue) {
+      if (this.metric.operation === 'increment') {
+        const countValue =
+          (await this.db.getLatestValue(
+            'history_float',
+            this.device, // has { name }
+            this.metric.updatePath
+          )) || 0
+        const newCountValue = countValue + 1
+        console.log(
+          this.me,
+          `incrementing ${this.metric.updatePath} to ${newCountValue}`
+        )
+        await this.db.writeHistory(
+          this.device_id,
+          this.update_id,
+          new Date().toISOString(),
+          newCountValue
+        )
+      }
+      // save watch value
+      this.lastWatchValue = currentWatchValue
+    }
+  }
+}
--- services/meter/src/metrics/watch.js
+++ services/meter/src/metrics/watch.js
@@ -1,12 +1,7 @@
-// watch a dataitem value and do some action on change
+// watch a dataitem and perform an action when value changes
 
-// in setup.yaml, add a section like this:
-
-// meter:
-//   watch:
-//     watchPath: processes/job/process_aggregate_id-order_number
-//     updatePath: process/life/cycle_count-all
-//     operation: increment
+// if operation is 'accumulate', will keep a running total count, eg for lifetime part counts.
+// if operation is 'count', will count number of non-unavailable transitions, eg for lifetime job counts.
 
 const metricIntervalDefault = 5 // seconds
 
@@ -18,62 +13,164 @@ export class Metric {
     this.device = device
     this.metric = metric
 
+    this.lastStopTime = undefined
     this.lastWatchValue = undefined
 
-    this.me = `Watch ${this.device.name}:`
-    console.log(this.me, `path is ${this.metric.watchPath}`)
+    this.me = `Watch ${device.name}:`
+    console.log(this.me, 'start metric', metric.watchPath, metric.updatePath)
 
-    // get node_ids since we'll be writing directly to history table
-    console.log(this.me, `get device and dataitem node_ids...`)
-    //. made getNodeId method in historian branch
+    console.log(this.me, `get device node_id...`)
     this.device_id = await this.db.getDeviceId(device.name) // repeats until device is there
-    this.update_id = await this.db.getDataItemId(metric.updatePath) // repeat until dataitem is there
+
+    // need this dataitemId as we'll be writing directly to the history table
+    console.log(this.me, `get update dataitem_id...`)
+    this.update_id = await this.db.getDataItemId(metric.updatePath) // repeat until dataitem there
 
     // get polling interval - either from metric in setup yaml or default value
     this.interval = (metric.interval || metricIntervalDefault) * 1000 // ms
 
-    this.offset = 3000 // ms - look this far back in time for raw count values so adapter has time to write data
+    // look this far back in time for raw count values so adapter has time to write data.
+    // this avoided a problem with missing data.
+    this.offset = 3000 // ms
 
+    // await this.backfill() // backfill any missing values
     await this.poll() // do first poll
     this.timer = setInterval(this.poll.bind(this), this.interval) // poll db
   }
 
+  // poll db and update lifetime count - called by timer
   async poll() {
     //
-    //. could be watching a number, or string - this returns a json object? check
-    const currentWatchValue = await this.db.getLatestValue(
-      'history_all',
-      this.device, // has { name }
-      this.metric.watchPath
+    const deviceName = this.device.name
+
+    // due to nature of js event loop, poll won't be called exactly every this.interval ms.
+    // that means we could miss job count records in the gaps, causing 'misses'.
+    // so keep track of lastStopTime.
+    // well even that didn't help.
+    // need an offset to give adapter time to write data also.
+    const now = new Date()
+    const stopMs = now.getTime() - this.offset // ms
+    const stopTime = new Date(stopMs).toISOString()
+    const startTime =
+      this.lastStopTime ?? new Date(stopMs - this.interval).toISOString()
+
+    const updatePath = this.metric.updatePath
+
+    // get last lifetime count, before start time
+    const record = await this.db.getLastRecord(
+      deviceName,
+      updatePath,
+      startTime
     )
-    console.log(this.me, `currentWatchValue`, currentWatchValue)
+    let lifetimeCount = record ? record.value : 0
 
-    // initialize saved watch value if needed - prevents immediate action on startup
-    this.lastWatchValue = this.lastWatchValue ?? currentWatchValue
-
-    // check if watch value changed - if so, do action
-    if (currentWatchValue !== this.lastWatchValue) {
-      if (this.metric.operation === 'increment') {
-        const countValue =
-          (await this.db.getLatestValue(
-            'history_float',
-            this.device, // has { name }
-            this.metric.updatePath
-          )) || 0
-        const newCountValue = countValue + 1
-        console.log(
-          this.me,
-          `incrementing ${this.metric.updatePath} to ${newCountValue}`
-        )
-        await this.db.writeHistory(
-          this.device_id,
-          this.update_id,
-          new Date().toISOString(),
-          newCountValue
-        )
+    // get job counts
+    //. currently gets from history_float view
+    const rows = await this.db.getHistory(
+      deviceName,
+      this.metric.watchPath,
+      startTime,
+      stopTime
+    )
+    // console.log(this.me, `job count rows`, rows)
+    // rows will be like (for start=10:00:00am, stop=10:00:05am)
+    // time, value
+    // 9:59:59am, 99
+    // 10:00:00am, 100
+    // 10:00:01am, 101
+    // 10:00:02am, 102
+    // 10:00:03am, 0
+    // 10:00:04am, 1
+    // 10:00:05am, 2
+    //. what if just one row? compare with last seen value eh?
+    if (rows && rows.length > 1) {
+      let previousRow = rows[0] // { time, value }
+      const lifetimeRows = []
+      for (let row of rows.slice(1)) {
+        // get delta from previous value
+        const deltaCount = row.value - previousRow.value
+        if (deltaCount > 0) {
+          lifetimeCount += deltaCount
+          // write time, lifetime
+          // better to save these up in an array and write all at once, for speed
+          // await this.db.writeHistory(
+          //   this.device_id,
+          //   this.update_id,
+          //   row.time.toISOString(),
+          //   lifetimeCount
+          // )
+          const lifetimeRow = {
+            node_id: this.device_id,
+            dataitem_id: this.update_id,
+            time: row.time.toISOString(),
+            value: lifetimeCount,
+          }
+          lifetimeRows.push(lifetimeRow)
+        }
+        previousRow = row
       }
-      // save watch value
-      this.lastWatchValue = currentWatchValue
+      // console.log(this.me, `writing lifetime rows`, lifetimeRows)
+      await this.db.addHistory(lifetimeRows)
     }
+    // save time for next poll
+    this.lastStopTime = stopTime
   }
+
+  //   // backfill missing partcount records
+  //   async backfill() {
+  //     const deviceName = this.device.name
+  //     console.log(this.me, `backfill any missed partcounts`)
+
+  //     const now = new Date()
+
+  //     // get latest lifetime count record
+  //     let record = await this.db.getLastRecord(
+  //       deviceName,
+  //       this.metric.updatePath,
+  //       now.toISOString()
+  //     )
+  //     console.log(this.me, `last record`, record)
+
+  //     // if no lifetime record, start from the beginning
+  //     if (!record) {
+  //       const record2 = await this.db.getFirstRecord(
+  //         deviceName,
+  //         this.metric.watchPath
+  //       )
+  //       console.log(this.me, `first record`, record2)
+  //       // no delta data either, so exit
+  //       if (!record2) {
+  //         return
+  //       }
+  //       // record = { time: record2.time, value: 0}
+  //       record = {}
+  //       record.time = record2.time
+  //       record.value = 0
+  //     }
+
+  //     const start = record.time.toISOString()
+  //     const stop = now.toISOString()
+  //     let lifetime = record.value
+  //     const rows = await this.db.getHistory(
+  //       deviceName,
+  //       this.metric.watchPath,
+  //       start,
+  //       stop
+  //     ) // gets last one before start also, if any
+  //     let previous = rows[0]
+  //     for (let row of rows.slice(1)) {
+  //       const delta = row.value - previous.value
+  //       if (delta > 0) {
+  //         lifetime += delta
+  //         await this.db.writeHistory(
+  //           this.device_id,
+  //           this.update_id,
+  //           row.time.toISOString(),
+  //           lifetime
+  //         )
+  //       }
+  //       previous = row
+  //     }
+  //     console.log(this.me, `backfill done`)
+  //   }
 }
