8e1e41afa092a2ecce25d2fe9a49152c79af7076
Brian Burns <bburns.km@gmail.com>
2022-10-21 19:25:47 -0400

    opc-test reads product name

--- services/adapter/src/drivers/opc.js
+++ services/adapter/src/drivers/opc.js
@@ -12,11 +12,17 @@ const {
   MessageSecurityMode,
   SecurityPolicy,
   AttributeIds,
-  // makeBrowsePath,
+  makeBrowsePath,
   // ClientSubscription,
   // TimestampsToReturn,
   // ClientMonitoredItem,
 } = pkg
+
+// host.docker.internal lets you access localhost on the host computer, which is running kepware
+const defaultUrl = 'opc.tcp://host.docker.internal:49320'
+
+// the simulator service runs an opc server on this port
+// const defaultUrl = 'opc.tcp://simulator:4334/UA/LittleServer'
 
 export class AdapterDriver {
   //
@@ -24,10 +30,13 @@ export class AdapterDriver {
   async init({ device, cache, source }) {
     console.log('OPC init', device.id)
 
+    const url = source?.connect?.url ?? defaultUrl
+
     // note: if agent has not connected yet, this will save the last value and send it on connection.
     //. should it save array of values and send them all? mebbe
     cache.set('opc-avail', 'UNAVAILABLE')
 
+    // create client
     console.log(`OPC create client...`)
     const connectionStrategy = {
       initialDelay: 1000,
@@ -40,10 +49,6 @@ export class AdapterDriver {
       securityPolicy: SecurityPolicy.None,
       endpointMustExist: false,
     })
-    // the simulator service runs an opc server on this port
-    // const url = 'opc.tcp://simulator:4334/UA/LittleServer'
-    // host.docker.internal lets you access localhost on the host computer, which is running kepware
-    const url = source?.connect?.url || 'opc.tcp://host.docker.internal:4840' // default is kepware url
 
     // get connection
     let connected = false
@@ -78,17 +83,45 @@ export class AdapterDriver {
     //. here we'll iterate over inputs, fetch or subscribe to them,
     // and set the cache key-value pairs.
 
+    const browseResult = await session.browse('RootFolder')
+    console.log('OPC references of RootFolder :')
+    for (const reference of browseResult.references) {
+      console.log('   -> ', reference.browseName.toString())
+    }
+
+    const browsePath = makeBrowsePath(
+      'RootFolder',
+      '/Objects/Server.ServerStatus.BuildInfo.ProductName'
+    )
+    const result = await session.translateBrowsePath(browsePath)
+    const productNameNodeId = result.targets[0].targetId
+    console.log('OPC Product Name nodeId = ', productNameNodeId.toString())
+
     // read operator
-    let nodeId = 'ns=1;s=Operator'
-    const dataValue3 = await session.read({
+    // let nodeId =
+    // 'ns=1;s=Kepware.KEPServerEX.V6.Simulation Examples.Function.User1'
+    let nodeId = productNameNodeId
+    const dataValue = await session.read({
       nodeId,
       attributeId: AttributeIds.Value,
     })
-    console.log(`OPC read ${nodeId}:`, dataValue3.value) // a variant
-    const operator = dataValue3.value.value //. better way?
-    const key = `${device.id}-operator`
-    console.log(`OPC setting cache ${key}:`, operator)
-    cache.set(key, operator)
+    console.log(`OPC read ${nodeId}:`, dataValue) //.value) // a variant
+    // const operator = dataValue.value.value //. better way?
+    // const key = `${device.id}-`
+    // console.log(`OPC setting cache ${key}:`, operator)
+    // cache.set(key, operator)
+
+    // // read operator
+    // let nodeId = 'ns=1;s=Operator'
+    // const dataValue3 = await session.read({
+    //   nodeId,
+    //   attributeId: AttributeIds.Value,
+    // })
+    // console.log(`OPC read ${nodeId}:`, dataValue3.value) // a variant
+    // const operator = dataValue3.value.value //. better way?
+    // const key = `${device.id}-operator`
+    // console.log(`OPC setting cache ${key}:`, operator)
+    // cache.set(key, operator)
 
     // // subscribe and monitor item for n seconds
     // const subscription = ClientSubscription.create(session, {
