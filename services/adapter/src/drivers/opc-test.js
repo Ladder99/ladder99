d11cdcfe326ef002b62f03fd4ac3b66ed345c382
Brian Burns <bburns.km@gmail.com>
2022-10-23 03:33:46 -0400

    opc-test fix struct

--- services/adapter/src/drivers/opc-test.js
+++ services/adapter/src/drivers/opc-test.js
@@ -8,11 +8,13 @@ driver.start({
       console.log('cache.set', key, value)
     },
   },
-  source: { outputs: { agent: { url: 'opc.tcp://127.0.0.1:49320' } } },
+  source: { connect: { url: 'opc.tcp://127.0.0.1:49320' } },
   module: {
-    inputs: [
-      { key: 'words', nodeId: 'ns=2;s=Simulation Examples.Functions.User1' },
-      { key: 'values', nodeId: 'ns=2;s=Simulation Examples.Functions.User2' },
-    ],
+    inputs: {
+      inputs: [
+        { key: 'words', nodeId: 'ns=2;s=Simulation Examples.Functions.User1' },
+        { key: 'values', nodeId: 'ns=2;s=Simulation Examples.Functions.User2' },
+      ],
+    },
   },
 })
