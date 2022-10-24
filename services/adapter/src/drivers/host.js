8957739c3fec36aa19e38222690d48c2e5d5ddd6
Brian Burns <bburns.km@gmail.com>
2022-10-24 04:35:39 -0400

    host wip

--- services/adapter/src/drivers/host.js
+++ services/adapter/src/drivers/host.js
@@ -22,12 +22,9 @@ export class AdapterDriver {
     this.cache = cache
     this.module = module
 
-    //. gather query by grouping items together
     // get specs object like { mem: 'total, free, used' }, as expected by si module
-    // const specs = {}
-    // inputs.inputs.forEach(input => (specs[input.item] = input.subitems))
 
-    // most interesting ones avail ---
+    // most interesting ones avail in si.get() -
     // battery: hasBattery, currentCapacity, maxCapacity, capacityUnit, percent mWh
     // cpu: manufacturer, brand, speed, cores
     // cpuTemperature: main, cores
@@ -38,71 +35,108 @@ export class AdapterDriver {
     // mem: total, free, used
     // osInfo: platform, distro, release, codename, arch, hostname
     // wifiInterfaces: id, model, vendor
-    this.specs = {
-      cpuTemperature: 'main',
-      currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
-      mem: 'total, free, used',
-      fsSize: 'fs, size, used, use, available', // gives an array
-      osInfo: 'platform, distro, release, codename, arch, hostname',
-    }
+
+    // this.specs = {
+    //   cpuTemperature: 'main',
+    //   currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
+    //   mem: 'total, free, used',
+    //   fsSize: 'fs, size, used, use, available', // gives an array
+    //   osInfo: 'platform, distro, release, codename, arch, hostname',
+    // }
+
+    const inputs = module?.inputs?.inputs || {}
+    this.query = this.getQuery(inputs)
+    console.log(this.query)
 
     // setUnavailable() //. do we need this? on agent disconnect, agent outputs unavailable.
-    setInterval(this.poll.bind(this), pollInterval)
+    // setInterval(this.poll.bind(this), pollInterval)
   }
 
-  async poll() {
-    try {
-      const data = await si.get(this.specs)
+  //
 
-      // get total disk space as { size, used, use }
-      // console.log('Host fsSize', data.fsSize)
-      // console.log(data.fsSize.map(d => d.fs))
-      // data.fsSize is sthing like this - reduce to single object - or just use drvfs?
-      // windows:
-      // [
-      //   { fs: 'overlay', size: 269490393088, used: 26778972160, use: 10.47 },
-      //   { fs: 'drvfs', size: 489472126976, used: 420276023296, use: 85.86 },
-      //   { fs: '/dev/sdc', size: 269490393088, used: 26778972160, use: 10.47 }
-      // ]
-      // linux: pi just has overlay
-      //
-      // const disk = data.fsSize.reduce(
-      //   (acc, fs) => {
-      //     acc.size += fs.size
-      //     acc.used += fs.used
-      //     acc.available += fs.available
-      //     return acc
-      //   },
-      //   { size: 0, used: 0, available: 0 }
-      // )
-      // disk.use = (disk.used / (disk.size || 1)) * 100
-      // const disk = data.fsSize.find(o => o.fs === 'drvfs') || {}
-      const disk = data.fsSize.find(o => o.fs === 'overlay') || {}
-
-      // write values to cache
-      this.setValue('avail', 'AVAILABLE')
-      this.setValue('cond', 'NORMAL')
-      this.setValue('temp', lib.rounded(data.cpuTemperature.main, 1))
-      this.setValue('cputot', lib.rounded(data.currentLoad.currentLoad, 1))
-      this.setValue('cpuuser', lib.rounded(data.currentLoad.currentLoadUser, 1))
-      this.setValue(
-        'cpusys',
-        lib.rounded(data.currentLoad.currentLoadSystem, 1)
-      )
-      this.setValue('memtot', lib.rounded(data.mem.total, -6))
-      this.setValue('memfree', lib.rounded(data.mem.free, -6))
-      this.setValue('memused', lib.rounded(data.mem.used, -6))
-      this.setValue('disksize', disk.size) // bytes
-      this.setValue('diskused', lib.rounded(disk.used, -6)) // bytes rounded to mb
-      this.setValue('diskuse', lib.rounded(disk.use, 0)) // percent
-      this.setValue('diskavail', lib.rounded(disk.available, -6)) // bytes rounded to mb
-      this.setValue('os', getDataSet(data.osInfo))
-      //
-    } catch (error) {
-      console.log(error.message)
-      this.setUnavailable()
-    }
+  // get a systeminformation query
+  // inputs is like
+  // currentLoad:
+  //   currentLoad:
+  //     name: cputot
+  //     decimals: 1
+  //   currentLoadUser:
+  //     name: cpuuser
+  //     decimals: 1
+  //   currentLoadSystem:
+  //     name: cpusys
+  //     decimals: 1
+  // query is like
+  // {
+  //   cpuTemperature: 'main',
+  //   currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
+  //   mem: 'total, free, used',
+  //   fsSize: 'fs, size, used, use, available', // gives an array
+  //   osInfo: 'platform, distro, release, codename, arch, hostname',
+  // }
+  getQuery(inputs) {
+    const query = {}
+    Object.keys(inputs).forEach(item => {
+      query[item] = Object.keys(inputs[item]).join(',')
+    })
+    return query
   }
+
+  //
+
+  // async poll() {
+  //   try {
+  //     const data = await si.get(this.query)
+
+  //     // get total disk space as { size, used, use }
+  //     // console.log('Host fsSize', data.fsSize)
+  //     // console.log(data.fsSize.map(d => d.fs))
+  //     // data.fsSize is sthing like this - reduce to single object - or just use drvfs?
+  //     // windows:
+  //     // [
+  //     //   { fs: 'overlay', size: 269490393088, used: 26778972160, use: 10.47 },
+  //     //   { fs: 'drvfs', size: 489472126976, used: 420276023296, use: 85.86 },
+  //     //   { fs: '/dev/sdc', size: 269490393088, used: 26778972160, use: 10.47 }
+  //     // ]
+  //     // linux: pi just has overlay
+  //     //
+  //     // const disk = data.fsSize.reduce(
+  //     //   (acc, fs) => {
+  //     //     acc.size += fs.size
+  //     //     acc.used += fs.used
+  //     //     acc.available += fs.available
+  //     //     return acc
+  //     //   },
+  //     //   { size: 0, used: 0, available: 0 }
+  //     // )
+  //     // disk.use = (disk.used / (disk.size || 1)) * 100
+  //     // const disk = data.fsSize.find(o => o.fs === 'drvfs') || {}
+  //     const disk = data.fsSize.find(o => o.fs === 'overlay') || {}
+
+  //     // write values to cache
+  //     this.setValue('avail', 'AVAILABLE')
+  //     this.setValue('cond', 'NORMAL')
+  //     this.setValue('temp', lib.rounded(data.cpuTemperature.main, 1))
+  //     this.setValue('cputot', lib.rounded(data.currentLoad.currentLoad, 1))
+  //     this.setValue('cpuuser', lib.rounded(data.currentLoad.currentLoadUser, 1))
+  //     this.setValue(
+  //       'cpusys',
+  //       lib.rounded(data.currentLoad.currentLoadSystem, 1)
+  //     )
+  //     this.setValue('memtot', lib.rounded(data.mem.total, -6))
+  //     this.setValue('memfree', lib.rounded(data.mem.free, -6))
+  //     this.setValue('memused', lib.rounded(data.mem.used, -6))
+  //     this.setValue('disksize', disk.size) // bytes
+  //     this.setValue('diskused', lib.rounded(disk.used, -6)) // bytes rounded to mb
+  //     this.setValue('diskuse', lib.rounded(disk.use, 0)) // percent
+  //     this.setValue('diskavail', lib.rounded(disk.available, -6)) // bytes rounded to mb
+  //     this.setValue('os', getDataSet(data.osInfo))
+  //     //
+  //   } catch (error) {
+  //     console.log(error.message)
+  //     this.setUnavailable()
+  //   }
+  // }
 
   //. get this list from inputs also
   setUnavailable() {
@@ -129,10 +163,10 @@ export class AdapterDriver {
 
 //. move into cache
 
-// get object in DATA_SET format for shdr,
+// get object in DATA_SET format for shdr.
 // will return something like "free=48237472 used=12387743 total=38828348"
+// need to sanitize the keys as well as the values.
 function getDataSet(obj) {
-  // sanitize the keys as well as the values
   const str = Object.keys(obj)
     .map(key => `${sanitize(key)}=${sanitize(obj[key])}`)
     .join(' ')
