7465dac641a38c5f9a4670748df3c06088eebfea
Brian Burns <bburns.km@gmail.com>
2022-10-25 07:45:55 -0400

    host add old cmt code

---
+++ services/adapter/src/drivers/old/host-old.js
@@ -0,0 +1,49 @@
+// this.setValue('temp', lib.rounded(data.cpuTemperature.main, 1))
+// this.setValue('cputot', lib.rounded(data.currentLoad.currentLoad, 1))
+// this.setValue('cpuuser', lib.rounded(data.currentLoad.currentLoadUser, 1))
+// this.setValue(
+//   'cpusys',
+//   lib.rounded(data.currentLoad.currentLoadSystem, 1)
+// )
+// this.setValue('memtot', lib.rounded(data.mem.total, -6))
+// this.setValue('memfree', lib.rounded(data.mem.free, -6))
+// this.setValue('memused', lib.rounded(data.mem.used, -6))
+// this.setValue('disksize', disk.size) // bytes
+// this.setValue('diskused', lib.rounded(disk.used, -6)) // bytes rounded to mb
+// this.setValue('diskuse', lib.rounded(disk.use, 0)) // percent
+// this.setValue('diskavail', lib.rounded(disk.available, -6)) // bytes rounded to mb
+// this.setValue('os', getDataSet(data.osInfo))
+// //
+
+//   // console.log(itemKey, subitemDict)
+//   if (Array.isArray(subitemDict)) {
+//     // eg fsSize is an array
+
+// // get total disk space as { size, used, use }
+
+// // . a single fn could check for existence of overlay key, use that or a sum reduction.
+// // . could store the fn in the inputs.yaml, or just use a fn name like 'sum' or 'overlay'.
+
+// console.log('Host fsSize', data.fsSize)
+// console.log(data.fsSize.map(d => d.fs))
+// // data.fsSize is sthing like this - reduce to single object - or just use drvfs?
+// // windows:
+// // [
+// //   { fs: 'overlay', size: 269490393088, used: 26778972160, use: 10.47 },
+// //   { fs: 'drvfs', size: 489472126976, used: 420276023296, use: 85.86 },
+// //   { fs: '/dev/sdc', size: 269490393088, used: 26778972160, use: 10.47 }
+// // ]
+// // linux: pi just has overlay
+
+// // const disk = data.fsSize.reduce(
+// //   (acc, fs) => {
+// //     acc.size += fs.size
+// //     acc.used += fs.used
+// //     acc.available += fs.available
+// //     return acc
+// //   },
+// //   { size: 0, used: 0, available: 0 }
+// // )
+// // disk.use = (disk.used / (disk.size || 1)) * 100
+// // const disk = data.fsSize?.find(o => o.fs === 'drvfs') || {}
+// const disk = data.fsSize?.find(o => o.fs === 'overlay') || {}
