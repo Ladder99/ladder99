d1df16e37ff05aacbda1c6f9f6fa2069edb10874
Brian Burns <bburns.km@gmail.com>
2022-10-26 19:11:26 -0400

    host driver wip

--- services/adapter/src/drivers/host.js
+++ services/adapter/src/drivers/host.js
@@ -1,15 +1,12 @@
 // host driver
-// fetches systeminfo from host system.
+// fetches data from host system, writes to cache.
 // see setups/common/modules/host folder.
 
-// this library causes highcpu on windows - 2022-02
-// seems to be fixed, or at least works when run from git bash - 2022-09-26.
-// see https://github.com/sebhildebrandt/systeminformation/issues/626
 import si from 'systeminformation' // see https://github.com/sebhildebrandt/systeminformation
 
 import * as lib from '../common/lib.js' // for lib.round
 
-const pollInterval = 5000 // msec //. get from base setup
+const defaultPollInterval = 5000 // ms
 
 export class AdapterDriver {
   //
@@ -21,99 +18,66 @@ export class AdapterDriver {
     this.module = module
     this.inputs = module?.inputs?.inputs || {}
 
-    // build up a systeminformation query from inputs.yaml, like
-    // {
-    //   cpuTemperature: 'main',
-    //   currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
-    //   mem: 'total, free, used',
-    //   fsSize: 'fs, size, used, available', // gives an array
-    //   osInfo: 'platform, distro, release, codename, arch, hostname',
-    // }
-    this.query = {}
-    Object.keys(this.inputs).forEach(item => {
-      this.query[item] = Object.keys(this.inputs[item]).join(',')
-    })
-    console.log('Host query', this.query)
-
-    this.setUnavailable()
-    this.poll() // first poll
-    setInterval(this.poll.bind(this), pollInterval)
-  }
-
-  async poll() {
-    try {
-      // get data, eg
-      // {
-      //   cpuTemperature: { main: 50.5 },
-      //   currentLoad: {
-      //     currentLoad: 0.01,
-      //     currentLoadUser: 0.01,
-      //     currentLoadSystem: 0
-      //   },
-      //   mem: { total: 16777216, free: 16777216, used: 0 },
-      //   fsSize: [
-      //     {
-      //       fs: 'C:\\',
-      //       size: 16777216,
-      //       used: 0,
-      //       use: 0,
-      //       available: 16777216
-      //     }
-      //   ],
-      //   osInfo: {
-      //     platform: 'win32',
-      //     distro: 'Windows 10',
-      //     release: '10.0.19043',
-      //     codename: 'Windows 10',
-      //     arch: 'x64',
-      //     hostname: 'DESKTOP'
-      //   }
-      // }
-      const data = await si.get(this.query)
-
-      // write to cache
-      this.setValue('avail', 'AVAILABLE')
-      this.setValue('cond', 'NORMAL')
-
-      // iterate over query inputs, extract info from data, write to cache
-      // eg this.setValue('temp', lib.round(data.cpuTemperature.main, 1))
-      // eg itemKey = 'cpuTemperature', subitemDict = {main: { name, decimals }}
-      // fsSize returns an array though - see https://systeminformation.io/filesystem.html
-      for (let [itemKey, subitemDict] of Object.entries(this.inputs)) {
-        // console.log(itemKey, subitemDict)
-        // subitemKey is eg 'main'
-        for (let subitemKey of Object.keys(subitemDict)) {
-          // subitem is like { name, decimals }
-          const subitem = subitemDict[subitemKey]
-          // const value = data[itemKey][subitemKey] // pick value out of query response - won't work for fsSize
-          const itemData = data[itemKey]
-          let value
-          // check if array, eg for fsSize
-          if (Array.isArray(itemData)) {
-            //. pick out relevant drives and sum up values
-          } else {
-            value = itemData[subitemKey]
-          }
-          // console.log(subitemKey, subitem, value)
-          this.setValue(subitem.name, lib.round(value, subitem.decimals))
-        }
+    // get queries from inputs.yaml and start polling data
+    const itemKeys = Object.keys(this.inputs) // eg ['cpuTemperature', 'currentLoad', 'mem', 'fsSize']
+    for (let itemKey of itemKeys) {
+      const item = this.inputs[itemKey] // eg { main: { name, decimals }}
+      const subitemString = Object.keys(item).join(',') // eg 'main'
+      const query = { [itemKey]: subitemString } // eg { cpuTemperature: 'main' }
+      // query data
+      this.poll(query)
+      // start polling data also, unless interval is null
+      if (item._interval !== null) {
+        const pollInterval = item.pollInterval ?? defaultPollInterval
+        setInterval(this.poll.bind(this, query), pollInterval)
       }
-    } catch (error) {
-      console.log(error.message)
-      this.setUnavailable()
     }
+
+    // write to cache
+    this.setValue('avail', 'AVAILABLE')
+    this.setValue('cond', 'NORMAL')
   }
 
-  // set all keys to unavailable
-  setUnavailable() {
-    this.setValue('avail', 'UNAVAILABLE')
-    this.setValue('cond', 'UNAVAILABLE')
-    // for each input item, set each subitem.name to unavailable
-    Object.values(this.inputs).forEach(item => {
-      Object.values(item).forEach(subitem => {
-        this.setValue(subitem.name, 'UNAVAILABLE')
-      })
-    })
+  async poll(query) {
+    // get data from systeminformation
+    // eg if query is { cpuTemperature: 'main' }
+    // data will be { cpuTemperature: { main: 50.5 }}
+    // other examples:
+    // { currentLoad: {
+    //     currentLoad: 0.01,
+    //     currentLoadUser: 0.01,
+    //     currentLoadSystem: 0
+    //   } }
+    // { mem: { total: 16777216, free: 16777216, used: 0 } }
+    // fsSize returns an array -
+    // { fsSize: [
+    //   {
+    //      fs: 'C:\\',
+    //      size: 16777216,
+    //      used: 0,
+    //      use: 0,
+    //      available: 16777216
+    //   }
+    // ] }
+    const data = await si.get(query)
+
+    // extract data and write all values to cache
+    const itemKey = Object.keys(data)[0] // eg 'cpuTemperature'
+    const itemData = data[itemKey] // eg { main: 50.5 }, or [ { fs: 'C:\\', size: 16777216, used: 0, use: 0, available: 16777216 } ]
+    // fsSize returns an array, so handle specially
+    if (itemKey === 'fsSize') {
+      platform = process.platform
+    } else {
+      const subitemKeys = Object.keys(itemData) // eg ['main']
+      for (let subitemKey of subitemKeys) {
+        const subitemData = itemData[subitemKey] // eg 50.5
+        const inputOptions = this.inputs[itemKey][subitemKey] // eg { name, decimals }
+        const name = inputOptions.name // eg 'temp'
+        const decimals = inputOptions.decimals // eg 1
+        const value = lib.round(subitemData, decimals) // eg 50.5
+        this.setValue(name, value)
+      }
+    }
   }
 
   // set a cache value
--- services/adapter/src/drivers/old/host-old.js
+++ services/adapter/src/drivers/old/host-old.js
@@ -1,3 +1,7 @@
+// this library causes highcpu on windows - 2022-02
+// seems to be fixed, or at least works when run from git bash - 2022-09-26.
+// see https://github.com/sebhildebrandt/systeminformation/issues/626
+
 // this.setValue('temp', lib.round(data.cpuTemperature.main, 1))
 // this.setValue('cputot', lib.round(data.currentLoad.currentLoad, 1))
 // this.setValue('cpuuser', lib.round(data.currentLoad.currentLoadUser, 1))
@@ -54,3 +58,66 @@
 // disk.use = (disk.used / (disk.size || 1)) * 100
 // const disk = data.fsSize?.find(o => o.fs === 'drvfs') || {}
 // const disk = data.fsSize?.find(o => o.fs === 'overlay') || {}
+
+// // build up a systeminformation query from inputs.yaml, like
+// // {
+// //   cpuTemperature: 'main',
+// //   currentLoad: 'currentLoad, currentLoadUser, currentLoadSystem',
+// //   mem: 'total, free, used',
+// //   fsSize: 'fs, size, used, available', // gives an array
+// //   osInfo: 'platform, distro, release, codename, arch, hostname',
+// // }
+// this.query = {}
+// Object.keys(this.inputs).forEach(item => {
+//   this.query[item] = Object.keys(this.inputs[item]).join(',')
+// })
+// console.log('Host query', this.query)
+
+// this.poll() // first poll
+// setInterval(this.poll.bind(this), pollInterval)
+
+// // iterate over query inputs, extract info from data, write to cache
+// // eg this.setValue('temp', lib.round(data.cpuTemperature.main, 1))
+// // eg itemKey = 'cpuTemperature', subitemDict = {main: { name, decimals }}
+// // fsSize returns an array though - see https://systeminformation.io/filesystem.html
+// for (let [itemKey, subitemDict] of Object.entries(this.inputs)) {
+//   // console.log(itemKey, subitemDict)
+//   // subitemKey is eg 'main'
+//   for (let subitemKey of Object.keys(subitemDict)) {
+//     // subitem is like { name, decimals }
+//     const subitem = subitemDict[subitemKey]
+//     const value = data[itemKey][subitemKey] // pick value out of query response - won't work for fsSize
+//     // const itemData = data[itemKey]
+//     // let value
+//     // // check if array, eg for fsSize
+//     // if (Array.isArray(itemData)) {
+//     //   //. pick out relevant drives and sum up values
+//     //   //. depends on the os, which can get from process.platform = 'win32' | 'darwin' | 'linux' ...
+//     //   //. but also need to calculate use%, so need size, used, available
+//     //   // so better to hardcode most of this
+//     // } else {
+//     //   value = itemData[subitemKey]
+//     // }
+//     // console.log(subitemKey, subitem, value)
+//     this.setValue(subitem.name, lib.round(value, subitem.decimals))
+//   }
+// }
+
+// } catch (error) {
+//   console.log(error.message)
+//   // this.setUnavailable()
+//   // this.setValue('avail', 'UNAVAILABLE')
+//   // this.setValue('cond', 'FAULT')
+// }
+
+// // set all keys to unavailable
+// setUnavailable() {
+//   this.setValue('avail', 'UNAVAILABLE')
+//   this.setValue('cond', 'UNAVAILABLE')
+//   // for each input item, set each subitem.name to unavailable
+//   Object.values(this.inputs).forEach(item => {
+//     Object.values(item).forEach(subitem => {
+//       this.setValue(subitem.name, 'UNAVAILABLE')
+//     })
+//   })
+// }
