6957cbc4bd65c035655722e4043505d6cd5db774
Brian Burns <bburns.km@gmail.com>
2022-10-30 03:18:17 -0400

    jobboss refac

--- services/adapter/src/drivers/erp/jobboss/jobs.js
+++ services/adapter/src/drivers/erp/jobboss/jobs.js
@@ -2,7 +2,11 @@
 
 // see also feedback.js driver, which checks jobnum changes in cache and resets partcounts.
 
+//. make a params object
 const pollInterval = 5000 // ms - ie poll for job num change every 5 secs. keep largish to reduce db hits
+const noneJob = 'NONE' // jobnum for no job
+const jobKey = 'job' // cache key for jobnum - corresponds to path 'processes/job/process_aggregate_id-order_number'
+const jobCompleteKey = 'jcomplete' // cache key for job complete time - corresponds to 'processes/job/process_time-complete'
 
 export class Jobs {
   //
@@ -13,7 +17,7 @@ export class Jobs {
     this.pool = pool
     this.devices = devices
     this.lastJobs = {} // device.id: jobnum
-    this.deviceJobsCompleted = {} // device -> { job -> timestamp } - cache of completed jobs for each device
+    this.deviceJobsCompleted = {} // { device: { job: timestamp } } - cache of completed jobs for each device
     this.lastDate = null
 
     // await this.backfill()
@@ -34,22 +38,26 @@ export class Jobs {
 
     // iterate over all devices, check if has a jobbossId
     for (let device of this.devices) {
-      const jobbossId = device.custom?.jobbossId
+      const jobbossId = device.custom?.jobbossId // eg '8EE4B90E-7224-4A71-BE5E-C6A713AECF59'
       if (jobbossId) {
-        // get the most recently started jobs for this workcenter/device.
+        // get list of most recently started jobs for this workcenter/device.
         const jobs = await this.getJobs(jobbossId)
-        if (jobs === undefined) continue // must have had a problem - skip this device
+        if (!jobs) continue // db or other error - skip this device. note: an empty array is still truthy.
 
+        // handle jobs
         this.handleJobs(device, jobs)
       }
     }
   }
 
+  // get list of most recently started jobs for this workcenter/device from jobboss db.
+  // eg ['123','456','789']
   async getJobs(jobbossId) {
-    // get the most recently started jobs for this workcenter/device.
     // could also use where work_center='MARUMATSU', but not guaranteed unique.
     // status is C=complete, S=started?, O=ongoing?
-    // make sure status is not complete, ie C.
+    // make sure status is not complete, ie <>'C'.
+    // order by start date desc, so most recent is first.
+    // inside_oper is 1 if inside operation, 0 if not.
     const sql = `
         select top 5
           Job
@@ -59,43 +67,47 @@ export class Jobs {
           WorkCenter_OID = '${jobbossId}'
           and Status <> 'C'
           and Actual_Start is not null
+          and Inside_Oper = 1
         order by
           Actual_Start desc
       `
     // pool error handler should catch any errors, but add try/catch in case not
     let jobs
     try {
-      const result = await this.pool.query(sql)
-      // 'Job' must match case of sql
-      // use NONE to indicate no job
-      //. what if recordset is empty? then would get []? fix
-      jobs = result?.recordset?.map(record => record.Job) || ['NONE']
+      const result = await this.pool.query(sql) // query jobboss db
+      jobs = result?.recordset?.map(record => record.Job) || [] // '.Job' must match case of sql
     } catch (error) {
       console.log(`JobBoss jobs ${device.name} error`, error.message)
       console.log(`JobBoss jobs sql`, sql)
+      jobs = null // signal error
     }
     return jobs
   }
 
+  // handle jobs for a single device
   handleJobs(device, jobs) {
-    //. what if jobs is [] on accident? fix
-    const activeJob = jobs[0] // assume this is the active job, others are inactive
+    //
+    // get active job
+    // assume jobs[0] is the active job, others are inactive.
+    // if list is empty, use string 'NONE'.
+    const activeJob = jobs[0] ?? noneJob
 
+    // get cache of completed jobs for each device
+    // ie dict of { job: timestamp }
     const jobsCompleted = this.deviceJobsCompleted[device.id] ?? {}
-    // if active job changed, reset completed cache
-    if (activeJob !== this.lastActiveJobs[device.id]) {
+
+    // if active job changed
+    const lastActiveJob = this.lastActiveJobs[device.id]
+    if (activeJob !== lastActiveJob) {
     }
 
-    // this.deviceJobsCompleted[device.id] = this.deviceJobsCompleted[device.id] ?? {}
-    // this.deviceJobsCompleted[device.id][job] = 0
     // set cache value
     // sends shdr to agent IF cache value changed
-    // note: this key corresponds to path 'processes/job/process_aggregate_id-order_number'
     //. what if could pass an optional code block here to run if cache value changed?
     // eg reset the part count by sending a message to the device
     //. or, attach some code to that cache value? ie you'd have some code that would output shdr,
     // and some code that would set the jcomplete time on change.
-    this.cache.set(`${device.id}-job`, activeJob)
+    this.cache.set(`${device.id}-${jobKey}`, activeJob)
 
     // initialize last job if not set
     this.lastJobs[device.id] = this.lastJobs[device.id] ?? activeJob
@@ -108,10 +120,9 @@ export class Jobs {
     const oldJob = this.lastJobs[device.id]
     if (activeJob !== oldJob) {
       console.log(`JobBoss jobs ${device.name} job ${oldJob} to ${activeJob}`)
-      if (oldJob !== 'NONE') {
+      if (oldJob !== noneJob) {
         const now = new Date().toISOString()
-        // this key corresponds to path 'processes/job/process_time-complete'
-        this.cache.set(`${device.id}-jcomplete`, now)
+        this.cache.set(`${device.id}-${jobCompleteKey}`, now)
       }
       this.lastJobs[device.id] = activeJob // bug: had this inside the oldJob !== 'NONE' block, so didn't update
     }
