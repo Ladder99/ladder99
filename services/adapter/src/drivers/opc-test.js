88771563d805d8f95b81895e112dc67de50dcae0
Brian Burns <bburns.km@gmail.com>
2022-11-07 18:44:45 -0500

    cmt

--- services/adapter/src/drivers/opc-test.js
+++ services/adapter/src/drivers/opc-test.js
@@ -1,6 +1,11 @@
+// run this with `node opc-test.js`
+// have kepware running on localhost:49320
+// note: must use 127.0.0.1, not localhost, for the connection to work
+
 import { AdapterDriver } from './opc.js'
 
 const driver = new AdapterDriver()
+
 driver.start({
   device: { id: 'opc' },
   cache: {
