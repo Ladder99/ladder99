6b0b31f7bc710c7aa1589b1b1837df8dba118f28
Brian Burns <bburns.km@gmail.com>
2022-11-13 09:55:18 -0500

    mqtt test works

--- services/simulator/src/mqtt.js
+++ services/simulator/src/mqtt.js
@@ -13,11 +13,13 @@ export class Simulator {
       console.log(`mqtt connected`)
       // loop and publish random values with id=1 or 2, delay 1sec
       setInterval(() => {
+        const topic = 'l99/test'
         const id = Math.floor(Math.random() * 2) + 1
         const user1 = Math.random() > 0.5 ? 'jon' : 'alice'
         const user2 = Math.random() * 100
-        const payload = { id, user1, user2 }
-        this.mqtt.publish('l99/test', JSON.stringify(payload))
+        const payload = JSON.stringify({ id, user1, user2 })
+        console.log(`mqtt publish ${topic}: ${payload}`)
+        this.mqtt.publish(topic, payload)
       }, 1000)
     })
   }
