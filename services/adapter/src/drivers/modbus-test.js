df9752da1310907d96d029aaf1a61fd3953babef
Brian Burns <bburns.km@gmail.com>
2022-11-24 04:59:02 -0500

    wip

--- services/adapter/src/drivers/modbus-test.js
+++ services/adapter/src/drivers/modbus-test.js
@@ -11,8 +11,10 @@ driver.start({
       console.log('cache.set', key, value)
     },
   },
-  // source: { connect: { host: '10.1.10.130', port: 502 } },
+  source: { connect: { host: '10.1.10.130', port: 502 } },
   // source: { connect: { host: '10.1.10.131', port: 502 } },
-  source: { connect: { host: '10.1.10.132', port: 502 } },
+  // source: { connect: { host: '10.1.10.132', port: 502 } },
+  // source: { connect: { host: '10.1.10.130', port: 5021 } }, // no such port
+  // source: { connect: { host: '10.1.10.139', port: 502 } }, // no such host
   schema: {},
 })
