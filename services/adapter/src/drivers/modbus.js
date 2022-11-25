e4c02b7dcf62d1fdaee9a46eac5f16b077f95bff
Brian Burns <bburns.km@gmail.com>
2022-11-25 02:32:37 -0500

    modbus try read register

--- services/adapter/src/drivers/modbus.js
+++ services/adapter/src/drivers/modbus.js
@@ -20,14 +20,16 @@ export class AdapterDriver {
     // this.subscriptions = []
 
     try {
-      this.session = await this.getSession() // connect to server
+      this.client = await this.getClient() // connect to server
     } catch (error) {
       console.error('Modbus connection error', error)
       return
     }
 
-    console.log(`Modbus connected`, this.session)
+    console.log(`Modbus connected`)
     this.setValue('avail', 'AVAILABLE') // connected successfully
+
+    this.poll()
 
     // // iterate over inputs, fetch latest values, write to cache
     // for (let input of this.inputs) {
@@ -36,33 +38,30 @@ export class AdapterDriver {
     // }
   }
 
-  async getSession() {
+  async getClient() {
     console.log(`Modbus connecting to`, this.host, this.port)
     return new Promise((resolve, reject) => {
-      const session = new ModbusRTU()
-      session
+      const client = new ModbusRTU()
+      client
         .connectTCP(this.host, { port: this.port })
-        .then(() => resolve(session))
+        .then(() => resolve(client))
         .catch(error => reject(error))
     })
   }
 
-  // foo() {
-  //   // set the client's unit id
-  //   // set a timout for requests default is null (no timeout)
-  //   this.client.setID(1)
-  //   this.client.setTimeout(1000)
+  poll() {
+    // // set the client's unit id
+    // // set a timout for requests default is null (no timeout)
+    // this.client.setID(1)
+    // this.client.setTimeout(1000)
+    console.log('Modbus poll')
 
-  //   this.client
-  //     .readHoldingRegisters(5, 4)
-  //     .then(function (d) {
-  //       console.log('Receive:', d.data)
-  //     })
-  //     .catch(function (e) {
-  //       console.log(e.message)
-  //     })
-  //   // .then(readCoils)
-  // }
+    console.log('Modbus reading holding registers')
+    this.client
+      .readHoldingRegisters(5, 4)
+      .then(d => console.log('Receive:', d.data))
+      .catch(error => console.log(error.message))
+  }
 
   // helper methods
 
