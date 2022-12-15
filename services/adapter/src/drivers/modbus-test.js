a3826b66897b5f7eba4cf047fd6f400513e9748e
Tukusej’s Sirs <tukusejssirs@protonmail.com>
2022-12-15 22:10:40 +0100

    fix: use `Buffer#readUInt32BE()` to get piece count values

--- services/adapter/src/drivers/modbus-test.js
+++ services/adapter/src/drivers/modbus-test.js
@@ -20,7 +20,6 @@ driver.start({
   // source: { connect: { host: '10.1.10.132', port: 502 } },
   // source: { connect: { host: '10.1.10.130', port: 5021 } }, // no such port
   // source: { connect: { host: '10.1.10.250', port: 502 } }, // no such host
-  // source: { connect: { host: '192.168.2.250', port: 502 } }, // for cct test
   source: { connect: { host: '192.168.2.249', port: 502 } }, // for cct test
   schema: {
     inputs: {
@@ -30,6 +29,21 @@ driver.start({
         { key: 'warn', address: 2102 },
         { key: 'nlanes', address: 3000 },
         { key: 'l1-pcall', address: 5000, datatype: 'uint32' },
+        { key: 'l2-pcall', address: 5002, datatype: 'uint32' },
+        { key: 'l3-pcall', address: 5002, datatype: 'uint32' },
+        { key: 'l4-pcall', address: 5002, datatype: 'uint32' },
+        { key: 'l1-pcgood', address: 5008, datatype: 'uint32' },
+        { key: 'l2-pcgood', address: 5010, datatype: 'uint32' },
+        { key: 'l3-pcgood', address: 5012, datatype: 'uint32' },
+        { key: 'l4-pcgood', address: 5014, datatype: 'uint32' },
+        { key: 'l1-pcbad', address: 5016, datatype: 'uint32' },
+        { key: 'l2-pcbad', address: 5018, datatype: 'uint32' },
+        { key: 'l3-pcbad', address: 5020, datatype: 'uint32' },
+        { key: 'l4-pcbad', address: 5022, datatype: 'uint32' },
+        { key: 'l1-pcfailed', address: 5024, datatype: 'uint32' },
+        { key: 'l2-pcfailed', address: 5026, datatype: 'uint32' },
+        { key: 'l3-pcfailed', address: 5028, datatype: 'uint32' },
+        { key: 'l4-pcfailed', address: 5030, datatype: 'uint32' }
       ],
     },
   },
