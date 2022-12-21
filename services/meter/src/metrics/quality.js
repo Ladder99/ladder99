e58cb298fa9808fbf352a201437efbaa0a700945
Brian Burns <bburns.km@gmail.com>
2022-12-21 14:57:15 -0600

    meter - start quality metric plugin

---
+++ services/meter/src/metrics/quality.js
@@ -0,0 +1,159 @@
+// update good count and total count bins based on pcall, pcgood counters in history table.
+
+const metricIntervalDefault = 5 // seconds
+
+export class Metric {
+  //
+  async start({ client, db, device, metric }) {
+    this.client = client
+    this.db = db
+    this.device = device
+    this.metric = metric
+    // this.lastStop = null
+
+    // const deviceName = this.device.name
+    // console.log(`Count ${deviceName} - start metric...`)
+
+    // console.log(`Count ${deviceName} - get device node_id...`)
+    // this.device_id = await this.db.getNodeId(device.path) // repeats until device is there
+
+    // // need this dataitemId as we'll be writing directly to the history table
+    // console.log(`Count ${deviceName} - get dataitem_id...`)
+    // const path = `${device.path}/${metric.lifetimePath}`
+    // this.lifetime_id = await this.db.getNodeId(path) // repeat until dataitem there
+
+    // // get polling interval - either from metric in setup yaml or default value
+    // this.interval = (metric.interval || metricIntervalDefault) * 1000 // ms
+
+    // this.offset = 3000 // ms - look this far back in time for raw count values so adapter has time to write data
+
+    // await this.backfill() // backfill any missing values
+    await this.poll() // do first poll
+    // this.timer = setInterval(this.poll.bind(this), this.interval) // poll db
+  }
+
+  // poll db and update lifetime count - called by timer
+  async poll() {
+    // const deviceName = this.device.name
+    // // console.log(`Count ${deviceName} - poll db, write lifetime count`)
+    // // due to nature of js event loop, poll is not gonna be called exactly every this.interval ms.
+    // // that means we could miss job count records, causing 'misses'.
+    // // so keep track of lastStop.
+    // // well that didn't help. so add an offset to give adapter time to write data.
+    // const now = new Date()
+    // const start =
+    //   this.lastStop ||
+    //   new Date(now.getTime() - this.offset - this.interval).toISOString()
+    // // const stop = now.toISOString()
+    // const stop = new Date(now.getTime() - this.offset).toISOString()
+    // const lifetimePath = this.metric.lifetimePath
+    // // console.log(`Count ${deviceName} - start,stop`, start, stop)
+    // // get last lifetime count, before start time
+    // const record = await this.db.getLastRecord(deviceName, lifetimePath, start)
+    // let lifetimeCount = record ? record.value : 0
+    // // console.log(`Count ${deviceName} - lifetimeCount`, lifetimeCount)
+    // // get job counts
+    // //. currently gets from history_float view
+    // const rows = await this.db.getHistory(
+    //   deviceName,
+    //   this.metric.deltaPath,
+    //   start,
+    //   stop
+    // )
+    // // console.log(`Count ${deviceName} - job count rows`, rows)
+    // // rows will be like (for start=10:00:00am, stop=10:00:05am)
+    // // time, value
+    // // 9:59:59am, 99
+    // // 10:00:00am, 100
+    // // 10:00:01am, 101
+    // // 10:00:02am, 102
+    // // 10:00:03am, 0
+    // // 10:00:04am, 1
+    // // 10:00:05am, 2
+    // if (rows && rows.length > 1) {
+    //   let previousRow = rows[0] // { time, value }
+    //   const lifetimeRows = []
+    //   for (let row of rows.slice(1)) {
+    //     // get delta from previous value
+    //     const deltaCount = row.value - previousRow.value
+    //     if (deltaCount > 0) {
+    //       lifetimeCount += deltaCount
+    //       // write time, lifetime
+    //       // better to save these up in an array and write all at once, for speed
+    //       // await this.db.writeHistory(
+    //       //   this.device_id,
+    //       //   this.lifetime_id,
+    //       //   row.time.toISOString(),
+    //       //   lifetimeCount
+    //       // )
+    //       const lifetimeRow = {
+    //         node_id: this.device_id,
+    //         dataitem_id: this.lifetime_id,
+    //         time: row.time.toISOString(),
+    //         value: lifetimeCount,
+    //       }
+    //       lifetimeRows.push(lifetimeRow)
+    //     }
+    //     previousRow = row
+    //   }
+    //   // console.log(`Count ${deviceName} - writing lifetime rows`, lifetimeRows)
+    //   await this.db.addHistory(lifetimeRows)
+    // }
+    // // save time for next poll
+    // this.lastStop = stop
+  }
+
+  // backfill missing partcount records
+  async backfill() {
+    // const deviceName = this.device.name
+    // console.log(`Count ${deviceName} - backfill any missed partcounts`)
+    // const now = new Date()
+    // // get latest lifetime count record
+    // let record = await this.db.getLastRecord(
+    //   deviceName,
+    //   this.metric.lifetimePath,
+    //   now.toISOString()
+    // )
+    // console.log(`Count ${deviceName} - last record`, record)
+    // // if no lifetime record, start from the beginning
+    // if (!record) {
+    //   const record2 = await this.db.getFirstRecord(
+    //     deviceName,
+    //     this.metric.deltaPath
+    //   )
+    //   console.log(`Count ${deviceName} - first record`, record2)
+    //   // no delta data either, so exit
+    //   if (!record2) {
+    //     return
+    //   }
+    //   // record = { time: record2.time, value: 0}
+    //   record = {}
+    //   record.time = record2.time
+    //   record.value = 0
+    // }
+    // const start = record.time.toISOString()
+    // const stop = now.toISOString()
+    // let lifetime = record.value
+    // const rows = await this.db.getHistory(
+    //   deviceName,
+    //   this.metric.deltaPath,
+    //   start,
+    //   stop
+    // ) // gets last one before start also, if any
+    // let previous = rows[0]
+    // for (let row of rows.slice(1)) {
+    //   const delta = row.value - previous.value
+    //   if (delta > 0) {
+    //     lifetime += delta
+    //     await this.db.writeHistory(
+    //       this.device_id,
+    //       this.lifetime_id,
+    //       row.time.toISOString(),
+    //       lifetime
+    //     )
+    //   }
+    //   previous = row
+    // }
+    // console.log(`Count ${deviceName} - backfill done`)
+  }
+}
---
+++ services/relay/src/migrations/versions/024-update-bins-metrics.sql
@@ -0,0 +1,65 @@
+
+------------------------------------------------------------
+-- bins table
+------------------------------------------------------------
+-- add more columns
+
+-- create table if not exists bins (
+--   device_id integer references nodes, -- node_id of a device
+--   resolution interval, -- 1min, 1hr, 1day, 1week, 1month, 1quarter, 1year
+--   time timestamptz, -- rounded down to start of current minute, hour, day, etc
+--   active int, -- number of minutes device was active during time resolution
+--   available int, -- number of minutes device was available during time resolution
+--   primary key (device_id, resolution, time) -- need this so can find right record quickly for updating
+-- );
+
+-- alter table raw.bins add column clock_secs int; --. to calc rates
+alter table raw.bins add column good_count int;
+alter table raw.bins add column total_count int;
+alter table raw.bins add column reject_count int;
+alter table raw.bins add column actual_rate int; --. not needed?
+alter table raw.bins add column ideal_rate int;
+
+
+------------------------------------------------------------
+-- metrics view
+------------------------------------------------------------
+-- drop view if exists metrics_secondary; -- depends on metrics view
+drop view if exists metrics;
+
+create or replace view metrics as
+select 
+  devices.props->>'path' as device,
+  bins.resolution,
+  bins.time,
+  bins.active, --. rename to active_mins
+  bins.available, --. rename to available_mins
+  -- note: coalesce returns the first non-null value (works like an or operator),
+  -- and nullif returns the first value, unless it equals 0.0, when it returns null -
+  -- then the whole expression is null. avoids div by zero error.
+  coalesce(bins.active::float,0) / nullif(bins.available::float,0.0) as availability,
+  bins.good_count,
+  bins.total_count,
+  bins.reject_count,
+  coalesce(bins.good_count::float,0) / nullif(bins.total_count::float,0.0) as quality,
+  bins.actual_rate,
+  bins.ideal_rate,
+  coalesce(bins.actual_rate::float,0) / nullif(bins.ideal_rate::float,0.0) as performance
+from raw.bins
+join raw.nodes as devices on raw.bins.device_id = devices.node_id;
+
+
+------------------------------------------------------------
+-- metrics_secondary view
+------------------------------------------------------------
+
+drop view if exists metrics_secondary;
+
+create or replace view metrics_secondary as
+select 
+  device,
+  resolution,
+  time,
+  availability * quality * performance as oee
+from metrics;
+
