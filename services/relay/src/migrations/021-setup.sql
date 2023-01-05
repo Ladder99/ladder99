d4a1ff796d68006d7a98774df1f2287803313c0a
Brian Burns <bburns.km@gmail.com>
2023-01-05 14:55:43 -0600

    cmts

--- services/relay/src/migrations/021-setup.sql
+++ services/relay/src/migrations/021-setup.sql
@@ -2,3 +2,11 @@
 --. make a table to mirror the setup.yaml configuration file.
 ---------------------------------------------------------------------
 
+
+-- CREATE TABLE IF NOT EXISTS setup (
+--   key text PRIMARY KEY, -- adds setup_pkey index
+--   value jsonb
+-- );
+
+
+
