32a389ac4611f13d79e2c2bc2bd2fbe6531d329c
Brian Burns <bburns.km@gmail.com>
2023-01-16 10:29:31 -0600

    meter on rollover start with delta 0
    because it was starting at 928, as read from sencon,
    and writing that all into one minute bin

--- services/meter/src/drivers/bin.js
+++ services/meter/src/drivers/bin.js
@@ -82,7 +82,8 @@ export class Metric {
       // might lose some counts if counter resets to 0 before we get a chance to read it
       if (deltaCount < 0) {
         console.log(this.me, `count reset (delta < 0)`)
-        deltaCount = currentCount
+        // deltaCount = currentCount // problem -
+        deltaCount = 0
       }
 
       if (deltaCount > 0) {
