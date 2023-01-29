fbf65739ebaddeb7128f8737f5e16b8b6aeab8f6
Brian Burns <bburns.km@gmail.com>
2023-01-29 04:19:23 -0600

    sql shift_end to shift_stop

--- services/relay/src/migrations/versions/030-setup.sql
+++ services/relay/src/migrations/versions/030-setup.sql
@@ -8,7 +8,7 @@ create table if not exists setup.devices (
   name text primary key, -- adds devices_pkey index
   setup_allowance_mins float default 0.0,
   shift_start text default '08:00',
-  shift_end text default '17:00'
+  shift_stop text default '17:00'
 );
 
 -- insert into setup.devices (name, setup_allowance_mins) values 
