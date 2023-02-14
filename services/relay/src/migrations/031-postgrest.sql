fcfa8e78dcd2dd894d558ca6d3f712af0de6203e
Brian Burns <bburns.km@gmail.com>
2023-02-14 04:00:55 -0600

    compose - add relay PGAUTHPASSWORD envar

--- services/relay/src/migrations/031-postgrest.sql
+++ services/relay/src/migrations/031-postgrest.sql
@@ -1,15 +1,12 @@
 -- add postgrest roles etc
 -- see https://postgrest.org/en/latest/install.html
 
--- IMPORTANT: MUST CREATE AUTHENTICATOR ROLE MANUALLY through psql or pgadmin!
--- use the same password as the one in the .env file for PGAUTHPASSWORD.
---. alternative would be to use text substitution here - could do later.
+-- note: there is no "create role if not exists"
 
--- create role authenticator noinherit login password '${PGAUTHPASSWORD}';
+-- create role for postgrest
+create role authenticator noinherit login password '${PGAUTHPASSWORD}';
 
--- there is no "create role if not exists" - 
--- so must keep this in a versioned migration, or use script from here -
--- https://stackoverflow.com/questions/8092086/create-postgresql-role-user-if-it-doesnt-exist
+-- create role for anonymous users
 create role anon_user nologin;  -- read-only user
 grant anon_user to authenticator;
 
