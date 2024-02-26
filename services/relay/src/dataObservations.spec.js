f910fe71c823dc398bf1c5a680afabb9e43c996d
Otto Bolyós <obolyos@mriiot.com>
2024-02-26 15:06:58 +0100

    feat(relay): persist MTConnect XML condition tag name as value

---
+++ services/relay/src/dataObservations.spec.js
@@ -0,0 +1,109 @@
+import { jest } from '@jest/globals'
+
+import { getHistoryRecords } from './dataObservations'
+
+describe('dataObservations', () => {
+  describe('getHistoryRecords()', () => {
+    describe('general tests', () => {
+      it('should return an empty array when no observation has `dataitem_id` property', () => {
+        expect(getHistoryRecords([{
+          category: 'CONDITION',
+          dataItemId: '48e7290b1184_condition',
+          device_id: 751,
+          sequence: '51',
+          tag: 'Normal',
+          timestamp: '2024-02-25T03:13:36.67177Z',
+          type: 'SYSTEM',
+          uid: 'Main/48e7290b1184_condition'
+        }])).toStrictEqual([])
+      })
+
+      it('should return an empty array when no observation has `dataitem_id` property', () => {
+        const warn = jest.spyOn(console, 'warn').mockImplementation((m) => {})
+        const observations = {
+          category: 'CONDITION',
+          dataItemId: '48e7290b1184_condition',
+          dataitem_id: 774,
+          device_id: 751,
+          sequence: '51',
+          tag: 'Normal',
+          timestamp: '2024-02-25T03:13:36.67177Z',
+          type: 'SYSTEM',
+          uid: 'Main/48e7290b1184_condition'
+        }
+
+        getHistoryRecords(observations)
+
+        expect(warn).toHaveBeenCalledWith(`[getHistoryRecords] WARN The following value was set to 'observations', but it is not an array:`, observations)
+      })
+    })
+
+    describe('test `value` type conversion', () => {
+      it('should be converted to number when `category` is `SAMPLE` and `value` is a number stored as a string', () => {
+        expect(getHistoryRecords([{
+          category: 'SAMPLE',
+          dataItemId: '409151d72b38_pcc',
+          dataitem_id: 771,
+          device_id: 769,
+          name: 'partcount',
+          sequence: '31',
+          subType: 'COMPLETE',
+          tag: 'PartCount',
+          timestamp: '2024-02-25T03:05:44.384587Z',
+          uid: 'Main/409151d72b38_pcc',
+          value: '111'
+        }])).toStrictEqual([
+          {
+            dataitem_id: 771,
+            node_id: 769,
+            time: '2024-02-25T03:05:44.384587Z',
+            value: 111
+          }
+        ])
+      })
+
+      it('should be converted to JSON string', () => {
+        expect(getHistoryRecords([{
+          dataItemId: 'ox003_sharcs_adapter_uri',
+          dataitem_id: 743,
+          device_id: 111,
+          sequence: '9',
+          tag: 'AdapterURI',
+          timestamp: '2024-02-25T03:05:44.384387Z',
+          uid: 'Main/ox003_sharcs_adapter_uri',
+          value: 'mqtt://mosquitto:1883'
+        }])).toStrictEqual([
+          {
+            dataitem_id: 743,
+            node_id: 111,
+            time: '2024-02-25T03:05:44.384387Z',
+            value: '"mqtt://mosquitto:1883"'
+          }
+        ])
+      })
+    })
+
+    describe('test the value for `CONDITION` category', () => {
+      it('should be set to the `tag` value', () => {
+        expect(getHistoryRecords([{
+          category: 'CONDITION',
+          dataItemId: '48e7290b1184_condition',
+          dataitem_id: 1201,
+          device_id: 751,
+          sequence: '51',
+          tag: 'Normal',
+          timestamp: '2024-02-25T03:13:36.67177Z',
+          type: 'SYSTEM',
+          uid: 'Main/48e7290b1184_condition'
+        }])).toStrictEqual([
+          {
+            dataitem_id: 1201,
+            node_id: 751,
+            time: '2024-02-25T03:13:36.67177Z',
+            value: '"Normal"'
+          }
+        ])
+      })
+    })
+  })
+})
